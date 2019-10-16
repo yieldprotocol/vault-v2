eec009a8b7c0398ed54a6aa9a64180e953b003f7
Allan Niemerg <allan.niemerg@bloxroute.com>
2019-10-16 18:53:58 -0500

    added development network

--- truffle-config.js
+++ truffle-config.js
@@ -52,13 +52,12 @@ module.exports = {
     //  network_id: "*",       // Any network (default: none)
     // },
 
-    /****
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    ****/
+
 
     // Another network with more advanced options...
     // advanced: {
