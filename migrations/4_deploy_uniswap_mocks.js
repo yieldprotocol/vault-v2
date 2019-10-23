d1420d4146012279524099e00e502af26886a0be
Allan Niemerg <allan.niemerg@bloxroute.com>
2019-10-22 21:12:28 -0500

    added initial deploy for development

--- migrations/4_deploy_uniswap_mocks.js
+++ migrations/4_deploy_uniswap_mocks.js
@@ -1,6 +1,25 @@
 const UniswapFactoryMock = artifacts.require("UniswapFactoryMock");
+const Treasurer = artifacts.require("Treasurer");
+const Oracle = artifacts.require("Oracle");
+const yToken = artifacts.require("yToken");
+
+setup = async () => {
+  let treasurer = await Treasurer.deployed();
+  let oracle = await Oracle.deployed();
+  var rate = web3.utils.toWei(".01");
+  await oracle.set(rate);
+  await treasurer.setOracle(oracle.address);
+  var thedate = Math.floor(Date.now() / 1000) + (24*60*60)*30;
+  await treasurer.issue("1", thedate.toString());
+}
 
 module.exports = function(deployer, network, accounts) {
-  if(network == "development")
-    deployer.deploy(UniswapFactoryMock);
+  if(network == "development"){
+
+    //var apromise = setup(deployer.provider);
+    deployer.deploy(UniswapFactoryMock)
+      .then(async () => {await setup()})
+
+    //return apromise;
+  }
 };
