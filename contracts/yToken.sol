ccdb931e5c856ba73f795a5e5d2c941f07b6f119
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-04-18 21:16:03 +0100

    Replaced openzeppelin imports.

--- contracts/yToken.sol
+++ contracts/yToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.2;
 
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
 
 
 contract yToken is ERC20Burnable, ERC20Mintable {
