934ce43f21cc944a9039fc4e502552feac8b52a4
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-04-23 14:55:08 +0100

    Fixed linting.

--- contracts/YDai.sol
+++ contracts/YDai.sol
@@ -5,14 +5,13 @@ import "./IVat.sol";
 import "./IPot.sol";
 import "@hq20/contracts/contracts/math/DecimalMath.sol";
 
+
 ///@dev yDai is a yToken targeting Dai
-
-
 contract YDai is YToken {
     using DecimalMath for uint256;
-    
+
     Vat public vat;
-    Pot public pot; 
+    Pot public pot;
 
     uint256 public chi;  //chi accumulator (for dsr) at maturity
     uint256 public rate; //rate accumulator (for stability fee) at maturity
@@ -28,7 +27,6 @@ contract YDai is YToken {
         pot = Pot(pot_);
     }
 
-
     /// @dev Return debt in underlying of an user
     function debtOf(address user) public view returns (uint256) {
         if (isMature){
@@ -37,9 +35,8 @@ contract YDai is YToken {
             return debt[user].muld(currentRate.divd(rate, 27) ,27);
         } else {
             return debt[user];
-        }   
+        }
     }
-
 
     /// @dev Mature yToken to make redeemable.
     function mature() public returns (bool) {
