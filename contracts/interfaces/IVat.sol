f810ec44a17b0afbac03210ddbd8de56b6ccfa9c
Allan Niemerg <niemerg@gmail.com>
2020-04-27 19:09:37 -0400

    expose function in definition

--- contracts/interfaces/IVat.sol
+++ contracts/interfaces/IVat.sol
@@ -7,7 +7,7 @@ interface IVat {
     // function can(address, address) external view returns (uint);
     function ilks(bytes32) external view returns (uint, uint, uint, uint, uint);
     // function dai(address) external view returns (uint);
-    // function urns(bytes32, address) external view returns (uint, uint);
+    function urns(bytes32, address) external view returns (uint, uint);
     function frob(bytes32, address, address, address, int, int) external;
     // function hope(address) external;
     // function move(address, address, uint) external;
