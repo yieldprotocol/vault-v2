31036c0847025a31309e71c4edc3a0502ed612bf
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-05-07 21:26:23 +0100

    _price

--- contracts/mocks/TestOracle.sol
+++ contracts/mocks/TestOracle.sol
@@ -5,13 +5,13 @@ import "./../interfaces/IOracle.sol";
 
 //Using fake contract instead of abstract for mocking
 contract TestOracle is IOracle {
-    uint256 internal price; // units of collateral per dai in RAY
+    uint256 internal _price; // units of collateral per dai in RAY
 
     function setPrice(uint256 price_) public {
-        price = price_;
+        _price = price_;
     }
 
     function price() public view override returns (uint256) {
-        return price;
+        return _price;
     }
 }
