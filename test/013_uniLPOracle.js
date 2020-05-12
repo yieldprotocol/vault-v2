30a44f93d93be9e9a58f149591fdce55037d6108
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-05-12 15:20:21 +0100

    Removed unnecessary code.

--- test/013_uniLPOracle.js
+++ test/013_uniLPOracle.js
@@ -1,59 +1,13 @@
 const UniLPOracle = artifacts.require('./UniLPOracle');
-const ERC20 = artifacts.require("./TestERC20");
-const DaiJoin = artifacts.require('DaiJoin');
-const GemJoin = artifacts.require('./GemJoin');
-const Vat = artifacts.require('./Vat');
-const Pot = artifacts.require('./Pot');
 const Uniswap = artifacts.require('./Uniswap');
 
-const truffleAssert = require('truffle-assertions');
-const helper = require('ganache-time-traveler');
-
-contract('ChaiOracle', async (accounts) =>  {
+contract('UniLPOracle', async (accounts) =>  {
     let [ owner ] = accounts;
-    let vat;
-    let pot;
-    let dai;
-    let weth;
-    let daiJoin;
-    let wethJoin;
-    let chaiOracle;
     let uniswap;
 
-    const ilk = web3.utils.fromAscii("ETH-A")
-    const Line = web3.utils.fromAscii("Line")
-    const spot = web3.utils.fromAscii("spot")
-    const linel = web3.utils.fromAscii("line")
-
     const RAY  = "1000000000000000000000000000";
-    const RAD = web3.utils.toBN('45');
-    const supply = web3.utils.toWei("1000");
-    const limits =  web3.utils.toBN('10000').mul(web3.utils.toBN('10').pow(RAD)).toString(); // 10000 * 10**45
 
     beforeEach(async() => {
-        // Set up vat, join and weth
-        vat = await Vat.new();
-        await vat.rely(vat.address, { from: owner });
-
-        weth = await ERC20.new(supply, { from: owner }); 
-        await vat.init(ilk, { from: owner }); // Set ilk rate to 1.0
-        wethJoin = await GemJoin.new(vat.address, ilk, weth.address, { from: owner });
-        await vat.rely(wethJoin.address, { from: owner });
-
-        dai = await ERC20.new(supply, { from: owner });
-        daiJoin = await DaiJoin.new(vat.address, dai.address, { from: owner });
-        await vat.rely(daiJoin.address, { from: owner });
-
-        // Setup vat
-        await vat.file(ilk, spot,    RAY, { from: owner });
-        await vat.file(ilk, linel, limits, { from: owner });
-        await vat.file(Line,       limits); // TODO: Why can't we specify `, { from: owner }`?
-
-        // Setup pot
-        pot = await Pot.new(vat.address);
-        await vat.rely(pot.address, { from: owner });
-
-
         uniswap = await Uniswap.new();
         // Setup UniLPOracle
         uniLPoracle = await UniLPOracle.new(uniswap.address, { from: owner });
@@ -84,6 +38,4 @@ contract('ChaiOracle', async (accounts) =>  {
             expectedResult
         );
     });
-
-    // TODO: Test with ERC20Dealer
 });
