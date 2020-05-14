890c2da38499ca8357acff20df6a9eaaf6949bb4
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-05-14 11:28:57 +0100

    ChaiDealer uses the uncoverted Saver interface

--- contracts/ChaiDealer.sol
+++ contracts/ChaiDealer.sol
@@ -11,30 +11,31 @@ import "./ERC20Dealer.sol";
 contract ChaiDealer is ERC20Dealer {
 
     ISaver internal _saver;
+    IERC20 internal _chai;
 
     constructor (
         address saver_,
         address yDai_,
         address chai_,
         address chaiOracle_
-    ) public ERC20Dealer(yDai_, chai_, chaiOracle_) {
+    ) public ERC20Dealer(yDai_, chai_, chaiOracle_) { // ERC20Dealer also moves chai
         _saver = ISaver(saver_);
+        _chai = IERC20(chai_);
     }
 
     /// @dev Takes chai from `from` address and gives it to the Saver
     // from --- Chai ---> saver
     function post(address from, uint256 chai) public override {
         // TODO: Consider a require on super.post()
-        super.post(from, chai);                // Grab chai and update posted
-        _token.approve(address(_saver), chai); // Saver will take chai
-        _saver.join(address(this), chai);      // Send chai to Saver
+        super.post(from, chai);                  // Grab chai and update posted
+        _chai.transfer(address(_saver), chai);   // Give chai to saver
     }
 
     /// @dev Takes chai from Saver and gives it to `to` address
     // us --- Token ---> to
     function withdraw(address to, uint256 chai) public override {
-        _saver.exit(address(this), chai);      // Take chai from Saver
-        super.withdraw(to, chai);              // Check collateralization, send chai to user and update posted
+        _saver.releaseChai(address(this), chai); // Take chai from Saver
+        super.withdraw(to, chai);                // Check collateralization, send chai to user and update posted
         // TODO: Consider a require on super.withdraw()
     }
 }
