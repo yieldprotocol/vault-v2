02d24281d44d2f1398d2d741e564f2c635704a4d
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-06-01 11:03:05 +0100

    Added `toWad` for consistency

--- test/shared/utils.js
+++ test/shared/utils.js
@@ -1,6 +1,12 @@
 const ethers = require("ethers");
 const bigNumberify = ethers.utils.bigNumberify;
 
+/// @dev Converts a number to WAD precision, for number up to 10 decimal places
+const toWad = function(value) {
+    let exponent = bigNumberify('10').pow(bigNumberify('8'));
+    return bigNumberify(value*10**10).mul(exponent);
+}
+
 /// @dev Converts a number to RAY precision, for number up to 10 decimal places
 const toRay = function(value) {
     let exponent = bigNumberify('10').pow(bigNumberify('17'));
@@ -14,6 +20,7 @@ const toRad = function(value) {
 }
 
 module.exports = {
+    toWad,
     toRay,
     toRad
 }
