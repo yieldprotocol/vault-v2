5e7d155fcd23d2a5094d6ed7c7c93c0a3abe294e
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-06-08 15:47:18 +0100

    End.sol and Shutdown.sol compile.

--- contracts/Shutdown.sol
+++ contracts/Shutdown.sol
@@ -7,9 +7,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IVat.sol";
+import "./interfaces/IEnd.sol";
 import "./interfaces/IChai.sol";
 import "./interfaces/IOracle.sol";
 import "./interfaces/ITreasury.sol";
+import "./interfaces/IVault.sol";
+import "./interfaces/IYDai.sol";
 import "./Constants.sol";
 import "@nomiclabs/buidler/console.sol";
 
@@ -30,12 +33,12 @@ contract Shutdown is Constants() {
     IOracle internal _chaiOracle;
     IEnd internal _end;
     ITreasury internal _treasury;
-    IDealer internal _chaiDealer;
-    IDealer internal _wethDealer;
+    IVault internal _chaiDealer;
+    IVault internal _wethDealer;
 
-    mapping(uint256 => YDai) public series;
+    mapping(uint256 => IYDai) public series;
     mapping(bytes32 => mapping(address => uint256)) public posted;
-    mapping(bytes32 => mapping(uint256 => mapping(address => uint256)) public debtYDai;
+    mapping(bytes32 => mapping(uint256 => mapping(address => uint256))) public debtYDai;
 
     constructor () public {
         _vat.hope(address(_treasury));
@@ -43,36 +46,36 @@ contract Shutdown is Constants() {
     }
 
     /// @dev Takes a series position from Dealer
-    function grab(series, collateral, user) public {
+    function grab(uint256 maturity, bytes32 collateral, address user) public {
         // Copy and delete debtYdai[series][collateral][user]
         // Add and remove inDai(debtYdai[series][collateral][user])*oracles[collateral].price() from/to posted[collateral][user]
     }
 
     /// @dev Takes any collateral from Dealer, if there are no positions
-    function grab(collateral, user) public {
+    function grab(bytes32 collateral, address user) public {
         // Check totalDebtYdai[user] == 0
         // Add and remove posted[collateral][user]
     }
 
     /// @dev Converts a chai position to a weth one
-    function chaiToWeth(series, user) public {
+    function chaiToWeth(uint256 maturity, address user) public {
         
     }
 
     /// @dev Converts a weth position to a chai one
-    function wethToChai(series, user) public {
+    function wethToChai(uint256 maturity, address user) public {
         
     }
 
     /// @dev Repays debt using YDai
-    function repay(series, collateral, user, yDaiAmount) public { }
+    function repay(uint256 maturity, bytes32 collateral, address user, uint256 yDaiAmount) public { }
 
     /// @dev Repays weth debt using posted collateral. Users might have to convert from chai to weth or viceversa
     /// according to the contract holdings.
     /// posted[WETH][user] = posted[WETH][user] - inDai(debt[WETH][series][user]) * fix[ilk]; delete debt[WETH][series][user]
     /// posted[CHAI][user] = posted[CHAI][user] - inDai(debt[CHAI][series][user] * fix[ilk]) / chi; delete debt[CHAI][series][user]
-    function settle(series, collateral, user) public { }
+    function settle(uint256 maturity, bytes32 collateral, address user) public { }
 
     /// @dev Withdraw free collateral
-    function withdraw(collateral, user) public { }
+    function withdraw(bytes32 collateral, address user) public { }
 }
---
+++ contracts/interfaces/IEnd.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.6.2;
+
+
+/// @dev interface for the End contract from MakerDao
+interface IEnd {
+
+}
