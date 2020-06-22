d822e66c8bf477bb189d2d5df7dcb37ad93bce99
Alfredo Palhares <alfredo@palhares.me>
2020-06-22 21:27:02 +0100

    Add Buidler Testing

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
 
-# Add date
 log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')]  $1"
 }
 
 command=$1
 
-yarn_v=$(which yarn --version)
-
+yarn_v=$(yarn --version)
 log "Using yarn version: ${yarn_v}"
+
+# Assume it will fail
+retcode=1
 
 case $command in
     "lint")
     log "Running Lint"
     yarn run lint
-    local _retcode=$?
-    log "Yarn gave return code: $?"
-    # TODO: Return the proper code
+    retcode=$?
+    log "Lint gave return code: $?"
     ;;
     "test")
     log "Running Tests"
-    # TODO: Add buidler
-    ;;
-    "ganache") #This is bug
-    log "Running Ganache"
-    #npx ganache-cli \
-    #    # --db "db_ganache/" \
-    #    --mnemonic "all your mnemonic are belong to us seed me up scotty over" \
-    #    --gasLimit 0xfffffffffff \
-    #    --gasPrice 0 \
-    #    --port 8545 \
-    #    --networkId 5777 \
-    #    --host 0.0.0.0
+    npx buidler test
+    retcode=$?
+    log "Buidler gave return code: $?"
     ;;
     *)
+    echo "Please use lint|test"
     ;;
 esac
 
 
-exit 0
+exit $retcode
