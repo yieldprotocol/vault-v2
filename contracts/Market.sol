1b8c859cd4006d43f2f29cded330277bd48e6319
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-06-25 16:11:13 +0100

    Generalized variables and functions

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -2,176 +2,130 @@ pragma solidity ^0.6.2;
 
 import "@hq20/contracts/contracts/math/DecimalMath.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Constants.sol";
 import "@nomiclabs/buidler/console.sol";
 
 
 /// @dev The Market contract exchanges Dai for yDai at a price defined by a specific formula.
-contract Market is Constants {
+contract Market is ERC20, Constants {
     using DecimalMath for uint256;
     using DecimalMath for uint8;
     using SafeMath for uint256;
 
     event Investment(address investor, uint256 sharesPurchased);
-    event DaiToYDai(address trader, uint256 daiIn, uint256 yDaiOut);
+    event Swap(
+        address indexed trader,
+        address indexed tokenSold,
+        address indexed tokenReceived,
+        uint256 tokensIn,
+        uint256 tokensOut
+    );
 
     uint256 public constant FEE_RATE = 500;        // fee = 1/feeRate = 0.2%
 
-    IERC20 public dai;
-    IERC20 public yDai;
+    IERC20 public t0; // Dai
+    IERC20 public t1; // yDai
 
-    uint256 public daiPool;
-    uint256 public yDaiPool;
-    uint256 public invariant;
-    uint256 public totalShares;
-    mapping(address => uint256) public shares;
+    uint256 internal _invariant;
+    bool setup = true;
 
-    constructor(address dai_, address yDai_) public {
-        dai = IERC20(dai_);
-        yDai = IERC20(yDai_);
+    mapping(address => uint256) public pools;
+
+    constructor(address t0_, address t1_) public ERC20("Name", "Symbol") {
+        t0 = IERC20(t0_);
+        t1 = IERC20(t1_);
     }
 
-    modifier initialized() {
-        require(
-            invariant > 0 && totalShares > 0,
-            "Market: Not initialized"
-        );
-        _;
-    }
-
-    function initializeExchange(uint256 daiAmount, uint256 yDaiAmount) external {
-        require(
-            invariant == 0 && totalShares == 0,
-            "Invariant or totalShares != 0"
-        );
-        // Prevents share cost from being too high or too low - potentially needs work
-        require(
-            daiAmount >= 10000 && yDaiAmount >= 10000,
+    function init(uint256 t0Amount, uint256 t1Amount) external {
+        /* require(
+            setup,
+            revert("Market: Already initialized")
+        ); */ // TODO: Find out syntax issue
+        require( // Prevents share cost from being too high or too low - potentially needs work
+            t0Amount >= 10000 && t1Amount >= 10000,
             "Market: Initialization Parameters"
         );
-        daiPool = daiAmount;
-        yDaiPool = yDaiAmount;
-        invariant = daiPool.mul(yDaiPool);
-        shares[msg.sender] = 1000;
-        totalShares = 1000;
-        dai.transferFrom(msg.sender, address(this), daiAmount);
-        yDai.transferFrom(msg.sender, address(this), yDaiAmount);
+        delete setup;
+        pools[address(t0)] = t0Amount;
+        pools[address(t1)] = t1Amount;
+
+        _invariant = t0Amount.mul(t1Amount);
+        _mint(msg.sender, 1000);
+
+        t0.transferFrom(msg.sender, address(this), t0Amount);
+        t1.transferFrom(msg.sender, address(this), t1Amount);
     }
 
-    function investLiquidity(uint256 daiAmount, uint256 minShares) external initialized {
+    /// @dev Mint liquidity tokens in exchange for adding liquidity
+    /// The parameter passed is the amount of `t0` being invested, an appropriate amount of `t1` to be invested alongside will be calculated and taken by this function from the caller.
+    function mint(uint256 t0Amount, uint256 minShares) external {
         require(
-            daiAmount > 0 && minShares > 0,
+            t0Amount > 0 && minShares > 0,
             "Market: InvestDai Parameters"
         );
-        uint256 daiPerShare = daiPool.div(totalShares);
+        address t0Address = address(t0);
+        address t1Address = address(t1);
+
+        uint256 t0PerShare = pools[t0Address].div(totalSupply());
         require(
-            daiAmount >= daiPerShare,
-            "Market: Not enough dai"
+            t0Amount >= t0PerShare,
+            "Market: Not enough t0 tokens"
         );
-        uint256 sharesPurchased = daiAmount.div(daiPerShare);
+        uint256 sharesPurchased = t0Amount.div(t0PerShare); // TODO: I'm just letting this revert if not initialized
         require(
             sharesPurchased >= minShares,
             "Market: Not enough shares"
         );
 
-        uint256 yDaiPerShare = yDaiPool.div(totalShares);
-        uint256 yDaiRequired = sharesPurchased.mul(yDaiPerShare);
-        shares[msg.sender] = shares[msg.sender].add(sharesPurchased);
-        totalShares = totalShares.add(sharesPurchased);
+        uint256 t1PerShare = pools[t1Address].div(totalSupply());
+        uint256 t1Required = sharesPurchased.mul(t1PerShare);
+        _mint(msg.sender, sharesPurchased);
 
-        uint256 daiUsed = sharesPurchased.mul(daiPerShare);
-        daiPool = daiPool.add(daiUsed);
-        yDaiPool = yDaiPool.add(yDaiRequired);
-        invariant = daiPool.mul(yDaiPool);
-        dai.transferFrom(msg.sender, address(this), daiUsed);
-        yDai.transferFrom(msg.sender, address(this), yDaiRequired);
+        uint256 t0Used = sharesPurchased.mul(t0PerShare);
+        pools[t0Address] = pools[t0Address].add(t0Used);
+        pools[t1Address] = pools[t1Address].add(t1Required);
+        _invariant = pools[t0Address].mul(pools[t1Address]);
+        t0.transferFrom(msg.sender, address(this), t0Used);
+        t1.transferFrom(msg.sender, address(this), t1Required);
         emit Investment(msg.sender, sharesPurchased);
     }
 
-    function daiToYDai(uint256 daiSold, uint256 minYDaiReceived, uint256 timeout) external {
+    /// @dev The `tInAddress` and `tOutAddress` can match `t0` and `t1` to sell `t0` tokens for `t1` tokens, or they can be reversed to sell `t1` for `t0`
+    function swap(address tInAddress, address tOutAddress, uint256 tInAmount, uint256 minTOutAmount, uint256 timeout) external {
         require(
-            daiSold > 0 && minYDaiReceived > 0 && now < timeout,
-            "Market: DaiToYDai Parameters"
+            now < timeout,
+            "Market: Timeout expired"
         );
-        _daiToYDai(daiSold, minYDaiReceived);
+        /* require(
+            tInAddress != tOutAddress && pools[tInAddress] != 0 && pools[tOutAddress] != 0,
+            revert("Market: Token parameters")
+        ); */ // TODO: Find out syntax issue
+
+        uint256 fee = tInAmount.div(FEE_RATE);
+        uint256 newTInPool = pools[tInAddress].add(tInAmount);
+        uint256 tempTInPool = newTInPool.sub(fee);
+        uint256 newTOutPool = reciprocalPool(tempTInPool);
+        uint256 tOutAmount = pools[tOutAddress].sub(newTOutPool);
+
+        require(
+            tOutAmount >= minTOutAmount,
+            "Market: Not enough TOut tokens"
+        );
+
+        pools[tInAddress] = newTInPool;
+        pools[tOutAddress] = newTOutPool;
+
+        _invariant = pools[tInAddress].mul(pools[tOutAddress]); // TODO: Why do we do this?
+        IERC20(tInAddress).transferFrom(msg.sender, address(this), tInAmount);
+        IERC20(tOutAddress).transfer(msg.sender, tOutAmount);
+
+        emit Swap(msg.sender, tInAddress, tOutAddress, tInAmount, tOutAmount);
     }
 
-    function yDaiToDai(uint256 yDaiSold, uint256 minDaiReceived, uint256 timeout) external {
-        require(
-            yDaiSold > 0 && minDaiReceived > 0 && now < timeout,
-            "Market: DaiToYDai Parameters"
-        );
-        _ydaiToDai(yDaiSold, minDaiReceived);
-    }
-
-    function _daiToYDai(uint256 daiIn, uint256 minYDaiOut) internal {
-        uint256 fee = daiIn.div(FEE_RATE);
-        uint256 newDaiPool = daiPool.add(daiIn);
-        uint256 tempDaiPool = newDaiPool.sub(fee);
-        uint256 newYDaiPool = invariant.div(tempDaiPool);
-        uint256 yDaiOut = yDaiPool.sub(newYDaiPool);
-
-        require(
-            yDaiOut >= minYDaiOut,
-            "Market: Not enough YDai"
-        );
-
-        daiPool = newDaiPool;
-        yDaiPool = newYDaiPool;
-        invariant = newYDaiPool.mul(newDaiPool); // TODO: Why do we do this?
-        dai.transferFrom(msg.sender, address(this), daiIn);
-        yDai.transfer(msg.sender, yDaiOut);
-
-        emit DaiToYDai(msg.sender, daiIn, yDaiOut);
-    }
-
-    function _yDaiToDai(uint256 yDaiIn, uint256 minDaiOut) internal {
-        uint256 fee = yDaiIn.div(FEE_RATE);
-        uint256 newYDaiPool = yDaiPool.add(yDaiIn);
-        uint256 tempYDaiPool = newYDaiPool.sub(fee);
-        uint256 newDaiPool = invariant.div(tempYDaiPool);
-        uint256 daiOut = daiPool.sub(newDaiPool);
-
-        require(
-            daiOut >= minDaiOut,
-            "Market: Not enough Dai"
-        );
-
-        yDaiPool = newYDaiPool;
-        daiPool = newDaiPool;
-        invariant = newDaiPool.mul(newYDaiPool); // TODO: Why do we do this?
-        yDai.transferFrom(msg.sender, address(this), yDaiIn);
-        dai.transfer(msg.sender, daiOut);
-
-        emit YDaiToDai(msg.sender, yDaiIn, daiOut);
-    }
-
-    // TODO: mapping(address => uint256) public pools;
-    function _swap(address t0, address t1, uint256 t0In, uint256 minT1Out) internal {
-        require(
-            t0 != t1 && pools[t0] != address(0) && pools[t1] != address(0),
-            revert("Market: Token parameters");
-        );
-
-        uint256 fee = t0In.div(FEE_RATE);
-        uint256 newT0Pool = pools[t0].add(t0In);
-        uint256 tempT0Pool = newT0Pool.sub(fee);
-        uint256 newT1Pool = invariant.div(tempT0Pool);
-        uint256 t1Out = t1Pool.sub(newT1Pool);
-
-        require(
-            t1Out >= minT1Out,
-            "Market: Not enough T1"
-        );
-
-        pools[t0] = newT0Pool;
-        pools[t1] = newT1Pool;
-
-        invariant = pools[t0].mul(pools[t1]); // TODO: Why do we do this?
-        IERC20(t0).transferFrom(msg.sender, address(this), t0In);
-        IERC20(t1).transfer(msg.sender, t1Out);
-
-        emit YDaiToDai(msg.sender, yDaiIn, daiOut);
+    /// @dev For the pool passed as an argument, returns the size of the reciprocal pool
+    function reciprocalPool(uint256 pool) public returns(uint256) {
+        return _invariant.div(pool);
     }
 }
