71fd7a8b9aa99f7c855072fbac9e5e4794f53006
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-07-08 15:53:14 +0100

    REviewed code for SafeMath and SafeCast

--- contracts/peripheral/Market.sol
+++ contracts/peripheral/Market.sol
@@ -31,12 +31,13 @@ contract Market is IMarket, ERC20, Delegable, Constants {
 
     State internal state;
 
+    // TODO: Choose liquidity token name
     constructor(address pot_, address chai_, address yDai_) public ERC20("Name", "Symbol") Delegable() {
         _pot = IPot(pot_);
         chai = IERC20(chai_);
         yDai = IYDai(yDai_);
 
-        maturity = uint128(yDai.maturity()); // TODO: Consider SafeCast
+        maturity = uint128(yDai.maturity()); // SafeCast might be needed if repurposing, but not right now.
     }
 
     /// @dev Mint initial liquidity tokens
@@ -66,6 +67,7 @@ contract Market is IMarket, ERC20, Delegable, Constants {
         yDai.transferFrom(msg.sender, address(this), yDaiRequired);
         _mint(msg.sender, tokensMinted);
 
+        // The operations below are bounded by what was transferred, so they can't overflow
         _updateState(chaiReserves + chaiOffered, yDaiReserves + yDaiRequired);
     }
 
@@ -81,6 +83,7 @@ contract Market is IMarket, ERC20, Delegable, Constants {
         chai.transfer(msg.sender, chaiReturned);
         yDai.transfer(msg.sender, yDaiReturned);
 
+        // The operations below are bounded by what can be transferred and can't overflow
         _updateState(chaiReserves - chaiReturned, yDaiReserves - yDaiReturned);
     }
 
@@ -92,19 +95,20 @@ contract Market is IMarket, ERC20, Delegable, Constants {
         external override
         onlyHolderOrDelegate(from, "Market: Only Holder Or Delegate")
     {
-        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27); // TODO: Conside SafeCast
-        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));  // TODO: Conside SafeCast
-        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));  // TODO: Conside SafeCast
+        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27); // If chi is above 2**64 we've got a bigger problem than overflow
+        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));  // Unlikely the market will hold 2**128 chai
+        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));  // Unlikely the market will hold 2**128 yDai
         uint256 yDaiOut = YieldMath.yDaiOutForChaiIn(
             chaiReserves, yDaiReserves,
             chaiIn,
-            uint128(maturity - now), k, c, g                            // TODO: Conside SafeCast
+            uint128(maturity - now), k, c, g                            // Won't overflow unless we allow maturities beyond the death of the universe
         );
 
         chai.transferFrom(from, address(this), chaiIn);
         yDai.transfer(to, yDaiOut);
 
-        _updateState(chaiReserves + chaiIn, yDaiReserves - yDaiOut);    // TODO: Conside SafeMath
+        // The operations below are bounded by what can be transferred and can't overflow
+        _updateState(chaiReserves + chaiIn, yDaiReserves - yDaiOut);
     }
 
     /// @dev Buy Chai for yDai
@@ -115,19 +119,20 @@ contract Market is IMarket, ERC20, Delegable, Constants {
         external override
         onlyHolderOrDelegate(from, "Market: Only Holder Or Delegate")
     {
-        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27); // TODO: Conside SafeCast
-        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));  // TODO: Conside SafeCast
-        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));  // TODO: Conside SafeCast
+        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27);
+        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));
+        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));
         uint256 yDaiIn = YieldMath.yDaiInForChaiOut(
             chaiReserves, yDaiReserves,
             chaiOut,
-            uint128(maturity - now), k, c, g                             // TODO: Conside SafeCast
+            uint128(maturity - now), k, c, g
         );
 
         yDai.transferFrom(from, address(this), yDaiIn);
         chai.transfer(to, chaiOut);
 
-        _updateState(chaiReserves - chaiOut, yDaiReserves + yDaiIn);    // TODO: Conside SafeMath
+        // The operations below are bounded by what can be transferred and can't overflow
+        _updateState(chaiReserves - chaiOut, yDaiReserves + yDaiIn);
     }
 
     /// @dev Sell yDai for Chai
@@ -138,19 +143,20 @@ contract Market is IMarket, ERC20, Delegable, Constants {
         external override
         onlyHolderOrDelegate(from, "Market: Only Holder Or Delegate")
     {
-        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27); // TODO: Conside SafeCast
-        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));  // TODO: Conside SafeCast
-        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));  // TODO: Conside SafeCast
+        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27);
+        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));
+        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));
         uint256 chaiOut = YieldMath.chaiOutForYDaiIn(
             chaiReserves, yDaiReserves,
             yDaiIn,
-            uint128(maturity - now), k, c, g                             // TODO: Conside SafeCast
+            uint128(maturity - now), k, c, g
         );
 
         yDai.transferFrom(from, address(this), yDaiIn);
         chai.transfer(to, chaiOut);
 
-        _updateState(chaiReserves - chaiOut, yDaiReserves + yDaiIn);    // TODO: Conside SafeMath
+        // The operations below are bounded by what can be transferred and can't overflow
+        _updateState(chaiReserves - chaiOut, yDaiReserves + yDaiIn);
     }
 
     /// @dev Buy yDai for chai
@@ -161,19 +167,20 @@ contract Market is IMarket, ERC20, Delegable, Constants {
         external override
         onlyHolderOrDelegate(from, "Market: Only Holder Or Delegate")
     {
-        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27); // TODO: Conside SafeCast
-        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));  // TODO: Conside SafeCast
-        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));  // TODO: Conside SafeCast
+        int128 c = int128((((now > _pot.rho()) ? _pot.drip() : _pot.chi()) << 64) / 10**27);
+        uint128 chaiReserves = uint128(chai.balanceOf(address(this)));
+        uint128 yDaiReserves = uint128(yDai.balanceOf(address(this)));
         uint256 chaiIn = YieldMath.chaiInForYDaiOut(
             chaiReserves, yDaiReserves,
             yDaiOut,
-            uint128(maturity - now), k, c, g                             // TODO: Conside SafeCast
+            uint128(maturity - now), k, c, g
         );
 
         chai.transferFrom(from, address(this), chaiIn);
         yDai.transfer(to, yDaiOut);
 
-        _updateState(chaiReserves + chaiIn, yDaiReserves - yDaiOut);    // TODO: Conside SafeMath
+        // The operations below are bounded by what can be transferred and can't overflow
+        _updateState(chaiReserves + chaiIn, yDaiReserves - yDaiOut);
     }
 
     /// @dev Maintain the price oracle
