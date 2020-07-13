e1b109ea9c79147155bcc086258064bd3dd8fad7
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-07-13 09:10:32 +0100

    Renamed shadowing Maker math functions

--- contracts/external/maker/jug.sol
+++ contracts/external/maker/jug.sol
@@ -64,7 +64,7 @@ contract Jug is LibNote {
       }
     }
     uint256 constant ONE = 10 ** 27;
-    function add(uint x, uint y) internal pure returns (uint z) {
+    function add_(uint x, uint y) internal pure returns (uint z) {
         z = x + y;
         require(z >= x);
     }
@@ -103,7 +103,7 @@ contract Jug is LibNote {
     function drip(bytes32 ilk) external note returns (uint rate) {
         require(now >= ilks[ilk].rho, "Jug/invalid-now");
         (, uint prev) = vat.ilks(ilk);
-        rate = rmul(rpow(add(base, ilks[ilk].duty), now - ilks[ilk].rho, ONE), prev);
+        rate = rmul(rpow(add_(base, ilks[ilk].duty), now - ilks[ilk].rho, ONE), prev);
         vat.fold(ilk, vow, diff(rate, prev));
         ilks[ilk].rho = now;
     }
