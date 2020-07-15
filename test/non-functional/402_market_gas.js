47396cab4a67ad4e1fc2154cb9b74b301621304b
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-07-15 18:30:34 +0100

    Fixed gas tests

--- test/non-functional/402_market_gas.js
+++ test/non-functional/402_market_gas.js
@@ -209,19 +209,6 @@ contract('Market', async (accounts) =>  {
             await market.init(daiReserves, yDaiReserves, { from: user1 });
         });
 
-        it("sells dai", async() => {
-            const tradeSize = toWad(1).div(1000);
-            await getDai(from, daiTokens1);
-
-            await market.addDelegate(operator, { from: from });
-            await dai.approve(market.address, tradeSize, { from: from });
-            await market.sellDai(from, to, tradeSize, { from: operator });
-
-            const yDaiOut = new BN(await yDai1.balanceOf(to));
-
-            results.add(['sellDai', daiReserves, yDaiReserves, tradeSize, yDaiOut]);
-        });
-
         it("buys dai", async() => {
             const tradeSize = toWad(1).div(1000);
             await yDai1.mint(from, yDaiTokens1.div(1000), { from: owner });
@@ -247,27 +234,49 @@ contract('Market', async (accounts) =>  {
             results.add(['sellYDai', daiReserves, yDaiReserves, tradeSize, daiOut]);
         });
 
-        it("buys yDai", async() => {
-            const tradeSize = toWad(1).div(1000);
-            await getDai(from, daiTokens1.div(1000));
+        describe("with extra yDai reserves", () => {
+            beforeEach(async() => {
+                const additionalYDaiReserves = toWad(34.4);
+                await yDai1.mint(operator, additionalYDaiReserves, { from: owner });
+                await yDai1.approve(market.address, additionalYDaiReserves, { from: operator });
+                await market.sellYDai(operator, operator, additionalYDaiReserves, { from: operator });
+            });
 
-            await market.addDelegate(operator, { from: from });
-            await dai.approve(market.address, daiTokens1.div(1000), { from: from });
-            await market.buyYDai(from, to, tradeSize, { from: operator });
+            it("sells dai", async() => {
+                const tradeSize = toWad(1).div(1000);
+                await getDai(from, daiTokens1);
+    
+                await market.addDelegate(operator, { from: from });
+                await dai.approve(market.address, tradeSize, { from: from });
+                await market.sellDai(from, to, tradeSize, { from: operator });
+    
+                const yDaiOut = new BN(await yDai1.balanceOf(to));
+    
+                results.add(['sellDai', daiReserves, yDaiReserves, tradeSize, yDaiOut]);
+            });
 
-            const daiIn = (new BN(daiTokens1.div(1000).toString())).sub(new BN(await dai.balanceOf(from)));
-            results.add(['buyYDai', daiReserves, yDaiReserves, daiIn, tradeSize]);
-        });
-
-        it("prints results", async() => {
-            for (line of results.values()) {
-                console.log("| " + 
-                    line[0].padEnd(10, ' ') + "· " +
-                    line[1].toString().padEnd(23, ' ') + "· " +
-                    line[2].toString().padEnd(23, ' ') + "· " +
-                    line[3].toString().padEnd(23, ' ') + "· " +
-                    line[4].toString().padEnd(23, ' ') + "|");
-            }
+            it("buys yDai", async() => {
+                const tradeSize = toWad(1).div(1000);
+                await getDai(from, daiTokens1.div(1000));
+    
+                await market.addDelegate(operator, { from: from });
+                await dai.approve(market.address, daiTokens1.div(1000), { from: from });
+                await market.buyYDai(from, to, tradeSize, { from: operator });
+    
+                const daiIn = (new BN(daiTokens1.div(1000).toString())).sub(new BN(await dai.balanceOf(from)));
+                results.add(['buyYDai', daiReserves, yDaiReserves, daiIn, tradeSize]);
+            });
+            
+            it("prints results", async() => {
+                for (line of results.values()) {
+                    console.log("| " + 
+                        line[0].padEnd(10, ' ') + "· " +
+                        line[1].toString().padEnd(23, ' ') + "· " +
+                        line[2].toString().padEnd(23, ' ') + "· " +
+                        line[3].toString().padEnd(23, ' ') + "· " +
+                        line[4].toString().padEnd(23, ' ') + "|");
+                }
+            });        
         });
     });
 });
