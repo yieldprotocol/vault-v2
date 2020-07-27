146b5b6e76bf35109504fe6226e55a8a022b8aef
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-07-27 16:22:29 +0100

    Fixed delegation and renamed

--- contracts/peripheral/DaiProxy.sol
+++ contracts/peripheral/DaiProxy.sol
@@ -5,15 +5,12 @@ import "../interfaces/IVat.sol";
 import "../interfaces/IPot.sol";
 import "../interfaces/IController.sol";
 import "../interfaces/IMarket.sol";
-import "../helpers/Delegable.sol";
 import "../helpers/DecimalMath.sol";
-import "@nomiclabs/buidler/console.sol";
 
 /**
  * @dev The ControllerDai is a proxy contract of Controller that allows users to immediately sell borrowed yDai for Dai, and to sell Dai at market rates to repay YDai debt.
- * Users can delegate the control of their accounts in Controllers to any address.
  */
-contract ControllerDai is Delegable(), DecimalMath {
+contract DaiProxy is DecimalMath {
 
     bytes32 public constant CHAI = "CHAI";
     bytes32 public constant WETH = "ETH-A";
@@ -46,59 +43,51 @@ contract ControllerDai is Delegable(), DecimalMath {
     }
 
     /// @dev Borrow yDai from Controller and sell it immediately for Dai, for a maximum yDai debt.
-    /// Must have approved the operator with `controller.addDelegate(controllerDai.address, { from: from })`.
-    /// `from` can delegate to other addresses to use his vault with this proxy, with `controllerDai.addDelegate(someone, { from: from })`.
+    /// Must have approved the operator with `controller.addDelegate(controllerDai.address)`.
     /// @param collateral Valid collateral type.
     /// @param maturity Maturity of an added series
-    /// @param from Yield vault to borrow yDai from.
     /// @param to Wallet to send the resulting Dai to.
     /// @param maximumYDai Maximum amount of YDai to borrow.
     /// @param daiToBorrow Exact amount of Dai that should be obtained.
     function borrowDaiForMaximumYDai(
         bytes32 collateral,
         uint256 maturity,
-        address from,
         address to,
         uint256 maximumYDai,
         uint256 daiToBorrow
     )
         public
-        onlyHolderOrDelegate(from, "Controller: Only Holder Or Delegate")
         returns (uint256)
     {
         uint256 yDaiToBorrow = _market.buyDaiPreview(toUint128(daiToBorrow));
         require (yDaiToBorrow <= maximumYDai);
 
         // The collateral for this borrow needs to have been posted beforehand
-        _controller.borrow(collateral, maturity, from, address(this), yDaiToBorrow);
+        _controller.borrow(collateral, maturity, msg.sender, address(this), yDaiToBorrow);
         _market.buyDai(address(this), to, toUint128(daiToBorrow));
 
         return requiredCollateral;
     }
 
     /// @dev Borrow yDai from Controller and sell it immediately for Dai, if a minimum amount of Dai can be obtained such.
-    /// Must have approved the operator with `controller.addDelegate(controllerDai.address, { from: from })`.
-    /// `from` can delegate to other addresses to use his vault with this proxy, with `controllerDai.addDelegate(someone, { from: from })`.
+    /// Must have approved the operator with `controller.addDelegate(controllerDai.address)`.
     /// @param collateral Valid collateral type.
     /// @param maturity Maturity of an added series
-    /// @param from Yield vault to lock collateral from.
     /// @param to Wallet to sent the resulting Dai to.
     /// @param yDaiToBorrow Amount of yDai to borrow.
     /// @param minimumDaiToBorrow Minimum amount of Dai that should be borrowed.
     function borrowMinimumDaiForCollateral(
         bytes32 collateral,
         uint256 maturity,
-        address from,
         address to,
         uint256 yDaiToBorrow,
         uint256 minimumDaiToBorrow
     )
         public
-        onlyHolderOrDelegate(from, "Controller: Only Holder Or Delegate")
         returns (uint256)
     {
         // The collateral for this borrow needs to have been posted beforehand
-        _controller.borrow(collateral, maturity, from, address(this), yDaiToBorrow);
+        _controller.borrow(collateral, maturity, msg.sender, address(this), yDaiToBorrow);
         uint256 boughtDai = _market.sellYDai(address(this), to, toUint128(yDaiToBorrow));
         require (boughtDai >= minimumDaiToBorrow);
 
@@ -106,57 +95,49 @@ contract ControllerDai is Delegable(), DecimalMath {
     }
 
     /// @dev Repay an amount of yDai debt in Controller using Dai exchanged for yDai at market rates, up to a maximum amount of Dai spent.
-    /// Must have approved the operator with `controller.addDelegate(controllerDai.address, { from: from })`.
-    /// `from` can delegate to other addresses to use his vault with this proxy, with `controllerDai.addDelegate(someone, { from: from })`.
+    /// Must have approved the operator with `controller.addDelegate(controllerDai.address)`.
     /// @param collateral Valid collateral type.
     /// @param maturity Maturity of an added series
-    /// @param from Wallet to take Dai from.
     /// @param to Yield Vault to repay yDai debt for.
     /// @param yDaiRepayment Amount of yDai debt to repay.
     /// @param maximumRepaymentInDai Maximum amount of Dai that should be spent on the repayment.
     function repayYDaiDebtForMaximumDai(
         bytes32 collateral,
         uint256 maturity,
-        address from,
         address to,
         uint256 yDaiRepayment,
         uint256 maximumRepaymentInDai
     )
         public
-        onlyHolderOrDelegate(from, "Controller: Only Holder Or Delegate")
         returns (uint256)
     {
-        uint256 repaymentInDai = _market.buyYDai(from, to, toUint128(yDaiRepayment));
+        uint256 repaymentInDai = _market.buyYDai(msg.sender, address(this), toUint128(yDaiRepayment));
         require (repaymentInDai <= maximumRepaymentInDai);
-        _controller.repayYDai(collateral, maturity, from, to, yDaiRepayment);
+        _controller.repayYDai(collateral, maturity, address(this), to, yDaiRepayment);
 
         return repaymentInDai;
     }
 
     /// @dev Repay an amount of yDai debt in Controller using a given amount of Dai exchanged for yDai at market rates, with a minimum of yDai debt required to be paid.
-    /// Must have approved the operator with `controller.addDelegate(controllerDai.address, { from: from })`.
-    /// `from` can delegate to other addresses to use his vault with this proxy, with `controllerDai.addDelegate(someone, { from: from })`.
+    /// Must have approved the operator with `controller.addDelegate(controllerDai.address)`.
     /// @param collateral Valid collateral type.
     /// @param maturity Maturity of an added series
-    /// @param from Wallet to take Dai from.
     /// @param to Yield Vault to repay yDai debt for.
     /// @param minimumYDaiRepayment Minimum amount of yDai debt to repay.
     /// @param repaymentInDai Exact amount of Dai that should be spent on the repayment.
     function repayMinimumYDaiDebtForDai(
         bytes32 collateral,
         uint256 maturity,
-        address from,
         address to,
         uint256 minimumYDaiRepayment,
         uint256 repaymentInDai
     )
         public
-        onlyHolderOrDelegate(from, "Controller: Only Holder Or Delegate")
         returns (uint256)
     {
-        uint256 yDaiRepayment = _market.sellDai(from, to, toUint128(repaymentInDai));
+        uint256 yDaiRepayment = _market.sellDai(msg.sender, address(this), toUint128(repaymentInDai));
         require (yDaiRepayment >= minimumYDaiRepayment);
-        _controller.repayYDai(collateral, maturity, from, to, yDaiRepayment);
+        _controller.repayYDai(collateral, maturity, address(this), to, yDaiRepayment);
 
         return yDaiRepayment;
     }
