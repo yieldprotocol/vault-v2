6992a35baa25d0dcfeafb996ac9a3a499debe0e3
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-07-27 08:10:48 +0100

    Fixed EthProxy conflicts

--- contracts/peripheral/EthProxy.sol
+++ contracts/peripheral/EthProxy.sol
@@ -29,36 +29,21 @@ contract EthProxy {
 
     /// @dev Users use `post` in EthProxy to post ETH to the Controller, which will be converted to Weth here.
     /// Users must have called `controller.addDelegate(ethProxy.address)` to authorize EthProxy to act in their behalf.
-<<<<<<< HEAD
     /// Use of EthProxy cannot be delegated further.
-    /// @param from Wallet to take eth from.
-    /// @param to Yield vault to put the collateral in.
     /// @param amount Amount of collateral to move.
-    function post(address from, address to, uint256 amount)
-        public payable onlyHolderOrDelegate(from, "EthProxy: Only Holder Or Delegate") {
-=======
     function post(uint256 amount)
         public payable {
->>>>>>> master
         _weth.deposit{ value: amount }();
         _controller.post(WETH, address(this), msg.sender, amount);
     }
 
     /// @dev Users wishing to withdraw their Weth as ETH from the Controller should use this function.
     /// Users must have called `controller.addDelegate(ethProxy.address)` to authorize EthProxy to act in their behalf.
-<<<<<<< HEAD
     /// Use of EthProxy cannot be delegated further.
-    /// @param from Yield vault to take weth from.
-    /// @param to Wallet to put the weth in.
     /// @param amount Amount of weth to move.
-    function withdraw(address from, address payable to, uint256 amount)
-        public onlyHolderOrDelegate(from, "EthProxy: Only Holder Or Delegate") {
-        _controller.withdraw(WETH, from, address(this), amount);
-=======
     function withdraw(uint256 amount)
-        public  {
+        public {
         _controller.withdraw(WETH, msg.sender, address(this), amount);
->>>>>>> master
         _weth.withdraw(amount);
         msg.sender.transfer(amount);
     }
