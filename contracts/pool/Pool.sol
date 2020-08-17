8dc4f419eec6a93f6f233e46956aa2d3229fc4d3
Georgios Konstantopoulos <me@gakonst.com>
2020-08-17 15:09:35 +0300

    fix(pool): fix stack too deep error by scoping

--- contracts/pool/Pool.sol
+++ contracts/pool/Pool.sol
@@ -105,7 +105,7 @@ contract Pool is IPool, ERC20, Delegable {
     /// @return The amount of liquidity tokens minted.
     function mint(address from, address to, uint256 daiOffered)
         external override
-        // onlyHolderOrDelegate(from, "Pool: Only Holder Or Delegate") <-- TODO: Fix here, stack too deep
+        onlyHolderOrDelegate(from, "Pool: Only Holder Or Delegate")
         returns (uint256)
     {
         uint256 supply = totalSupply();
@@ -131,15 +131,19 @@ contract Pool is IPool, ERC20, Delegable {
     /// @return The amount of reserve tokens returned (daiTokens, yDaiTokens).
     function burn(address from, address to, uint256 tokensBurned)
         external override
-        // onlyHolderOrDelegate(from, "Pool: Only Holder Or Delegate") <-- TODO: Fix here, stack too deep
+        onlyHolderOrDelegate(from, "Pool: Only Holder Or Delegate")
         returns (uint256, uint256)
     {
         uint256 supply = totalSupply();
         uint256 daiReserves = dai.balanceOf(address(this));
         // use the actual reserves rather than the virtual reserves
+        uint256 daiReturned;
+        uint256 yDaiReturned;
+        { // avoiding stack too deep
         uint256 yDaiReserves = yDai.balanceOf(address(this));
-        uint256 daiReturned = tokensBurned.mul(daiReserves).div(supply);
-        uint256 yDaiReturned = tokensBurned.mul(yDaiReserves).div(supply);
+        daiReturned = tokensBurned.mul(daiReserves).div(supply);
+        yDaiReturned = tokensBurned.mul(yDaiReserves).div(supply);
+        }
 
         _burn(from, tokensBurned);
         dai.transfer(to, daiReturned);
