b33451b3256d42c8d5366f643ff2386edfeff7bf
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-08-18 21:25:48 +0100

    Drafted Delegable.permit

--- contracts/helpers/Delegable.sol
+++ contracts/helpers/Delegable.sol
@@ -6,7 +6,29 @@ pragma solidity ^0.6.10;
 contract Delegable {
     event Delegate(address indexed user, address indexed delegate, bool enabled);
 
+    bytes32 public DOMAIN_SEPARATOR;
+    // keccak256("Permit(address user,address delegate,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x0000000000000000000000000000000;
+    mapping(address => uint) public nonces;
+
     mapping(address => mapping(address => bool)) public delegated;
+
+    constructor () public {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+                keccak256(bytes('Yield')), // Can we get the name of the inheriting contract somehow?
+                keccak256(bytes('1')),
+                chainId,
+                address(this)
+            )
+        );
+    }
 
     /// @dev Require that msg.sender is the account holder or a delegate
     modifier onlyHolderOrDelegate(address holder, string memory errorMessage) {
@@ -29,5 +51,20 @@ contract Delegable {
         require(delegated[msg.sender][delegate], "Delegable: Already undelegated");
         delegated[msg.sender][delegate] = false;
         emit Delegate(msg.sender, delegate, false);
+    }
+
+    /// @dev Add a delegate through a permit
+    function permit(address user, address delegate, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+        require(deadline >= block.timestamp, 'Yield: EXPIRED');
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                '\x19\x01',
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH, user, delegate, nonces[user]++, deadline))
+            )
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == user, 'Yield: INVALID_SIGNATURE');
+        delegated[user][delegate] = true;
     }
 }
