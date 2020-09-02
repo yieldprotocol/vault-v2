74a4239382f5929ee9bf31bce32095be409c1f6d
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-02 18:00:48 +0100

    Tested limits

--- test/peripheral/551_liquidityProxy.ts
+++ test/peripheral/551_liquidityProxy.ts
@@ -224,6 +224,33 @@ contract('YieldProxy - LiquidityProxy', async (accounts) => {
         expect(await pool.balanceOf(proxy.address)).to.be.bignumber.lt(new BN('10'))
       })
 
+      it('doesn\'t remove liquidity if minimum prices not achieved', async () => {
+        const poolTokens = await pool.balanceOf(user2)
+        const debt = await controller.debtYDai(CHAI, maturity1, user2)
+        const daiBalance = await dai.balanceOf(user2)
+
+        // Has pool tokens
+        expect(poolTokens).to.be.bignumber.gt(new BN('0'))
+        // Has yDai debt
+        expect(debt).to.be.bignumber.gt(new BN('0'))
+        // Doesn't have dai
+        expect(daiBalance).to.be.bignumber.eq(new BN('0'))
+        // Doesn't have yDai
+        expect(await yDai1.balanceOf(user2)).to.be.bignumber.eq(new BN('0'))
+
+        // the proxy must be a delegate in the pool because in order to remove
+        // liquidity via the proxy we must authorize the proxy to burn from our balance
+        await pool.addDelegate(proxy.address, { from: user2 })
+        await expectRevert(
+          proxy.removeLiquidityEarly(pool.address, poolTokens, toRay(2), '0', { from: user2 }),
+          "YieldProxy: minimumDaiPrice not reached",
+        )
+        await expectRevert(
+          proxy.removeLiquidityEarly(pool.address, poolTokens, '0', toRay(2), { from: user2 }),
+          "YieldProxy: minimumYDaiPrice not reached",
+        )
+      })
+
       it('removes liquidity after maturity by redeeming', async () => {
         await helper.advanceTime(31556952)
         await helper.advanceBlock()
