c53a2d3e7ca9b9fc447c254dfdcdb0de053847d2
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-02 17:15:18 +0100

    Use redeemed dai to repay debt

--- contracts/interfaces/IYDai.sol
+++ contracts/interfaces/IYDai.sol
@@ -16,7 +16,7 @@ interface IYDai is IERC20, IERC2612 {
     function mint(address, uint) external;
     function burn(address, uint) external;
     function flashMint(address, uint, bytes calldata) external;
-    function redeem(address, address, uint256) external;
+    function redeem(address, address, uint256) external returns (uint256);
     // function transfer(address, uint) external returns (bool);
     // function transferFrom(address, address, uint) external returns (bool);
     // function approve(address, uint) external returns (bool);
