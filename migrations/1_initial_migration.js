67d248b4fc45f3483aa879d3694a313fbd6a0cd3
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-16 12:32:25 +0100

    fix: Deploy when a git commit is not found

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -5,7 +5,12 @@ const fs = require('fs')
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
-  const branch = fs.readFileSync(".git/HEAD").toString().replace('ref: ','').trim() 
-  const commit = fs.readFileSync(`.git/${branch}`).toString().trim()
+  let commit = "Unknown"
+  try{
+    const branch = fs.readFileSync(".git/HEAD").toString().replace('ref: ','').trim() 
+    commit = fs.readFileSync(`.git/${branch}`).toString().trim()  
+  } catch (error) {
+    console.log("Commit not found")
+  }
   deployer.deploy(Migrations, commit);
 };
