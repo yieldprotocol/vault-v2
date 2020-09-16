104d7dfb70b20fcc9889991b50342633e6119724
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-16 12:59:27 +0100

    network: fork mainnet in ganache and migrate contracts to it

---
+++ scripts/mainnet-ganache.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# move to subfolder
+# cd scripts
+
+# create db directory
+# [ ! -d "./db_ganache" ] && mkdir db_ganache
+
+# start ganache
+npx ganache-cli \
+    --mnemonic "all your mnemonic are belong to us seed me up scotty over" \
+    --defaultBalanceEther 1000000 \
+    --gasLimit 0xfffffffffff \
+    --gasPrice 0 \
+    --port 8545 \
+    --networkId 1 \
+    --host 0.0.0.0 \
+    --fork https://mainnet.infura.io/v3/`cat .infuraKey` &
