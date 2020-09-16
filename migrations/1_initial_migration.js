43013596a699fa15172308f95478169451d336af
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-16 12:21:12 +0100

    feat: Store commit identifier in `Migrations.version`

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,11 @@
+const { callbackify } = require('util');
+
+const fs = require('fs')
+
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  const branch = fs.readFileSync(".git/HEAD").toString().replace('ref: ','').trim() 
+  const commit = fs.readFileSync(`.git/${branch}`).toString().trim()
+  deployer.deploy(Migrations, commit);
 };
