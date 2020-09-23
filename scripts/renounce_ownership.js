417ed8b5fff0c1ec41f86f1c03ec0e9e8cc3edc2
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-23 10:59:15 +0100

    fix: Ownable has a renounceOwnership() function

--- scripts/renounce_ownership.js
+++ scripts/renounce_ownership.js
@@ -7,26 +7,26 @@ const EDai = artifacts.require('EDai')
 
 module.exports = async (deployer, network) => {
   const migrations = await Migrations.deployed()
-  await migrations.transferOwnership('0x000000000000000000000000000000000000dead')
+  await migrations.renounceOwnership()
 
   const treasury = await Treasury.deployed()
-  await treasury.transferOwnership('0x000000000000000000000000000000000000dead')
+  await treasury.renounceOwnership()
 
   const controller = await Controller.deployed()
-  await controller.transferOwnership('0x000000000000000000000000000000000000dead')
+  await controller.renounceOwnership()
 
   const liquidations = await Liquidations.deployed()
-  await liquidations.transferOwnership('0x000000000000000000000000000000000000dead')
+  await liquidations.renounceOwnership()
 
   const unwind = await Unwind.deployed()
-  await unwind.transferOwnership('0x000000000000000000000000000000000000dead')
+  await unwind.renounceOwnership()
   
   const eDais = []
   for (let i = 0; i < (await migrations.length()); i++) {
     const contractName = web3.utils.toAscii(await migrations.names(i))
     if (contractName.includes('eDai') && !contractName.includes('LP')) {
       const eDai = await EDai.at(await migrations.contracts(web3.utils.fromAscii(contractName)))
-      await eDai.transferOwnership('0x000000000000000000000000000000000000dead')
+      await eDai.renounceOwnership()
     }
   }
 }
