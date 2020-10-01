c2779731d39e171e45b6cacf1b6573ad0fa71963
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-10-01 11:11:40 +0100

    chore: imports maybe work

--- migrations/7_setup_pools.js
+++ migrations/7_setup_pools.js
@@ -1,4 +1,20 @@
-const utils = require("../test/utils")
+const ethers = require('ethers')
+const BigNumber = ethers.BigNumber
+
+const toWad = (value) => {
+  let exponent = BigNumber.from(10).pow(BigNumber.from(8))
+  return BigNumber.from((value) * 10 ** 10).mul(exponent)
+}
+
+const RAY = BigNumber.from(10).pow(BigNumber.from(27))
+
+const mulRay = (x, ray) => {
+  return BigNumber.from(x).mul(BigNumber.from(ray)).div(RAY)
+}
+
+const divRay = (x, ray) => {
+  return RAY.mul(BigNumber.from(x)).div(BigNumber.from(ray))
+}
 
 const Migrations = artifacts.require('Migrations')
 
@@ -11,10 +27,10 @@ const Treasury = artifacts.require('Treasury')
 const Controller = artifacts.require('Controller')
 const Pool = artifacts.require('Pool')
 
-const daiReserves = utils.toWad(10000)
+const daiReserves = toWad(10000)
 const targetRate = 1.05
 const YEAR = 60*60*24*365
-const ETH_A = web3.utils.toAscii("ETH-A")
+const ETH_A = web3.utils.fromAscii("ETH-A")
 const MAX = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
 
 module.exports = async (deployer, network) => {
@@ -34,11 +50,11 @@ module.exports = async (deployer, network) => {
     const secsToMaturity = maturity - fromDate
     const propOfYear = secsToMaturity/YEAR
     const price = Math.pow(rate, propOfYear)
-    return daiReserves.mul(new BN(price)).sub(daiReserves)
+    return daiReserves.mul(BigNumber.from(price)).sub(daiReserves)
   };
 
   const pools = {}
-  let totalEDai = new BN('0')
+  let totalEDai = BigNumber.from(0)
   for (let i = 0; i < (await migrations.length()); i++) {
     const contractName = web3.utils.toAscii(await migrations.names(i))
     if (!contractName.includes('eDaiLP')) continue
@@ -46,7 +62,7 @@ module.exports = async (deployer, network) => {
     const _eDai = await EDai.at(await pool.eDai())
     const _maturity = await eDai.maturity()
     const _eDaiToSell = eDaiToSell(_maturity, targetRate, daiReserves)
-    totalEDai += _eDaiToSell
+    totalEDai = totalEDai.add(_eDaiToSell)
 
     pools[contractName] = { 
       pool: _pool,
@@ -58,12 +74,12 @@ module.exports = async (deployer, network) => {
   const rate = await vat.ilks(ETH_A).rate
   const spot = await vat.ilks(ETH_A).spot
   const totalDai = daiReserves.mul(pools.length())
-  const normalizedDai = utils.divRay(totalDai, rate)
-  const wethForDai = utils.mulRay(totalDai, spot)
-  const wethForEDai = utils.mulRay(totalEDai, spot)
+  const normalizedDai = divRay(totalDai, rate)
+  const wethForDai = mulRay(totalDai, spot)
+  const wethForEDai = mulRay(totalEDai, spot)
 
   // Initialize pools
-  await weth.deposit({ value: utils.addBN(wethForDai, wethForEDai) }) // Work back how much we need for daiReserves * number of pools
+  await weth.deposit({ value: wethForDai.add(wethForEDai) }) // Work back how much we need for daiReserves * number of pools
 
   // Get Dai
   await weth.approve(wethJoin.address, MAX)  
