8cbf8dffc22ae65b763a9199f0015905c9e92ba3
Georgios Konstantopoulos <me@gakonst.com>
2020-10-14 21:16:22 +0300

    fix: remove maker proxy integration (#368)

--- contracts/peripheral/YieldProxy.sol
+++ contracts/peripheral/YieldProxy.sol
@@ -11,7 +11,6 @@ import "../interfaces/IPot.sol";
 import "../interfaces/IPool.sol";
 import "../interfaces/IFYDai.sol";
 import "../interfaces/IChai.sol";
-import "../interfaces/IFlashMinter.sol";
 import "../helpers/DecimalMath.sol";
 
 
@@ -35,7 +34,7 @@ library SafeCast {
     }
 }
 
-contract YieldProxy is DecimalMath, IFlashMinter {
+contract YieldProxy is DecimalMath {
     using SafeCast for uint256;
 
     IVat public vat;
@@ -517,174 +516,6 @@ contract YieldProxy is DecimalMath, IFlashMinter {
         (bytes32 r, bytes32 s, uint8 v) = unpack(signature);
         dai.permit(msg.sender, address(treasury), dai.nonces(msg.sender), uint(-1), true, v, r, s);
         controller.repayDai(collateral, maturity, msg.sender, to, daiAmount);
-    }
-
-
-    // YieldProxy: Maker to Yield proxy
-
-    /// @dev Transfer debt and collateral from MakerDAO to Yield
-    /// Needs vat.hope(splitter.address, { from: user });
-    /// Needs controller.addDelegate(splitter.address, { from: user });
-    /// @param pool The pool to trade in (and therefore fyDai series to borrow)
-    /// @param wethAmount weth to move from MakerDAO to Yield. Needs to be high enough to collateralize the dai debt in Yield,
-    /// and low enough to make sure that debt left in MakerDAO is also collateralized.
-    /// @param daiAmount dai debt to move from MakerDAO to Yield. Denominated in Dai (= art * rate)
-    function makerToYield(IPool pool, uint256 wethAmount, uint256 daiAmount) public {
-        onlyKnownPool(pool);
-        // The user specifies the fyDai he wants to mint to cover his maker debt, the weth to be passed on as collateral, and the dai debt to move
-        (uint256 ink, uint256 art) = vat.urns(WETH, msg.sender);
-        (, uint256 rate,,,) = vat.ilks("ETH-A");
-        require(
-            daiAmount <= muld(art, rate),
-            "YieldProxy: Not enough debt in Maker"
-        );
-        require(
-            wethAmount <= ink,
-            "YieldProxy: Not enough collateral in Maker"
-        );
-        // Flash mint the fyDai
-        IFYDai fyDai = pool.fyDai();
-        fyDai.flashMint(
-            fyDaiForDai(pool, daiAmount),
-            abi.encode(MTY, pool, msg.sender, wethAmount, daiAmount)
-        );
-    }
-
-    /// @dev Transfer debt and collateral from Yield to MakerDAO
-    /// Needs vat.hope(splitter.address, { from: user });
-    /// Needs controller.addDelegate(splitter.address, { from: user });
-    /// @param pool The pool to trade in (and therefore fyDai series to migrate)
-    /// @param wethAmount weth to move from Yield to MakerDAO. Needs to be high enough to collateralize the dai debt in MakerDAO,
-    /// and low enough to make sure that debt left in Yield is also collateralized.
-    /// @param fyDaiAmount fyDai debt to move from Yield to MakerDAO.
-    function yieldToMaker(IPool pool, uint256 wethAmount, uint256 fyDaiAmount) public {
-        onlyKnownPool(pool);
-        IFYDai fyDai = pool.fyDai();
-
-        // The user specifies the fyDai he wants to move, and the weth to be passed on as collateral
-        require(
-            fyDaiAmount <= controller.debtFYDai(WETH, fyDai.maturity(), msg.sender),
-            "YieldProxy: Not enough debt in Yield"
-        );
-        require(
-            wethAmount <= controller.posted(WETH, msg.sender),
-            "YieldProxy: Not enough collateral in Yield"
-        );
-        // Flash mint the fyDai
-        fyDai.flashMint(
-            fyDaiAmount,
-            abi.encode(YTM, pool, msg.sender, wethAmount, 0)
-        ); // The daiAmount encoded is ignored
-    }
-
-    /// @dev Callback from `FYDai.flashMint()`
-    function executeOnFlashMint(uint256 fyDaiAmount, bytes calldata data) external override {
-        (bool direction, IPool pool, address user, uint256 wethAmount, uint256 daiAmount) = 
-            abi.decode(data, (bool, IPool, address, uint256, uint256));
-        require(msg.sender == address(IPool(pool).fyDai()), "YieldProxy: Restricted callback");
-        onlyKnownPool(pool);
-
-        if(direction == MTY) _makerToYield(pool, user, wethAmount, daiAmount);
-        if(direction == YTM) _yieldToMaker(pool, user, wethAmount, fyDaiAmount);
-    }
-
-    /// @dev Minimum weth needed to collateralize an amount of dai in MakerDAO
-    function wethForDai(uint256 daiAmount) public view returns (uint256) {
-        (,, uint256 spot,,) = vat.ilks("ETH-A");
-        return divd(daiAmount, spot);
-    }
-
-    /// @dev Minimum weth needed to collateralize an amount of fyDai in Yield. Yes, it's the same formula.
-    function wethForFYDai(uint256 fyDaiAmount) public view returns (uint256) {
-        (,, uint256 spot,,) = vat.ilks("ETH-A");
-        return divd(fyDaiAmount, spot);
-    }
-
-    /// @dev Amount of fyDai debt that will result from migrating Dai debt from MakerDAO to Yield
-    function fyDaiForDai(IPool pool, uint256 daiAmount) public view returns (uint256) {
-        onlyKnownPool(pool);
-        return pool.buyDaiPreview(daiAmount.toUint128());
-    }
-
-    /// @dev Amount of dai debt that will result from migrating fyDai debt from Yield to MakerDAO
-    function daiForFYDai(IPool pool, uint256 fyDaiAmount) public view returns (uint256) {
-        onlyKnownPool(pool);
-        return pool.buyFYDaiPreview(fyDaiAmount.toUint128());
-    }
-
-    /// @dev Internal function to transfer debt and collateral from MakerDAO to Yield
-    /// @param pool The pool to trade in (and therefore fyDai series to borrow)
-    /// @param user Vault to migrate.
-    /// @param wethAmount weth to move from MakerDAO to Yield. Needs to be high enough to collateralize the dai debt in Yield,
-    /// and low enough to make sure that debt left in MakerDAO is also collateralized.
-    /// @param daiAmount dai debt to move from MakerDAO to Yield. Denominated in Dai (= art * rate)
-    /// Needs vat.hope(splitter.address, { from: user });
-    /// Needs controller.addDelegate(splitter.address, { from: user });
-    function _makerToYield(IPool pool, address user, uint256 wethAmount, uint256 daiAmount) internal {
-        onlyKnownPool(pool);
-        IFYDai fyDai = IFYDai(pool.fyDai());
-
-        // Pool should take exactly all fyDai flash minted. YieldProxy will hold the dai temporarily
-        uint256 fyDaiSold = pool.buyDai(address(this), address(this), daiAmount.toUint128());
-
-        daiJoin.join(user, daiAmount);      // Put the Dai in Maker
-        (, uint256 rate,,,) = vat.ilks("ETH-A");
-        vat.frob(                           // Pay the debt and unlock collateral in Maker
-            "ETH-A",
-            user,
-            user,
-            user,
-            -wethAmount.toInt256(),               // Removing Weth collateral
-            -divdrup(daiAmount, rate).toInt256()  // Removing Dai debt
-        );
-
-        vat.flux("ETH-A", user, address(this), wethAmount);             // Remove the collateral from Maker
-        wethJoin.exit(address(this), wethAmount);                       // Hold the weth in YieldProxy
-        controller.post(WETH, address(this), user, wethAmount);         // Add the collateral to Yield
-        controller.borrow(WETH, fyDai.maturity(), user, address(this), fyDaiSold); // Borrow the fyDai
-    }
-
-
-    /// @dev Internal function to transfer debt and collateral from Yield to MakerDAO
-    /// Needs vat.hope(splitter.address, { from: user });
-    /// Needs controller.addDelegate(splitter.address, { from: user });
-    /// @param pool The pool to trade in (and therefore fyDai series to migrate)
-    /// @param user Vault to migrate.
-    /// @param wethAmount weth to move from Yield to MakerDAO. Needs to be high enough to collateralize the dai debt in MakerDAO,
-    /// and low enough to make sure that debt left in Yield is also collateralized.
-    /// @param fyDaiAmount fyDai debt to move from Yield to MakerDAO.
-    function _yieldToMaker(IPool pool, address user, uint256 wethAmount, uint256 fyDaiAmount) internal {
-        onlyKnownPool(pool);
-        IFYDai fyDai = IFYDai(pool.fyDai());
-
-        // Pay the Yield debt - YieldProxy pays FYDai to remove the debt of `user`
-        // Controller should take exactly all fyDai flash minted.
-        controller.repayFYDai(WETH, fyDai.maturity(), address(this), user, fyDaiAmount);
-
-        // Withdraw the collateral from Yield, YieldProxy will hold it
-        controller.withdraw(WETH, user, address(this), wethAmount);
-
-        // Post the collateral to Maker, in the `user` vault
-        wethJoin.join(user, wethAmount);
-
-        // We are going to need to buy the FYDai back with Dai borrowed from Maker
-        uint256 daiAmount = pool.buyFYDaiPreview(fyDaiAmount.toUint128());
-
-        // Borrow the Dai from Maker
-        (, uint256 rate,,,) = vat.ilks("ETH-A"); // Retrieve the MakerDAO stability fee for Weth
-        vat.frob(
-            "ETH-A",
-            user,
-            user,
-            user,
-            wethAmount.toInt256(),                   // Adding Weth collateral
-            divdrup(daiAmount, rate).toInt256()      // Adding Dai debt
-        );
-        vat.move(user, address(this), daiAmount.mul(UNIT)); // Transfer the Dai to YieldProxy within MakerDAO, in RAD
-        daiJoin.exit(address(this), daiAmount);             // YieldProxy will hold the dai temporarily
-
-        // Sell the Dai for FYDai at Pool - It should make up for what was taken with repayYdai
-        pool.buyFYDai(address(this), address(this), fyDaiAmount.toUint128());
     }
 
     function onlyKnownPool(IPool pool) private view {
