89bfdf4244715f73a76bf434a2a8ed8775142f5e
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-10-19 11:54:51 +0100

    fix: Last time my contracts inherit interfaces

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -245,7 +245,7 @@ contract Controller is IController, Orchestrated(), Delegable(), DecimalMath {
     /// @param collateral Valid collateral type.
     /// @param user Address of the user vault.
     function locked(bytes32 collateral, address user)
-        public view override
+        public view
         validCollateral(collateral)
         returns (uint256)
     {
--- contracts/peripheral/YieldProxy.sol
+++ contracts/peripheral/YieldProxy.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.6.10;
 
-import "../interfaces/IController.sol";
 import "../interfaces/IWeth.sol";
 import "../interfaces/IDai.sol";
 import "../interfaces/IGemJoin.sol";
@@ -11,8 +10,33 @@ import "../interfaces/IPot.sol";
 import "../interfaces/IPool.sol";
 import "../interfaces/IFYDai.sol";
 import "../interfaces/IChai.sol";
+import "../interfaces/IDelegable.sol";
+import "../interfaces/ITreasury.sol";
 import "../helpers/DecimalMath.sol";
 
+
+interface IController is IDelegable {
+    function treasury() external view returns (ITreasury);
+    function series(uint256) external view returns (IFYDai);
+    function seriesIterator(uint256) external view returns (uint256);
+    function totalSeries() external view returns (uint256);
+    function containsSeries(uint256) external view returns (bool);
+    function posted(bytes32, address) external view returns (uint256);
+    function locked(bytes32, address) external view returns (uint256);
+    function debtFYDai(bytes32, uint256, address) external view returns (uint256);
+    function debtDai(bytes32, uint256, address) external view returns (uint256);
+    function totalDebtDai(bytes32, address) external view returns (uint256);
+    function isCollateralized(bytes32, address) external view returns (bool);
+    function inDai(bytes32, uint256, uint256) external view returns (uint256);
+    function inFYDai(bytes32, uint256, uint256) external view returns (uint256);
+    function erase(bytes32, address) external returns (uint256, uint256);
+    function shutdown() external;
+    function post(bytes32, address, address, uint256) external;
+    function withdraw(bytes32, address, address, uint256) external;
+    function borrow(bytes32, uint256, address, address, uint256) external;
+    function repayFYDai(bytes32, uint256, address, address, uint256) external returns (uint256);
+    function repayDai(bytes32, uint256, address, address, uint256) external returns (uint256);
+}
 
 library SafeCast {
     /// @dev Safe casting from uint256 to uint128
