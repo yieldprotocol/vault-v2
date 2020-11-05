df781bf8f814b164f5fe20055d52c33cecc567f0
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-11-05 15:07:15 +0000

    network: DSProxyFactory and ProxyRegistry

--- migrations/6_deploy_peripheral.js
+++ migrations/6_deploy_peripheral.js
@@ -6,19 +6,27 @@ const Treasury = artifacts.require('Treasury')
 const Controller = artifacts.require('Controller')
 const BorrowProxy = artifacts.require('BorrowProxy')
 const PoolProxy = artifacts.require('PoolProxy')
+const DSProxyFactory = artifacts.require('DSProxyFactory')
+const DSProxyRegistry = artifacts.require('ProxyRegistry')
 
 module.exports = async (deployer, network) => {
   const migrations = await Migrations.deployed()
 
-  let daiAddress, chaiAddress
+  let daiAddress, chaiAddress, proxyFactoryAddress, proxyRegistryAddress
   if (network === 'mainnet' || network === 'mainnet-ganache') {
     wethAddress = fixed_addrs[network].wethAddress
     daiAddress = fixed_addrs[network].daiAddress
     chaiAddress = fixed_addrs[network].chaiAddress
+    proxyFactoryAddress = fixed_addrs[network].proxyFactoryAddress
+    proxyRegistryAddress = fixed_addrs[network].proxyRegistryAddress
   } else {
     wethAddress = (await Weth.deployed()).address
     daiAddress = (await Dai.deployed()).address
     chaiAddress = (await Chai.deployed()).address
+
+    // Setup DSProxyFactory and DSProxyRegistry
+    proxyFactoryAddress = (await deployer.deploy(DSProxyFactory)).address
+    proxyRegistryAddress = (await deployer.deploy(DSProxyRegistry, proxyFactoryAddress)).address
   }
   const treasuryAddress = (await Treasury.deployed()).address
   const controllerAddress = (await Controller.deployed()).address
@@ -30,6 +38,8 @@ module.exports = async (deployer, network) => {
   const poolProxy = await PoolProxy.deployed()
 
   const deployment = {
+    ProxyFactory: proxyFactoryAddress,
+    ProxyRegistry: proxyRegistryAddress,
     BorrowProxy: borrowProxy.address,
     PoolProxy: poolProxy.address,
   }
