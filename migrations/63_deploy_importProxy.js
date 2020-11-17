414e34594c4f8468b2e3ebdb11bf76d3f99a3316
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-11-17 15:02:25 +0000

    network: deployment scripts

--- migrations/63_deploy_importProxy.js
+++ migrations/63_deploy_importProxy.js
@@ -9,32 +9,41 @@ const ImportProxy = artifacts.require('ImportProxy')
 
 module.exports = async (deployer, network) => {
 
-  let controllerAddress, proxyFactoryAddress, proxyRegistryAddress
+  let controllerAddress, proxyFactoryAddress, proxyRegistryAddress, migrations
+  let poolAddresses = []
   if (network === 'development') {
+    migrations = await Migrations.deployed()
     controllerAddress = (await Controller.deployed()).address
     proxyFactoryAddress = (await DSProxyFactory.deployed()).address
     proxyRegistryAddress = (await DSProxyRegistry.deployed()).address
+    for (let i = 0; i < (await migrations.length()); i++) {
+      const contractName = web3.utils.toAscii(await migrations.names(i))
+      if (!contractName.includes('fyDaiLP')) continue
+      poolAddresses.push(await migrations.contracts(web3.utils.fromAscii(contractName)))
+    }
   } else {
+    migrations = await Migrations.at(fixed_addrs[network].migrationsAddress)
     controllerAddress = fixed_addrs[network].controllerAddress
     proxyFactoryAddress = fixed_addrs[network].proxyFactoryAddress
     proxyRegistryAddress = fixed_addrs[network].proxyRegistryAddress  
+    poolAddresses = [
+      fixed_addrs[network].fyDaiLP20OctAddress,
+      fixed_addrs[network].fyDaiLP20DecAddress,
+      fixed_addrs[network].fyDaiLP21MarAddress,
+      fixed_addrs[network].fyDaiLP21JunAddress,
+      fixed_addrs[network].fyDaiLP21SepAddress,
+      fixed_addrs[network].fyDaiLP21DecAddress,
+    ]
   }
 
-  await deployer.deploy(ImportProxy, controllerAddress)
+  await deployer.deploy(ImportProxy, controllerAddress, poolAddresses, proxyRegistryAddress)
   const importProxy = await ImportProxy.deployed()
 
   const deployment = {
     ImportProxy: importProxy.address,
   }
 
-  let migrations
-  if (network === 'kovan' && network === 'kovan-fork') {
-    migrations = await Migrations.at(fixed_addrs[network].migrationsAddress)
-  } else if (network === 'development') {
-    migrations = await Migrations.deployed()
-  }
-
-  if (migrations !== undefined) {
+  if (migrations !== undefined && network !== 'mainnet') {
     for (name in deployment) {
       await migrations.register(web3.utils.fromAscii(name), deployment[name])
     }
