4a0781b99e3bc2d2a4699aeb4ebbf8edfee189de
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-02-09 17:08:41 +0000

    draft: accounting and liquidations

--- contracts/abi.sol
+++ contracts/abi.sol
@@ -24,13 +24,13 @@ contract YieldVat {
 
     // ---- Vault composition----
     // An user can own one or more Vaults, each one with a bytes12 identifier so that we can pack a singly linked list and a reverse search in a bytes32
-    mapping (address => bytes12)                    vaultFirst              // Each user points to the list head. We have 20 bytes here that we can still use.
-    mapping (bytes12 => bytes32)                    vaultNext               // With a vault identifier we can get both the owner and the next in the list. When giving a vault both are changed with 1 SSTORE.
+    mapping (address => bytes12)                    first              // Each user points to the list head. We have 20 bytes here that we can still use.
+    mapping (bytes12 => bytes32)                    next               // With a vault identifier we can get both the owner and the next in the list. When giving a vault both are changed with 1 SSTORE.
  
-    mapping (bytes12 => bytes32)                    vaultSeries             // Each vault is related to only one series, which also determines the underlying. If there is any other data that is set up on initialization, we can pack it with the series.
-    mapping (bytes12 => bytes32)                    vaultCollaterals        // Collaterals are identified by just 6 bytes, then in 32 bytes (one SSTORE) we can have an array of 5 collateral types to allow multi-collateral vaults. 
-    mapping (bytes12 => mapping(bytes6 => uint128)) vaultHoldings           // The collateral held in a vault can be on a uint128 for each type. With packing we can use only one SSTORE to modify both the debt and the first collateral balance.
-    mapping (bytes12 => uint128)                    vaultDebt
+    mapping (bytes12 => bytes32)                    series             // Each vault is related to only one series, which also determines the underlying. If there is any other data that is set up on initialization, we can pack it with the series.
+    mapping (bytes12 => bytes32)                    collaterals        // Collaterals are identified by just 6 bytes, then in 32 bytes (one SSTORE) we can have an array of 5 collateral types to allow multi-collateral vaults. 
+    mapping (bytes12 => mapping(bytes6 => uint128)) assets             // The collateral held in a vault can be on a uint128 for each type. With packing we can use only one SSTORE to modify both the debt and the first collateral balance.
+    mapping (bytes12 => uint128)                    debt
 
     // ---- Vault management ----
     // Create a new vault, linked to a series (and therefore underlying) and up to 6 collateral types
@@ -69,18 +69,25 @@ contract YieldVat {
     // Repay vault debt using underlying token, pulled from user. Collaterals are pushed to user. 
     function close(bytes12 vault, bytes32 collaterals, uint128[] memory inks, uint128 art) // Same cost as `frob`
 
+    // ---- Accounting ----
+
+    // Return the vault debt in underlying terms
+    function level(bytes12 vault) view returns (int128 uart)
+
+    // Return the vault collateral in underlying terms
+    function value(bytes12 vault) view returns (int128 uink)
+
     // Return the collateralization of a vault, in terms of debt that can still be acquired
     // 2 SLOAD, for collateral types array and debt, 1 SLOAD + 1 STATICCALL per existing collateral for balance and rate
     function left(bytes12 vault) view returns (int128 dart)
 
     // ---- Liquidations ----
-    // Stop a vault to be operated upon. 1 SSTORE.
-    function hold(bytes12 vault)
-
-    // Allow a vault to be operated upon again. 1 SSTORE.
-    function free(bytes12 vault)
-
-    // Remove collateral and debt from a vault as a result of a liquidation, pull underlying from buyer. Same cost as `frob`.
-    function buy(bytes12 vault, bytes32 collaterals,  uint128[] memory inks, uint128 art)
-
+    // Each liquidation engine can:
+    // - Mark vaults as not a target for liquidation
+    // - Donate assets to the Vat
+    // - Cancel debt in liquidation vaults at no cost
+    // - Retrieve collateral from liquidation vaults
+    // - Give vaults to non-privileged users
+    // Giving a user vault to a liquidation engine means it will be auctioned and liquidated.
+    // The vault will be returned to the user once it's healthy.
 }
