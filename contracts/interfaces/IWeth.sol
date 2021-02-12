00e5a95d5c3f10c2ef9e6d691414d7ead1de1da2
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-02-12 17:49:57 +0000

    feat: removed unused files

--- contracts/interfaces/IWeth.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.6.10;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-
-interface IWeth is IERC20 {
-    function deposit() external payable;
-    function withdraw(uint) external;
-}
