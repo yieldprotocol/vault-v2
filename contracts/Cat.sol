4947b8a6cf6e9baeb6c1bc6bbffefe573e9ba095
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-02-15 23:30:25 +0000

    draft: Cat to compile

--- contracts/Cat.sol
+++ contracts/Cat.sol
@@ -1,6 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+import "./interfaces/IOracle.sol";
+import "./interfaces/IVat.sol";
+import "./libraries/IlksPacking.sol";
+
+
 contract Cat {
+    using IlksPacking for bytes6[5];
   
-    mapping (bytes6 => address) oracles;                                               // [ilk] Spot oracles
+    IVat immutable public vat;
+
+    mapping (bytes6 => IOracle) oracles;                                               // [ilk] Spot oracles
+
+    constructor (IVat vat_) {
+        vat = vat_;
+    }
 
     // Put an undercollateralized vault up for liquidation.
     function grab(bytes12 vault)
@@ -17,7 +31,7 @@ contract Cat {
         int128 art = price(vault, ilks, inks);                                            // Cost of `price`
         require (art <= max, "Too expensive to buy");
         // TODO: Tweak `_frob` so that it takes the `art` from `msg.sender`, and sends the `ink` to him as well.
-        Balances memory balances = vat._frob(vault, ilk.toBytes1(), ink.toArray(), art);  // Cost of `vat._frob`
+        IVat.Balances memory balances = vat._frob(vault, ilks, inks, art);                // Cost of `vat._frob`
         if (balances == 0) vat.destroy(vault);                                            // Cost of `vat.destroy`. Check debt and all balances.
     }
 
@@ -30,32 +44,37 @@ contract Cat {
         // Let fail if the vault doesn't exist?
         // Let fail if the vault doesn't have the right ilk?
         // Let fail if the vault doesn't have enough ink?
-        uint32 timestamp = vat.timestamp(vault);                          // 1 SLOAD + 700 + 12*16
+        uint32 timestamp = vat.timestamps(vault);                          // 1 SLOAD + 700 + 12*16
         require (timestamp > 0, "Not for sale");
-        Ilks memory ilks = vat.vaultIlks(vault);                          // 1 SLOAD + 700 + 12*16. Maybe not needed.
-        Balances memory balances = vat.vaultBalances(vault);              // 3 SLOAD + 700 + 12*16. Maybe only 1 SLOAD needed.
-        uint128[] _weights = weights(ilks);
+        IVat.Ilks memory vaultIlks = vat.vaultIlks(vault);                 // 1 SLOAD + 700 + 12*16. Maybe not needed.
+        bytes6[6] selectedIlks = vaultIlks.select(ilks);
+        IVat.Balances memory balances = vat.vaultBalances(vault);          // 3 SLOAD + 700 + 12*16. Maybe only 1 SLOAD needed.
+        uint128[6] _weights = weights(selectedIlks);
         uint128 _unit = unit(balances.debt, block.timestamp - timestamp);
         uint128 _slice;
-        for ilk in ilks {
+        for (uint256 ilk = 0; ilk < selectedIlks[5]; ilk++) {
             _slice += inks[ilk] * _weights[ilk] * _unit;                  // Normalize each collateral amount, and then multiply by the unit price. The result is the proportion of the debt that must be repaid in the vault.
+        }
         return _slice * balances.debt;                                    // Price in underlying terms.
     }
 
     // Obtain the collateral normalization factors by dividing the spot price vs. ETH of each ilk, divided by the sum of all spot prices.
-    function weights(bytes6[] memory ilks)
+    function weights(bytes6[6] ilks)
         public
         view
-        returns uint128[] memory
+        returns (uint128[6])
     {
-        uint128[] spots;
-        for ilk in ilks {
+        uint128[6] spots;
+        for (uint256 ilk = 0; ilk < ilks[5]; ilk++) {
             spots[ilk] = oracles[ilk].spot();                             // C * Cost of `oracle.spot`
         }
-        uint128 total = sum(spots);
-        uint128[] _weights;
-        for ilk in ilks {
-            _weights[ilk] = spots[ilk] / weight;
+        uint128 total;
+        for (uint256 ilk = 0; ilk < ilks[5]; ilk++) {
+            total += spots[ilk];
+        }
+        uint128[6] _weights;
+        for (uint256 ilk = 0; ilk < ilks[5]; ilk++) {
+            _weights[ilk] = spots[ilk] / total;
         }
         return _weights;
     }
