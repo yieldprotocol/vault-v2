6a3570e3878e1176a512158ef42e44cbd9f4979b
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-02-16 12:46:19 +0000

    draft: bytes1 ilks to indexes or identifiers arrays

--- contracts/libraries/IlksPacking.sol
+++ contracts/libraries/IlksPacking.sol
@@ -1,13 +1,17 @@
 pragma solidity ^0.8.0;
+import "../libraries/DataTypes.sol";
 
 
 library IlksPacking {
-    function select(IVat.Ilks vaultIlks, bytes1 ilkSelector) internal view returns (bytes6[6] selectedIlks) {
-        uint256 count = 0;
-
-        for (uint256 i = 0; i < vaultIlks.length; i++)
+    function indexes(bytes1 ilkSelector) internal view returns (uint256[] memory selectedIlks) {
+        for (uint256 i = 0; i < 5; i++)
             if (ilkSelector & 2**i == 2**i)
-                selectedIlks[count++] = vaultIlks[i];
-        selectedIlks[5] = bytes6(count);
+                selectedIlks.push(i);
+    }
+
+    function identifiers(bytes1 ilkSelector, DataTypes.Ilks memory ilks) internal view returns (bytes6[] memory selectedIlks) {
+        for (uint256 i = 0; i < 5; i++)
+            if (ilkSelector & 2**i == 2**i)
+                selectedIlks.push(ilks.ids[i]);
     }
 }
