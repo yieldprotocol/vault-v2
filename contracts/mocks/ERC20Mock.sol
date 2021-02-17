a089c806b6ee1accbb21892475839b4f64805ccb
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-02-17 14:26:41 +0000

    draft: fyToken and ERC20 mocks

---
+++ contracts/mocks/ERC20Mock.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+import "@yield-protocol/utils/contracts/token/ERC20Permit.sol";
+
+
+contract ERC20Mock is ERC20Permit  {
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) public ERC20Permit(name, symbol) { }
+
+    /// @dev Give tokens to whoever asks for them.
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
