1ac54c332d27d9febaef84dbfaef85d00aa8b314
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-03-10 18:56:52 +0000

    feat: first multicall!

---
+++ test/075_ladle_multicall.ts
@@ -0,0 +1,59 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+
+import { Cauldron } from '../typechain/Cauldron'
+import { FYToken } from '../typechain/FYToken'
+import { ERC20Mock } from '../typechain/ERC20Mock'
+import { Ladle } from '../typechain/Ladle'
+
+import { ethers, waffle } from 'hardhat'
+import { expect } from 'chai'
+const { loadFixture } = waffle
+
+import { YieldEnvironment, WAD } from './shared/fixtures'
+import { EtherscanProvider } from '@ethersproject/providers'
+
+describe('Ladle - multicall', function() {
+  this.timeout(0)
+
+  let env: YieldEnvironment
+  let ownerAcc: SignerWithAddress
+  let otherAcc: SignerWithAddress
+  let owner: string
+  let other: string
+  let cauldron: Cauldron
+  let fyToken: FYToken
+  let base: ERC20Mock
+  let ladle: Ladle
+
+  async function fixture() {
+    return await YieldEnvironment.setup(ownerAcc, [baseId, ilkId], [seriesId])
+  }
+
+  before(async () => {
+    const signers = await ethers.getSigners()
+    ownerAcc = signers[0]
+    owner = await ownerAcc.getAddress()
+
+    otherAcc = signers[1]
+    other = await otherAcc.getAddress()
+  })
+
+  const baseId = ethers.utils.hexlify(ethers.utils.randomBytes(6))
+  const ilkId = ethers.utils.hexlify(ethers.utils.randomBytes(6))
+  const seriesId = ethers.utils.hexlify(ethers.utils.randomBytes(6))
+  const vaultId = ethers.utils.hexlify(ethers.utils.randomBytes(12))
+
+  beforeEach(async () => {
+    env = await loadFixture(fixture)
+    cauldron = env.cauldron
+    ladle = env.ladle
+    base = env.assets.get(baseId) as ERC20Mock
+    fyToken = env.series.get(seriesId) as FYToken
+  })
+
+  it('builds a vault and posts to it', async () => {
+    const buildCall = ladle.interface.encodeFunctionData('build', [vaultId, seriesId, ilkId])
+    const stirCall = ladle.interface.encodeFunctionData('stir', [vaultId, WAD, 0])
+    await ladle.multicall([buildCall, stirCall])
+  })
+})
