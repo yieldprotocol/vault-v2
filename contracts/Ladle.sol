53a6376cb357e5a452f3b8b9c6c1883ac7ab8333
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-15 13:12:31 +0100

    tweak: ladle uses 18 decimals

--- contracts/Ladle.sol
+++ contracts/Ladle.sol
@@ -15,10 +15,13 @@ import "@yield-protocol/utils-v2/contracts/TransferHelper.sol";
 import "@yield-protocol/utils-v2/contracts/IWETH9.sol";
 
 
-library LadleDMath { // Fixed point arithmetic in 6 decimal units
-    /// @dev Multiply an amount by a fixed point factor with 6 decimals, returning an amount
-    function dmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = x * y / 1e6;
+library LadleWMath { // Fixed point arithmetic in 18 decimal units
+    /// @dev Multiply an amount by a fixed point factor with 18 decimals
+    function wmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        unchecked {
+            z = x * y / 1e18;
+            require (z <= type(uint256).max, "WMUL Overflow");
+        }
     }
 }
 
@@ -40,7 +43,7 @@ library LadleSafe128 {
 
 /// @dev Ladle orchestrates contract calls throughout the Yield Protocol v2 into useful and efficient user oriented features.
 contract Ladle is AccessControl(), Multicall {
-    using LadleDMath for uint256;
+    using LadleWMath for uint256;
     using LadleSafe256 for uint256;
     using LadleSafe128 for uint128;
     using TransferHelper for IERC20;
@@ -487,7 +490,7 @@ contract Ladle is AccessControl(), Multicall {
         returns (uint128 amt)
     {
         if (uint32(block.timestamp) >= series.maturity) {
-            amt = uint256(art).dmul(cauldron.accrual(seriesId)).u128();
+            amt = uint256(art).wmul(cauldron.accrual(seriesId)).u128();
         } else {
             amt = art;
         }
