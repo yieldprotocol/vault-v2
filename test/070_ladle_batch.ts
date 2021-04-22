68ef3551d29fa13e6c2469f3242772e6700be9f6
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-22 10:43:17 +0100

    fix: missing import

--- test/070_ladle_batch.ts
+++ test/070_ladle_batch.ts
@@ -1,4 +1,5 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import { signatures } from '@yield-protocol/utils'
 import { constants } from '@yield-protocol/utils-v2'
 const { WAD, MAX128 } = constants
 const MAX = MAX128
