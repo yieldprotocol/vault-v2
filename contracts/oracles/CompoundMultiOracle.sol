f3b6e6ae952f24246f1c916cadbc22c92bd450d6
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-29 13:18:36 +0100

    tweak: CompoundMultiOracle to use bytes6

--- contracts/oracles/CompoundMultiOracle.sol
+++ contracts/oracles/CompoundMultiOracle.sol
@@ -6,25 +6,25 @@ import "@yield-protocol/vault-interfaces/IOracle.sol";
 import "./CTokenInterface.sol";
 
 
-library CastBytes32Address {
-    function addr(bytes32 x) internal pure returns (address y){
-        require (bytes32(bytes20(y = address(bytes20(x)))) == x, "Cast overflow");
+library CastBytes32Bytes6 {
+    function b6(bytes32 x) internal pure returns (bytes6 y){
+        require (bytes32(y = bytes6(x)) == x, "Cast overflow");
     }
 }
 
 contract CompoundMultiOracle is IOracle, Ownable {
-    using CastBytes32Address for bytes32;
+    using CastBytes32Bytes6 for bytes32;
 
-    event SourcesSet(address[] indexed bases, bytes32[] indexed kinds, address[] indexed sources_);
+    event SourcesSet(bytes6[] indexed bases, bytes32[] indexed kinds, address[] indexed sources_);
 
     uint public constant SCALE_FACTOR = 1; // I think we don't need scaling for rate and chi oracles
 
-    mapping(address => mapping(bytes32 => address)) public sources;
+    mapping(bytes6 => mapping(bytes32 => address)) public sources;
 
     /**
      * @notice Set or reset a number of oracle sources
      */
-    function setSources(address[] memory bases, bytes32[] memory kinds, address[] memory sources_) public onlyOwner {
+    function setSources(bytes6[] memory bases, bytes32[] memory kinds, address[] memory sources_) public onlyOwner {
         require(bases.length == kinds.length && kinds.length == sources_.length, "Mismatched inputs");
         for (uint256 i = 0; i < bases.length; i++)
             sources[bases[i]][kinds[i]] = sources_[i];
@@ -35,7 +35,7 @@ contract CompoundMultiOracle is IOracle, Ownable {
      * @notice Retrieve the latest price of a given source.
      * @return price
      */
-    function _peek(address base, bytes32 kind) private view returns (uint price, uint updateTime) {
+    function _peek(bytes6 base, bytes32 kind) private view returns (uint price, uint updateTime) {
         uint256 rawPrice;
         
         if (kind == "rate") rawPrice = CTokenInterface(sources[base][kind]).borrowIndex();
@@ -54,7 +54,7 @@ contract CompoundMultiOracle is IOracle, Ownable {
      */
     function peek(bytes32 base, bytes32 kind, uint256 amount) public virtual override view returns (uint256 value, uint256 updateTime) {
         uint256 price;
-        (price, updateTime) = _peek(base.addr(), kind);
+        (price, updateTime) = _peek(base.b6(), kind);
         value = price * amount / 1e18;
     }
 
@@ -64,7 +64,7 @@ contract CompoundMultiOracle is IOracle, Ownable {
      */
     function get(bytes32 base, bytes32 kind, uint256 amount) public virtual override view returns (uint256 value, uint256 updateTime) {
         uint256 price;
-        (price, updateTime) = _peek(base.addr(), kind);
+        (price, updateTime) = _peek(base.b6(), kind);
         value = price * amount / 1e18;
     }
 }
