db753929d2d024e396b5331ee0958edf5277565a
uivlis <vladsilviufarcas@gmail.com>
2021-06-21 10:30:15 +0300

    Add asset as a constructor param to Join. Remove nextAsset from JoinFactory.

--- contracts/JoinFactory.sol
+++ contracts/JoinFactory.sol
@@ -8,8 +8,6 @@ import "./Join.sol";
 /// @dev The JoinFactory can deterministically create new join instances.
 contract JoinFactory is IJoinFactory {
 
-  address private _nextAsset;
-
   /// @dev Deploys a new join.
   /// The asset address is written to a temporary storage slot to allow for simpler
   /// address calculation, while still allowing the Join contract to store the values as
@@ -17,9 +15,7 @@ contract JoinFactory is IJoinFactory {
   /// @param asset Address of the asset token.
   /// @return join The join address.
   function createJoin(address asset) external override returns (address) {
-    _nextAsset = asset;
-    Join join = new Join();
-    _nextAsset = address(0);
+    Join join = new Join(asset);
 
     join.grantRole(join.ROOT(), msg.sender);
     join.renounceRole(join.ROOT(), address(this));
@@ -27,11 +23,5 @@ contract JoinFactory is IJoinFactory {
     emit JoinCreated(asset, address(join));
 
     return address(join);
-  }
-
-  /// @dev Only used by the Join constructor.
-  /// @return The address token for the currently-constructing join.
-  function nextAsset() external view override returns (address) {
-    return _nextAsset;
   }
 }
