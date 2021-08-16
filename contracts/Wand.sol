012b621ba0ff56dab795bd62833124220a7b070c
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-08-16 13:05:43 +0100

    tweak: use selectors in Wand

--- contracts/Wand.sol
+++ contracts/Wand.sol
@@ -24,10 +24,10 @@ contract Wand is AccessControl, Constants {
 
     event Point(bytes32 indexed param, address value);
 
-    bytes4 public constant JOIN = bytes4(keccak256("join(address,uint128)"));
-    bytes4 public constant EXIT = bytes4(keccak256("exit(address,uint128)"));
-    bytes4 public constant MINT = bytes4(keccak256("mint(address,uint256)"));
-    bytes4 public constant BURN = bytes4(keccak256("burn(address,uint256)"));
+    bytes4 public constant JOIN = IJoin.join.selector;      // bytes4(keccak256("join(address,uint128)"));
+    bytes4 public constant EXIT = IJoin.exit.selector;      // bytes4(keccak256("exit(address,uint128)"));
+    bytes4 public constant MINT = IFYToken.mint.selector;   // bytes4(keccak256("mint(address,uint256)"));
+    bytes4 public constant BURN = IFYToken.burn.selector;   // bytes4(keccak256("burn(address,uint256)"));
 
     ICauldronGov public cauldron;
     ILadleGov public ladle;
