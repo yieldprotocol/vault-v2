621b680f893436f88f7668164b2942dbb94d1ca2
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-08-18 07:15:31 +0100

    fix: CompositeMultiOracle requires underlying IOracle to have 18 decimals

--- contracts/oracles/composite/CompositeMultiOracle.sol
+++ contracts/oracles/composite/CompositeMultiOracle.sol
@@ -14,28 +14,23 @@ contract CompositeMultiOracle is IOracle, AccessControl {
 
     uint8 public constant override decimals = 18;   // All prices are converted to 18 decimals
 
-    event SourceSet(bytes6 indexed baseId, bytes6 indexed quoteId, address indexed source);
+    event SourceSet(bytes6 indexed baseId, bytes6 indexed quoteId, IOracle indexed source);
     event PathSet(bytes6 indexed baseId, bytes6 indexed quoteId, bytes6[] indexed path);
 
-    struct Source {
-        address source;
-        uint8 decimals;
-    }
-
-    mapping(bytes6 => mapping(bytes6 => Source)) public sources;
+    mapping(bytes6 => mapping(bytes6 => IOracle)) public sources;
     mapping(bytes6 => mapping(bytes6 => bytes6[])) public paths;
 
     /**
      * @notice Set or reset an oracle source
      */
-    function setSource(bytes6 base, bytes6 quote, address source) external auth {
+    function setSource(bytes6 base, bytes6 quote, IOracle source) external auth {
         _setSource(base, quote, source);
     }
 
     /**
      * @notice Set or reset a number of oracle sources
      */
-    function setSources(bytes6[] memory bases, bytes6[] memory quotes, address[] memory sources_) external auth {
+    function setSources(bytes6[] memory bases, bytes6[] memory quotes, IOracle[] memory sources_) external auth {
         uint256 length = bases.length;
         require(
             length == quotes.length && 
@@ -76,17 +71,16 @@ contract CompositeMultiOracle is IOracle, AccessControl {
         external view virtual override
         returns (uint256 value, uint256 updateTime)
     {
+        value = amount;
         updateTime = block.timestamp;
-        uint256 price = 1e18; // 10 ** decimals
         bytes6 base_ = base.b6();
         bytes6 quote_ = quote.b6();
         bytes6[] memory path = paths[base_][quote_];
         for (uint256 p = 0; p < path.length; p++) {
-            (price, updateTime) = _peek(base_, path[p], price, updateTime);
+            (value, updateTime) = _peek(base_, path[p], value, updateTime);
             base_ = path[p];
         }
-        (price, updateTime) = _peek(base_, quote_, price, updateTime);
-        value = price * amount / 1e18; // Convert from our 10 ** decimals to `amount`
+        (value, updateTime) = _peek(base_, quote_, value, updateTime);
     }
 
     /**
@@ -96,55 +90,48 @@ contract CompositeMultiOracle is IOracle, AccessControl {
         external virtual override
         returns (uint256 value, uint256 updateTime)
     {
+        value = amount;
         updateTime = block.timestamp;
-        uint256 price = 1e18; // 10 ** decimals
         bytes6 base_ = base.b6();
         bytes6 quote_ = quote.b6();
         bytes6[] memory path = paths[base_][quote_];
         for (uint256 p = 0; p < path.length; p++) {
-            (price, updateTime) = _get(base_, path[p], price, updateTime);
+            (value, updateTime) = _get(base_, path[p], value, updateTime);
             base_ = path[p];
         }
-        (price, updateTime) = _get(base_, quote_, price, updateTime);
-        value = price * amount / 1e18; // Convert from our 10 ** decimals to `amount`
+        (value, updateTime) = _get(base_, quote_, value, updateTime);
     }
 
     /**
      * @notice Retrieve the value of the amount at the latest oracle price.
      */
-    function _peek(bytes6 base, bytes6 quote, uint256 priceIn, uint256 updateTimeIn)
-        private view returns (uint priceOut, uint updateTimeOut)
+    function _peek(bytes6 base, bytes6 quote, uint256 amount, uint256 updateTimeIn)
+        private view returns (uint256 value, uint256 updateTimeOut)
     {
-        Source memory source = sources[base][quote];
-        require (source.source != address(0), "Source not found");
-        (priceOut, updateTimeOut) = IOracle(source.source).peek(base, quote, 10 ** source.decimals);   // Get price for one unit
-        priceOut = priceIn * priceOut / (10 ** source.decimals);                                       // Fixed point according to decimals
-        updateTimeOut = (updateTimeOut < updateTimeIn) ? updateTimeOut : updateTimeIn;                 // Take the oldest update time
+        IOracle source = sources[base][quote];
+        require (source != IOracle(address(0)), "Source not found");
+        (value, updateTimeOut) = source.peek(base, quote, amount);
+        updateTimeOut = (updateTimeOut < updateTimeIn) ? updateTimeOut : updateTimeIn; // Take the oldest update time
     }
 
     /**
      * @notice Retrieve the value of the amount at the latest oracle price, updating it if possible.
      */
-    function _get(bytes6 base, bytes6 quote, uint256 priceIn, uint256 updateTimeIn)
-        private returns (uint priceOut, uint updateTimeOut)
+    function _get(bytes6 base, bytes6 quote, uint256 amount, uint256 updateTimeIn)
+        private returns (uint256 value, uint256 updateTimeOut)
     {
-        Source memory source = sources[base][quote];
-        require (source.source != address(0), "Source not found");
-        (priceOut, updateTimeOut) = IOracle(source.source).get(base, quote, 10 ** source.decimals);    // Get price for one unit
-        priceOut = priceIn * priceOut / (10 ** source.decimals);                                       // Fixed point according to decimals
-        updateTimeOut = (updateTimeOut < updateTimeIn) ? updateTimeOut : updateTimeIn;                 // Take the oldest update time
+        IOracle source = sources[base][quote];
+        require (source != IOracle(address(0)), "Source not found");
+        (value, updateTimeOut) = source.get(base, quote, amount);
+        updateTimeOut = (updateTimeOut < updateTimeIn) ? updateTimeOut : updateTimeIn; // Take the oldest update time
     }
 
     /**
-     * @dev Set a new price source
+     * @dev Set a new price source. It must conform to the IOracle interface and have the same decimals.
      */
-    function _setSource(bytes6 base, bytes6 quote, address source) internal {
-        uint8 decimals_ = IOracle(source).decimals();
-        require (decimals_ <= 18, "Unsupported decimals");
-        sources[base][quote] = Source({
-            source: source,
-            decimals: decimals_
-        });
+    function _setSource(bytes6 base, bytes6 quote, IOracle source) internal {
+        require (source.decimals() == decimals, "Unsupported decimals");
+        sources[base][quote] = source;
         emit SourceSet(base, quote, source);
     }
 
@@ -154,7 +141,7 @@ contract CompositeMultiOracle is IOracle, AccessControl {
     function _setPath(bytes6 base, bytes6 quote, bytes6[] memory path) internal {
         bytes6 base_ = base;
         for (uint256 p = 0; p < path.length; p++) {
-            require (sources[base_][path[p]].source != address(0), "Source not found");
+            require (sources[base_][path[p]] != IOracle(address(0)), "Source not found");
             base_ = path[p];
         }
         paths[base][quote] = path;
