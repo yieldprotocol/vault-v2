ab1ae8acd3ea367b7a85a1ac7cfd3aa69c63a286
Richie Humphrey <eihcir0@gmail.com>
2021-11-09 18:22:35 -0800

    feat: add tests and new constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,15 @@ import { ethers } from 'ethers'
 
 export const CHI = ethers.utils.formatBytes32String('CHI').slice(0, 14)
 export const RATE = ethers.utils.formatBytes32String('RATE').slice(0, 14)
+
 export const DAI = ethers.utils.formatBytes32String('01').slice(0, 14)
 export const USDC = ethers.utils.formatBytes32String('02').slice(0, 14)
 export const ETH = ethers.utils.formatBytes32String('00').slice(0, 14)
 export const STETH = ethers.utils.formatBytes32String('05').slice(0, 14)
 export const WSTETH = ethers.utils.formatBytes32String('06').slice(0, 14)
+
+//yearn vaults 1xx
+export const YVUSDC = ethers.utils.formatBytes32String('102').slice(0, 14)
+export const YVDAI = ethers.utils.formatBytes32String('101').slice(0, 14)
+export const YVETH = ethers.utils.formatBytes32String('100').slice(0, 14)
+export const YVSTETH = ethers.utils.formatBytes32String('105').slice(0, 14)
--- test/017_oracles_yearnvault.ts
+++ test/017_oracles_yearnvault.ts
@@ -4,25 +4,20 @@ import { expect } from 'chai'
 import { parseEther } from '@ethersproject/units'
 import { constants, id } from '@yield-protocol/utils-v2'
 const { WAD } = constants
-import { USDC, ETH, DAI, WSTETH, STETH } from '../src/constants'
+import { USDC, ETH, DAI, YVUSDC, YVDAI, YVSTETH } from '../src/constants'
 
-import { LidoOracle } from '../typechain/LidoOracle'
-import { WstETHMock } from '../typechain/WstETHMock'
-import { ChainlinkAggregatorV3Mock } from '../typechain/ChainlinkAggregatorV3Mock'
-import { ChainlinkMultiOracle } from '../typechain/ChainlinkMultiOracle'
-import { CompositeMultiOracle } from '../typechain/CompositeMultiOracle'
+import { YearnVaultMultiOracle } from '../typechain/YearnVaultMultiOracle'
+import { YearnVaultMock } from '../typechain/YearnVaultMock'
 import { WETH9Mock } from '../typechain/WETH9Mock'
 import { ERC20Mock } from '../typechain/ERC20Mock'
 import { USDCMock } from '../typechain/USDCMock'
 
-import LidoOracleArtifact from '../artifacts/contracts/oracles/lido/LidoOracle.sol/LidoOracle.json'
-import WstETHMockArtifact from '../artifacts/contracts/mocks/oracles/lido/WstETHMock.sol/WstETHMock.json'
-import ChainlinkAggregatorV3MockArtifact from '../artifacts/contracts/mocks/oracles/chainlink/ChainlinkAggregatorV3Mock.sol/ChainlinkAggregatorV3Mock.json'
-import ChainlinkMultiOracleArtifact from '../artifacts/contracts/oracles/chainlink/ChainlinkMultiOracle.sol/ChainlinkMultiOracle.json'
-import CompositeMultiOracleArtifact from '../artifacts/contracts/oracles/composite/CompositeMultiOracle.sol/CompositeMultiOracle.json'
+import YearnVaultMultiOracleArtifact from '../artifacts/contracts/oracles/yearn/YearnVaultMultiOracle.sol/YearnVaultMultiOracle.json'
+import YearnVaultMockArtifact from '../artifacts/contracts/mocks/YearnVaultMock.sol/YearnVaultMock.json'
 import WETH9MockArtifact from '../artifacts/contracts/mocks/WETH9Mock.sol/WETH9Mock.json'
 import ERC20MockArtifact from '../artifacts/contracts/mocks/ERC20Mock.sol/ERC20Mock.json'
 import USDCMockArtifact from '../artifacts/contracts/mocks/USDCMock.sol/USDCMock.json'
+import { BigNumber } from '@ethersproject/bignumber'
 
 const { deployContract } = waffle
 
@@ -30,137 +25,105 @@ function bytes6ToBytes32(x: string): string {
   return x + '00'.repeat(26)
 }
 
-describe('Oracles - Lido', function () {
+describe('Oracles - Yearn Vault', function () {
   this.timeout(0)
 
   let ownerAcc: SignerWithAddress
   let owner: string
-  let weth: WETH9Mock
-  let steth: ERC20Mock
   let usdc: USDCMock
-  let lidoOracle: LidoOracle
-  let lidoMock: WstETHMock
-  let chainlinkMultiOracle: ChainlinkMultiOracle
-  let compositeMultiOracle: CompositeMultiOracle
-  let stethEthAggregator: ChainlinkAggregatorV3Mock
-  let usdcEthAggregator: ChainlinkAggregatorV3Mock
-  const mockBytes6 = ethers.utils.hexlify(ethers.utils.randomBytes(6))
+  let initialYvUSDCRate = '1083891' // 6 decimals
+  let initialYvDAIRate = '1071594513314087964' // 18 decimals
+  let yearnVaultMultiOracle: YearnVaultMultiOracle
+  let yvUSDCMock: YearnVaultMock
+  let yvDAIMock: YearnVaultMock
 
   before(async () => {
     const signers = await ethers.getSigners()
     ownerAcc = signers[0]
     owner = await ownerAcc.getAddress()
-    lidoMock = (await deployContract(ownerAcc, WstETHMockArtifact)) as WstETHMock
-    await lidoMock.set('1008339308050006006')
 
-    weth = (await deployContract(ownerAcc, WETH9MockArtifact)) as WETH9Mock
+    yvUSDCMock = (await deployContract(ownerAcc, YearnVaultMockArtifact, [
+      'Yearn Vault USD Coin',
+      'yvUSDC',
+      6,
+      BigNumber.from(initialYvUSDCRate),
+    ])) as YearnVaultMock
+
+    yvDAIMock = (await deployContract(ownerAcc, YearnVaultMockArtifact, [
+      'Yearn Vault DAI',
+      'yvDAI',
+      18,
+      BigNumber.from(initialYvDAIRate),
+    ])) as YearnVaultMock
+
     usdc = (await deployContract(ownerAcc, USDCMockArtifact)) as USDCMock
-    steth = (await deployContract(ownerAcc, ERC20MockArtifact, ['Liquid staked Ether 2.0', 'stETH'])) as ERC20Mock
 
-    chainlinkMultiOracle = (await deployContract(ownerAcc, ChainlinkMultiOracleArtifact, [])) as ChainlinkMultiOracle
-    await chainlinkMultiOracle.grantRole(
-      id(chainlinkMultiOracle.interface, 'setSource(bytes6,address,bytes6,address,address)'),
+    yearnVaultMultiOracle = (await deployContract(ownerAcc, YearnVaultMultiOracleArtifact, [])) as YearnVaultMultiOracle
+    await yearnVaultMultiOracle.grantRole(
+      id(yearnVaultMultiOracle.interface, 'setSource(bytes6,bytes6,address)'),
       owner
     )
-    usdcEthAggregator = (await deployContract(ownerAcc, ChainlinkAggregatorV3MockArtifact)) as ChainlinkAggregatorV3Mock
-    stethEthAggregator = (await deployContract(
-      ownerAcc,
-      ChainlinkAggregatorV3MockArtifact
-    )) as ChainlinkAggregatorV3Mock
-
-    //Set stETH/ETH chainlink oracle
-    await chainlinkMultiOracle.setSource(STETH, steth.address, ETH, weth.address, stethEthAggregator.address)
-    await chainlinkMultiOracle.setSource(USDC, usdc.address, ETH, weth.address, usdcEthAggregator.address)
-
-    await stethEthAggregator.set('992415619690099500')
-    await usdcEthAggregator.set(WAD.div(4000)) // 1 USDC (1^6) in ETH
-    lidoOracle = (await deployContract(ownerAcc, LidoOracleArtifact, [
-      bytes6ToBytes32(WSTETH),
-      bytes6ToBytes32(STETH),
-    ])) as LidoOracle
-    await lidoOracle.grantRole(id(lidoOracle.interface, 'setSource(address)'), owner)
-    await lidoOracle['setSource(address)'](lidoMock.address) //mockOracle
-
-    compositeMultiOracle = (await deployContract(ownerAcc, CompositeMultiOracleArtifact)) as CompositeMultiOracle
-    compositeMultiOracle.grantRoles(
-      [
-        id(compositeMultiOracle.interface, 'setSource(bytes6,bytes6,address)'),
-        id(compositeMultiOracle.interface, 'setPath(bytes6,bytes6,bytes6[])'),
-      ],
-      owner
-    )
-    // Set up the CompositeMultiOracle to draw from the ChainlinkMultiOracle
-    await compositeMultiOracle.setSource(WSTETH, STETH, lidoOracle.address)
-    await compositeMultiOracle.setSource(STETH, ETH, chainlinkMultiOracle.address)
-    await compositeMultiOracle.setSource(USDC, ETH, chainlinkMultiOracle.address)
-
-    //Set path for wsteth-steth-eth
-    await compositeMultiOracle.setPath(WSTETH, ETH, [STETH])
-
-    // Set path for wsteth-steth-eth.USDC
-    await compositeMultiOracle.setPath(WSTETH, USDC, [STETH, ETH])
   })
 
-  it('sets and retrieves the value at spot price', async () => {
-    expect((await lidoOracle.callStatic.get(bytes6ToBytes32(STETH), bytes6ToBytes32(WSTETH), WAD))[0]).to.equal(
-      '991729660855795538'
-    )
-    expect(
-      (await lidoOracle.callStatic.get(bytes6ToBytes32(WSTETH), bytes6ToBytes32(STETH), parseEther('1')))[0]
-    ).to.equal('1008339308050006006')
+  it('get() reverts if pair not found', async () => {
+    await expect(
+      yearnVaultMultiOracle.get(bytes6ToBytes32(YVUSDC), bytes6ToBytes32(USDC), '2' + '000000')
+    ).to.be.revertedWith('Source not found')
   })
 
-  it('revert on unknown sources', async () => {
-    await expect(lidoOracle.callStatic.get(bytes6ToBytes32(DAI), bytes6ToBytes32(mockBytes6), WAD)).to.be.revertedWith(
-      'Source not found'
+  it('setSource() reverts if quote and base are the same', async () => {
+    await expect(yearnVaultMultiOracle.setSource(USDC, USDC, yvUSDCMock.address)).to.be.revertedWith(
+      'Set source not needed'
     )
   })
 
-  describe('Composite', () => {
-    it('retrieves the value at spot price for direct pairs', async () => {
-      // WSTETH-STETH
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(WSTETH), bytes6ToBytes32(STETH), parseEther('1')))[0]
-      ).to.equal('1008339308050006006')
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(STETH), bytes6ToBytes32(WSTETH), parseEther('1')))[0]
-      ).to.equal('991729660855795538')
+  it('setSource() sets a pair and the inverse pair', async () => {
+    //Set yvUSDC/USDC yearn vault oracle
+    expect(await yearnVaultMultiOracle.setSource(YVUSDC, USDC, yvUSDCMock.address))
+      .to.emit(yearnVaultMultiOracle, 'SourceSet')
+      .withArgs(USDC, YVUSDC, yvUSDCMock.address, 6, true)
 
-      // STETH-ETH
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(STETH), bytes6ToBytes32(ETH), parseEther('1')))[0]
-      ).to.equal('992415619690099500')
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(ETH), bytes6ToBytes32(STETH), parseEther('1')))[0]
-      ).to.equal('1007642342743727538')
+    await expect(yearnVaultMultiOracle.get(bytes6ToBytes32(USDC), bytes6ToBytes32(YVUSDC), '2' + '000000')).not.to.be
+      .reverted
+  })
 
-      // ETH-USDC
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(ETH), bytes6ToBytes32(USDC), parseEther('1')))[0]
-      ).to.equal('4000000000')
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(USDC), bytes6ToBytes32(ETH), parseEther('1')))[0]
-      ).to.equal('250000000000000000000000000')
+  describe('with sources set', function () {
+    beforeEach(async () => {
+      await yearnVaultMultiOracle.setSource(YVUSDC, USDC, yvUSDCMock.address)
+      await yearnVaultMultiOracle.setSource(YVDAI, DAI, yvDAIMock.address)
     })
 
-    it('retrieves the value at spot price for WSTETH -> ETH and reverse', async () => {
+    it('get() and peek() return correct values', async () => {
       expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(WSTETH), bytes6ToBytes32(ETH), parseEther('1')))[0]
-      ).to.equal('1000691679256332845')
+        (await yearnVaultMultiOracle.get(bytes6ToBytes32(YVUSDC), bytes6ToBytes32(USDC), '2' + '000000'))[0]
+      ).to.equal(BigNumber.from(initialYvUSDCRate).mul(2).toString())
+      expect((await yearnVaultMultiOracle.peek(bytes6ToBytes32(YVDAI), bytes6ToBytes32(DAI), WAD.mul(2)))[0]).to.equal(
+        BigNumber.from(initialYvDAIRate).mul(2).toString()
+      )
 
+      // check inverted pairs
+      const invertedYvUSDCRate = parseInt(((1 / parseFloat(initialYvUSDCRate)) * 10 ** 12).toString()).toString()
+      expect((await yearnVaultMultiOracle.get(bytes6ToBytes32(USDC), bytes6ToBytes32(YVUSDC), '1000000'))[0]).to.equal(
+        BigNumber.from(invertedYvUSDCRate).toString()
+      )
+
+      expect((await yearnVaultMultiOracle.peek(bytes6ToBytes32(DAI), bytes6ToBytes32(YVDAI), WAD))[0]).to.equal(
+        WAD.mul(WAD).div(BigNumber.from(initialYvDAIRate)).toString()
+      )
+
+      // change price
+      const newPrice = '1088888'
+      await yvUSDCMock.setPrice(newPrice)
       expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(ETH), bytes6ToBytes32(WSTETH), parseEther('1')))[0]
-      ).to.equal('999308798833176199')
+        (await yearnVaultMultiOracle.get(bytes6ToBytes32(YVUSDC), bytes6ToBytes32(USDC), '2' + '000000'))[0]
+      ).to.equal(BigNumber.from(newPrice).mul(2).toString())
     })
 
-    it('retrieves the value at spot price for WSTETH -> USDC and reverse', async () => {
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(WSTETH), bytes6ToBytes32(USDC), parseEther('1')))[0]
-      ).to.equal('4002766717')
-
-      expect(
-        (await compositeMultiOracle.peek(bytes6ToBytes32(USDC), bytes6ToBytes32(WSTETH), parseEther('1')))[0]
-      ).to.equal('249827199708294049841946834')
+    it('get() reverts on zero price ', async () => {
+      await yvUSDCMock.setPrice(0)
+      await expect(
+        yearnVaultMultiOracle.get(bytes6ToBytes32(YVUSDC), bytes6ToBytes32(USDC), '2' + '000000')
+      ).to.be.revertedWith('Zero price')
     })
   })
 })
