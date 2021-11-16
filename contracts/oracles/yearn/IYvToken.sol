fbbeb7054d59791303c53f71351866e9ab2ebb81
Richie Humphrey <eihcir0@gmail.com>
2021-11-15 18:02:53 -0800

    fix: interface inheritance

--- contracts/oracles/yearn/IYvToken.sol
+++ contracts/oracles/yearn/IYvToken.sol
@@ -4,7 +4,7 @@ import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 
 // @notice Interface for Yearn Vault tokens for use with Yield price oracles
 // @dev see https://github.com/yearn/yearn-vaults/blob/main/contracts/Vault.vy
-interface IYvToken is IERC20 {
+interface IYvToken is IERC20Metadata {
     // @notice Returns the price for a single Yearn Vault share.
     // @dev total vault assets / total token supply (calculated not cached)
     function pricePerShare() external view returns (uint256);
