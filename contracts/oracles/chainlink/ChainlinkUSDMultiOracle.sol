7068ab22ae9c82da1f8506df7836960894e2fb73
Owleksiy <owleksiy@gmail.com>
2021-11-22 12:46:10 -0800

    ChainlinkL2USDMultiOracle: Chainlink USD Oracle that's safe to use on L2
    Incorporates https://docs.chain.link/docs/l2-sequencer-flag/
    
    Instead of parametrizing ChainlinkUSDMultiOracle, we create new contract to avoid
    increasing the gas cost of ChainlinkUSDMultiOracle when there's no need to check the
    sequencer flag

--- contracts/oracles/chainlink/ChainlinkUSDMultiOracle.sol
+++ contracts/oracles/chainlink/ChainlinkUSDMultiOracle.sol
@@ -7,6 +7,7 @@ import '@yield-protocol/utils-v2/contracts/token/IERC20Metadata.sol';
 import '@yield-protocol/vault-interfaces/IOracle.sol';
 import '../../constants/Constants.sol';
 import './AggregatorV3Interface.sol';
+import './FlagsInterface.sol';
 
 /**
  * @title ChainlinkUSDMultiOracle
@@ -87,7 +88,7 @@ contract ChainlinkUSDMultiOracle is IOracle, AccessControl, Constants {
         bytes6 baseId,
         bytes6 quoteId,
         uint256 amountBase
-    ) private view returns (uint256 amountQuote, uint256 updateTime) {
+    ) internal view returns (uint256 amountQuote, uint256 updateTime) {
         (uint256 basePrice, uint256 updateTime1, uint8 baseDecimals) = _getPriceInUSD(baseId);
         (uint256 quotePrice, uint256 updateTime2, uint8 quoteDecimals) = _getPriceInUSD(quoteId);
 
@@ -102,5 +103,59 @@ contract ChainlinkUSDMultiOracle is IOracle, AccessControl, Constants {
         }
 
         updateTime = (updateTime1 < updateTime2) ? updateTime1 : updateTime2;
+    }
+}
+
+/**
+ * @title ChainlinkL2USDMultiOracle: ChainlinkUSDMultiOracle that's safe to use on L2
+ * @notice Chainlink recommends checking the sequencer status on some L2 networks to avoid
+  reading stale data
+  
+  https://docs.chain.link/docs/l2-sequencer-flag/
+ */
+contract ChainlinkL2USDMultiOracle is ChainlinkUSDMultiOracle {
+    using CastBytes32Bytes6 for bytes32;
+
+    FlagsInterface public chainlinkFlags;
+    // https://docs.chain.link/docs/l2-sequencer-flag/
+    address internal constant FLAG_ARBITRUM_SEQ_OFFLINE =
+        address(bytes20(bytes32(uint256(keccak256('chainlink.flags.arbitrum-seq-offline')) - 1)));
+
+    constructor(FlagsInterface flags) {
+        require(address(flags) != address(0), 'FlagsInterface has to be set');
+        chainlinkFlags = flags;
+    }
+
+    function assertCanGetPrice() private view {
+        bool isRaised = chainlinkFlags.getFlag(FLAG_ARBITRUM_SEQ_OFFLINE);
+        if (isRaised) {
+            // If flag is raised we shouldn't perform any critical operations
+            revert('Chainlink feeds are not being updated');
+        }
+    }
+
+    /// @dev Convert amountBase base into quote at the latest oracle price.
+    function peek(
+        bytes32 baseId,
+        bytes32 quoteId,
+        uint256 amountBase
+    ) external view virtual override returns (uint256 amountQuote, uint256 updateTime) {
+        assertCanGetPrice();
+        if (baseId == quoteId) return (amountBase, block.timestamp);
+
+        (amountQuote, updateTime) = _peekThroughUSD(baseId.b6(), quoteId.b6(), amountBase);
+    }
+
+    /// @dev Convert amountBase base into quote at the latest oracle price, updating state if necessary. Same as `peek` for this oracle.
+    function get(
+        bytes32 baseId,
+        bytes32 quoteId,
+        uint256 amountBase
+    ) external virtual override returns (uint256 amountQuote, uint256 updateTime) {
+        assertCanGetPrice();
+
+        if (baseId == quoteId) return (amountBase, block.timestamp);
+
+        (amountQuote, updateTime) = _peekThroughUSD(baseId.b6(), quoteId.b6(), amountBase);
     }
 }
