009b814c2ed9abeb6f587985080848644ebb2591
Richie <devtooligan@gmail.com>
2021-12-09 21:53:11 -0800

    fix/update yvToken mock

--- contracts/mocks/YearnVaultMock.sol
+++ contracts/mocks/YearnVaultMock.sol
@@ -1,34 +1,39 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.6;
-import "@yield-protocol/utils-v2/contracts/token/ERC20Permit.sol";
 
-import "contracts/oracles/yearn/IYvToken.sol";
+import "./oracles/ISourceMock.sol";
+import "@yield-protocol/utils-v2/contracts/token/ERC20.sol";
 
+contract YvTokenMock is ISourceMock, ERC20 {
+    ERC20 public token;
+    uint256 public price;
 
-contract YearnVaultMock is ERC20Permit, IYvToken {
-    uint256 public price; //return value of pricePerShare()
-
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint8 decimals,
-        uint256 pricePerShare_
-    ) ERC20Permit(name, symbol, decimals) {
-        setPrice(pricePerShare_);
-    }
-
-    function pricePerShare() external view override returns (uint256) {
-        return price;
-    }
-
-    /// @notice use to set share price of mock
-    /// @dev be sure to use correct decimals
-    function setPrice(uint256 price_) public {
-        price = price_;
+    constructor(string memory name, string memory symbol, uint8 decimals, ERC20 token_) ERC20(name, symbol, decimals) {
+        token = token_;
     }
 
     /// @dev Give tokens to whoever asks for them.
     function mint(address to, uint256 amount) public virtual {
         _mint(to, amount);
+    }
+
+    function deposit(uint256 deposited, address to) public returns (uint256 minted) {
+        token.transferFrom(msg.sender, address(this), deposited);
+        minted = deposited * token.decimals() / price;
+        _mint(msg.sender, minted);
+    }
+
+    function withdraw(uint256 withdrawn, address to) public returns (uint256 obtained) {
+        obtained = withdrawn * price / token.decimals();
+        _burn(msg.sender, withdrawn);
+        token.transfer(to, obtained);
+    }
+
+    function set(uint256 price_) external override {
+        price = price_;
+    }
+
+    function pricePerShare() public view returns (uint256) {
+        return price;
     }
 }
