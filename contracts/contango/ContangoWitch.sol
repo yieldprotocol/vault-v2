be9e00fb06a83d0d69da5f8eaffbea783c055050
Bruno Bonanno <bbonanno@gmail.com>
2022-03-02 12:54:33 -0500

    witch considers both, the aggregate collateralisation and the particular vault to be liquidated

--- contracts/contango/ContangoWitch.sol
+++ contracts/contango/ContangoWitch.sol
@@ -7,8 +7,10 @@ import "./interfaces/IContangoCauldron.sol";
 contract ContangoWitch is Witch {
     constructor(IContangoCauldron cauldron_, ILadle ladle_) Witch(cauldron_, ladle_) {}
 
-    // @dev check the aggregate of all vaults status
-    function _isVaultUndercollateralised(bytes12) internal override returns (bool) {
-        return IContangoCauldron(address(cauldron)).getFreeCollateralUSD() < 0;
+    // @dev check the aggregate of all vaults status first
+    function _isVaultUndercollateralised(bytes12 vaultId) internal override returns (bool) {
+        return
+            IContangoCauldron(address(cauldron)).getFreeCollateralUSD() < 0 &&
+            super._isVaultUndercollateralised(vaultId);
     }
 }
