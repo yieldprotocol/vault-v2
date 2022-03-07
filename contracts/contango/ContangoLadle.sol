3e7b8e33f65363f2d1e6bf11ef1eefa0052f6a5a
Bruno Bonanno <bbonanno@gmail.com>
2022-03-07 15:30:04 -0500

    deterministic vaultId

--- contracts/contango/ContangoLadle.sol
+++ contracts/contango/ContangoLadle.sol
@@ -4,16 +4,26 @@ pragma solidity 0.8.6;
 import "../Ladle.sol";
 
 contract ContangoLadle is Ladle {
+    constructor(ICauldron cauldron, IWETH9 weth) Ladle(cauldron, weth) {}
 
-    constructor (ICauldron cauldron, IWETH9 weth) Ladle(cauldron, weth) { }
-
-    // @dev auth version of build, only contango can create vaults here
-    // all other methods rely on being vault owner, so no need to secure them
-    function build(bytes6 seriesId, bytes6 ilkId, uint8 salt)
-        external override payable auth
-        returns(bytes12, DataTypes.Vault memory)
-    {
-        return _build(seriesId, ilkId, salt);
+    // @dev we want to use deterministic vault creation given is behind auth
+    function build(
+        bytes6,
+        bytes6,
+        uint8
+    ) external payable override auth returns (bytes12, DataTypes.Vault memory) {
+        revert("Use build");
     }
 
+    // @dev deterministic version of build, only contango can create vaults here
+    // all other methods rely on being vault owner, so no need to secure them
+    function build(
+        bytes12 vaultId,
+        bytes6 seriesId,
+        bytes6 ilkId
+    ) external payable auth returns (DataTypes.Vault memory vault) {
+        vault = cauldron.build(msg.sender, vaultId, seriesId, ilkId);
+        // Store the vault data in the cache
+        cachedVaultId = vaultId;
+    }
 }
