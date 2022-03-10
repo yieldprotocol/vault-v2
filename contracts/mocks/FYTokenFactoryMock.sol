9ff6c2a395f9cc91b34fdf75d6e47c7d5024cc32
Sabnock01 <james.thompson1848@gmail.com>
2022-03-10 14:07:06 -0600

    fix: fix mock formatting

--- contracts/mocks/FYTokenFactoryMock.sol
+++ contracts/mocks/FYTokenFactoryMock.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.6;
 
--- contracts/mocks/JoinFactoryMock.sol
+++ contracts/mocks/JoinFactoryMock.sol
@@ -1,4 +1,4 @@
-
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.6;
 
 import "@yield-protocol/vault-interfaces/IJoinFactory.sol";
