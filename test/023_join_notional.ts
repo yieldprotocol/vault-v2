0cd7723b654667fe6687d5bc0f6bc31a19b30f34
Alberto Cuesta Cañada <mail@albertocuesta.es>
2022-03-11 14:34:54 +0000

    feat: old tests work with new mocks

--- test/023_join_notional.ts
+++ test/023_join_notional.ts
@@ -4,13 +4,13 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-wit
 
 const { WAD, MAX256 } = constants
 
-import Join1155Artifact from '../artifacts/contracts/other/notional/Join1155.sol/Join1155.json'
+import NotionalJoinArtifact from '../artifacts/contracts/other/notional/NotionalJoin.sol/NotionalJoin.json'
 import ERC20MockArtifact from '../artifacts/contracts/mocks/ERC20Mock.sol/ERC20Mock.json'
-import ERC1155MockArtifact from '../artifacts/contracts/other/notional/ERC1155Mock.sol/ERC1155Mock.json'
+import FCashMockArtifact from '../artifacts/contracts/other/notional/FCashMock.sol/FCashMock.json'
 
-import { Join1155 } from '../typechain/Join1155'
+import { NotionalJoin } from '../typechain/NotionalJoin'
 import { ERC20Mock } from '../typechain/ERC20Mock'
-import { ERC1155Mock } from '../typechain/ERC1155Mock'
+import { FCashMock } from '../typechain/FCashMock'
 
 import { ethers, waffle } from 'hardhat'
 import { expect } from 'chai'
@@ -23,11 +23,12 @@ describe('Join1155', function () {
   let owner: string
   let otherAcc: SignerWithAddress
   let other: string
-  let join: Join1155
-  let token: ERC1155Mock
-  let tokenId = '1'
-  let otherERC20: ERC20Mock
-  let otherERC1155: ERC1155Mock
+  let join: NotionalJoin
+  let fCash: FCashMock
+  let underlying: ERC20Mock
+  const maturity: string = '1656288000'
+  const currencyId: string = '2'
+  const fCashId: string = '563373963149313'
 
   before(async () => {
     const signers = await ethers.getSigners()
@@ -39,10 +40,14 @@ describe('Join1155', function () {
   })
 
   beforeEach(async () => {
-    token = (await deployContract(ownerAcc, ERC1155MockArtifact)) as ERC1155Mock
-    otherERC20 = (await deployContract(ownerAcc, ERC20MockArtifact, ['', ''])) as ERC20Mock
-    otherERC1155 = (await deployContract(ownerAcc, ERC1155MockArtifact)) as ERC1155Mock
-    join = (await deployContract(ownerAcc, Join1155Artifact, [token.address, tokenId])) as Join1155
+    underlying = (await deployContract(ownerAcc, ERC20MockArtifact, ['', ''])) as ERC20Mock
+    fCash = (await deployContract(ownerAcc, FCashMockArtifact, [underlying.address, fCashId])) as FCashMock
+    join = (await deployContract(ownerAcc, NotionalJoinArtifact, [
+      fCash.address,
+      underlying.address,
+      maturity,
+      currencyId
+    ])) as NotionalJoin
 
     await join.grantRoles(
       [
@@ -54,45 +59,31 @@ describe('Join1155', function () {
       owner
     )
 
-    await token.mint(owner, tokenId, WAD.mul(100), '0x00')
-    await token.setApprovalForAll(join.address, true)
+    await fCash.mint(owner, fCashId, WAD.mul(100), '0x00')
+    await fCash.setApprovalForAll(join.address, true)
   })
 
-  it('retrieves airdropped ERC20 tokens', async () => {
-    await otherERC20.mint(join.address, WAD)
-    expect(await join.retrieve(otherERC20.address, other))
-      .to.emit(otherERC20, 'Transfer')
-      .withArgs(join.address, other, WAD)
-  })
-
-  it('retrieves airdropped ERC1155 tokens', async () => {
-    await otherERC1155.mint(join.address, tokenId, WAD, '0x00')
-    expect(await join.retrieveERC1155(otherERC1155.address, tokenId, other))
-      .to.emit(otherERC1155, 'TransferSingle')
-      .withArgs(join.address, join.address, other, tokenId, WAD)
-  })
-
-  it('pulls tokens from user', async () => {
+  it('pulls fCash from user', async () => {
     expect(await join.join(owner, WAD))
-      .to.emit(token, 'TransferSingle')
-      .withArgs(join.address, owner, join.address, tokenId, WAD)
+      .to.emit(fCash, 'TransferSingle')
+      .withArgs(join.address, owner, join.address, fCashId, WAD)
     expect(await join.storedBalance()).to.equal(WAD)
   })
 
-  describe('with tokens in the join', async () => {
+  describe('with fCashs in the join', async () => {
     beforeEach(async () => {
-      await token.safeTransferFrom(owner, join.address, tokenId, WAD, '0x00')
+      await fCash.safeTransferFrom(owner, join.address, fCashId, WAD, '0x00')
     })
 
     it('accepts surplus as a transfer', async () => {
-      expect(await join.join(owner, WAD)).to.not.emit(token, 'TransferSingle')
+      expect(await join.join(owner, WAD)).to.not.emit(fCash, 'TransferSingle')
       expect(await join.storedBalance()).to.equal(WAD)
     })
 
-    it('combines surplus and tokens pulled from the user', async () => {
+    it('combines surplus and fCashs pulled from the user', async () => {
       expect(await join.join(owner, WAD.mul(2)))
-        .to.emit(token, 'TransferSingle')
-        .withArgs(join.address, owner, join.address, tokenId, WAD)
+        .to.emit(fCash, 'TransferSingle')
+        .withArgs(join.address, owner, join.address, fCashId, WAD)
       expect(await join.storedBalance()).to.equal(WAD.mul(2))
     })
 
@@ -101,10 +92,10 @@ describe('Join1155', function () {
         await join.join(owner, WAD)
       })
 
-      it('pushes tokens to user', async () => {
+      it('pushes fCashs to user', async () => {
         expect(await join.exit(owner, WAD))
-          .to.emit(token, 'TransferSingle')
-          .withArgs(join.address, join.address, owner, tokenId, WAD)
+          .to.emit(fCash, 'TransferSingle')
+          .withArgs(join.address, join.address, owner, fCashId, WAD)
         expect(await join.storedBalance()).to.equal(0)
       })
 
