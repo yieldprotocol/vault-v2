ffaf2c9696bb67b771a850f5fd4a6e5fd2c2cbba
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-04-01 20:01:33 -0500

    chore: add rate chi oracle test in foundry

---
+++ contracts/foundry-tests/RateChiMultioracle.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.6;
+
+import "@yield-protocol/utils-v2/contracts/access/AccessControl.sol";
+import "../oracles/compound/CompoundMultiOracle.sol";
+import "../mocks/oracles/compound/CTokenChiMock.sol";
+import "../mocks/oracles/compound/CTokenRateMock.sol";
+import "./utils/Test.sol";
+import "./utils/TestConstants.sol";
+
+contract RateChiMultiOracleTest is Test, TestConstants, AccessControl {
+    CTokenChiMock public cTokenChi;
+    CTokenRateMock public cTokenRate;
+    CompoundMultiOracle public compoundMultiOracle;
+
+    bytes6 public baseId = 0x25dde80ea598;
+    bytes6 public mockBytes6 = 0x000000000001;
+
+    function setUp() public {
+        cTokenChi = new CTokenChiMock();
+        cTokenRate = new CTokenRateMock();
+        compoundMultiOracle = new CompoundMultiOracle();
+        compoundMultiOracle.grantRole(0x92b45d9c, address(this));
+        compoundMultiOracle.setSource(baseId, CHI, address(cTokenChi));
+        compoundMultiOracle.setSource(baseId, RATE, address(cTokenRate));
+    }
+
+    function testRevertUnknownSource() public {
+        vm.expectRevert("Source not found");
+        compoundMultiOracle.get(mockBytes6, mockBytes6, WAD);
+    }
+
+    function testSetRetrieveChiRate() public {
+        cTokenChi.set(WAD * 2);
+        cTokenRate.set(WAD * 3);
+        (uint256 getChiPrice,) = compoundMultiOracle.get(bytes32(baseId), bytes32(CHI), WAD);
+        require(getChiPrice == WAD * 2, "Failed to get CHI spot price");
+        (uint256 getRatePrice,) = compoundMultiOracle.get(bytes32(baseId), bytes32(RATE), WAD);
+        require(getRatePrice == WAD * 3, "Failed to get RATE spot price");
+        (uint256 peekChiPrice,) = compoundMultiOracle.peek(bytes32(baseId), bytes32(CHI), WAD);
+        require(peekChiPrice == WAD * 2, "Failed to peek CHI spot price");
+        (uint256 peekRatePrice,) = compoundMultiOracle.peek(bytes32(baseId), bytes32(RATE), WAD);
+        require(peekRatePrice == WAD * 3, "Failed to peek RATE spot price");
+    }
+}
--- contracts/foundry-tests/utils/TestConstants.sol
+++ contracts/foundry-tests/utils/TestConstants.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.6;
 
 contract TestConstants {
     uint256 public constant WAD = 1000000000000000000;
+
+    bytes6 public constant CHI = 0x434849000000;
+    bytes6 public constant RATE = 0x524154450000;
+
     bytes6 public constant ETH = 0x303000000000;
     bytes6 public constant DAI = 0x303100000000;
     bytes6 public constant USDC = 0x303200000000;
