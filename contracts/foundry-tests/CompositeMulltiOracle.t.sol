f0e2c9c60c51df39cf365b0876bd0aa5a287e4ef
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-04-10 11:32:53 -0500

    chore: add composite multioracle test

---
+++ contracts/foundry-tests/CompositeMulltiOracle.t.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.6;
+
+import "@yield-protocol/utils-v2/contracts/access/AccessControl.sol";
+import "../oracles/chainlink/ChainlinkMultiOracle.sol";
+import "../oracles/composite/CompositeMultiOracle.sol";
+import "../mocks/DAIMock.sol";
+import "../mocks/USDCMock.sol";
+import "../mocks/WETH9Mock.sol";
+import "../mocks/oracles/chainlink/ChainlinkAggregatorV3Mock.sol";
+import "./utils/Test.sol";
+import "./utils/TestConstants.sol";
+
+contract CompositeMultiOracleTest is Test, TestConstants, AccessControl {
+
+    event SourceSet(bytes6 indexed baseId, bytes6 indexed quoteId, IOracle indexed source);
+    event PathSet(bytes6 indexed baseId, bytes6 indexed quoteId, bytes6[] indexed path);
+
+    DAIMock public dai;
+    USDCMock public usdc;
+    WETH9Mock public weth;
+    ChainlinkMultiOracle public chainlinkMultiOracle;
+    CompositeMultiOracle public compositeMultiOracle;
+    ChainlinkAggregatorV3Mock public daiEthAggregator; 
+    ChainlinkAggregatorV3Mock public usdcEthAggregator;
+
+    bytes32 public mockBytes32 = 0x0000000000000000000000000000000000000000000000000000000000000001;
+    uint256 public oneUSDC = WAD / 1000000000000;
+    bytes6[] public path = new bytes6[](1);
+
+    function setUp() public {
+        dai = new DAIMock();
+        usdc = new USDCMock();
+        weth = new WETH9Mock();
+        daiEthAggregator = new ChainlinkAggregatorV3Mock();
+        usdcEthAggregator = new ChainlinkAggregatorV3Mock();
+        chainlinkMultiOracle = new ChainlinkMultiOracle();
+        chainlinkMultiOracle.grantRole(0xef532f2e, address(this));
+        chainlinkMultiOracle.setSource(DAI, dai, ETH, weth, address(daiEthAggregator));
+        chainlinkMultiOracle.setSource(USDC, usdc, ETH, weth, address(usdcEthAggregator));
+        vm.warp(uint256(mockBytes32));
+        daiEthAggregator.set(WAD / 2500);
+        usdcEthAggregator.set(WAD / 2500);
+        bytes4[] memory roles = new bytes4[](2);
+        roles[0] = 0x92b45d9c;
+        roles[1] = 0x60509e5f;
+        compositeMultiOracle = new CompositeMultiOracle();
+        compositeMultiOracle.grantRoles(roles, address(this));
+    }
+
+    function testSetSourceBothWays() public {
+        bytes6 baseId = DAI;
+        bytes6 quoteId = ETH;
+        address source = address(chainlinkMultiOracle);
+        require(address(compositeMultiOracle.sources(baseId, quoteId)) == 0x0000000000000000000000000000000000000000);
+        vm.expectEmit(true, true, true, false);
+        emit SourceSet(baseId, quoteId, IOracle(source));
+        compositeMultiOracle.setSource(baseId, quoteId, IOracle(source));
+    }
+
+    function testSetPathAndReservePath() public {
+        bytes6 baseId = DAI;
+        bytes6 quoteId = ETH;
+        path[0] = USDC;
+        compositeMultiOracle.setSource(DAI, USDC, IOracle(address(chainlinkMultiOracle)));
+        compositeMultiOracle.setSource(ETH, USDC, IOracle(address(chainlinkMultiOracle)));
+        vm.expectEmit(true, true, true, false);
+        emit PathSet(baseId, quoteId, path);
+        compositeMultiOracle.setPath(baseId, quoteId, path);
+        require(compositeMultiOracle.paths(baseId, quoteId, 0) == path[0]);
+        require(compositeMultiOracle.paths(quoteId, baseId, 0) == path[0]);
+    }
+
+    function setChainlinkMultiOracleSource() public {
+        compositeMultiOracle.setSource(DAI, ETH, IOracle(address(chainlinkMultiOracle)));
+        compositeMultiOracle.setSource(USDC, ETH, IOracle(address(chainlinkMultiOracle)));
+        path[0] = ETH;
+        compositeMultiOracle.setPath(DAI, USDC, path);
+    }
+
+    function testRetrieveConversionAndUpdateTime() public {
+        setChainlinkMultiOracleSource();
+        (uint256 conversion, uint256 updateTime) = compositeMultiOracle.peek(DAI, ETH, WAD);
+        require(conversion == WAD / 2500, "Get conversion unsuccessful");
+        require(updateTime > 0, "Update time below lower bound");
+        require(updateTime < 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff, "Update time above upper bound");
+        (uint256 usdcEthConversion,) = compositeMultiOracle.peek(USDC, ETH, oneUSDC);
+        require(usdcEthConversion == WAD / 2500, "Get USDC-ETH conversion unsuccessful");
+        (uint256 ethDaiConversion,) = compositeMultiOracle.peek(ETH, DAI, WAD);
+        require(ethDaiConversion == WAD * 2500, "Get ETH-DAI conversion unsuccessful");
+        (uint256 ethUsdcConversion,) = compositeMultiOracle.peek(ETH, USDC, WAD);
+        require(ethUsdcConversion == oneUSDC * 2500, "Get ETH-USDC conversion unsuccessful");
+    }
+
+    function testRevertOnTimestampGreaterThanCurrentBlock() public {
+        setChainlinkMultiOracleSource();
+        daiEthAggregator.setTimestamp(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+        vm.expectRevert("Invalid updateTime");
+        compositeMultiOracle.peek(DAI, ETH, WAD);
+    }
+
+    function testUseOldestTimestampFound() public {
+        setChainlinkMultiOracleSource();
+        daiEthAggregator.setTimestamp(1);
+        usdcEthAggregator.setTimestamp(block.timestamp);
+        (,uint256 updateTime) = compositeMultiOracle.peek(DAI, USDC, WAD);
+        require(updateTime == 1);
+    }
+
+    function testRetrieveDaiUsdcConversionAndReverse() public {
+        setChainlinkMultiOracleSource();
+        (uint256 daiUsdcConversion,) = compositeMultiOracle.peek(DAI, USDC, WAD);
+        require(daiUsdcConversion == oneUSDC);
+        (uint256 usdcDaiConversion,) = compositeMultiOracle.peek(USDC, DAI, oneUSDC);
+        require(usdcDaiConversion == WAD); 
+    }
+}
