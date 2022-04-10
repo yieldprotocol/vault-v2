76bda6442f503593009b33e4ce5f77508aae9bc2
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-04-10 15:48:56 -0500

    fix: add missing constants

--- contracts/foundry-tests/utils/TestConstants.sol
+++ contracts/foundry-tests/utils/TestConstants.sol
@@ -10,4 +10,6 @@ contract TestConstants {
     bytes6 public constant ETH = 0x303000000000;
     bytes6 public constant DAI = 0x303100000000;
     bytes6 public constant USDC = 0x303200000000;
+    bytes6 public constant WSTETH = 0x303400000000;
+    bytes6 public constant STETH = 0x303500000000;
 }
