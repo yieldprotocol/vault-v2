05b5efdf791cf97cad4b0248ad4a6ae88cb33d4c
Bruno Bonanno <bbonanno@gmail.com>
2022-05-17 12:01:36 +0200

    Remove contango cauldron

--- contracts/contango/interfaces/IContangoCauldron.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@yield-protocol/vault-interfaces/src/ICauldron.sol";
-
-interface IContangoCauldron is ICauldron {
-    function getFreeCollateral() external returns (int256);
-}
