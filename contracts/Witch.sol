0aab0e8f861b6c3aaea09c643debb22391574dee
alcueca <alberto@yield.is>
2022-05-18 10:56:51 +0100

    1e16 -> 0.01e18

--- contracts/Witch.sol
+++ contracts/Witch.sol
@@ -84,8 +84,8 @@ contract Witch is AccessControl {
     ) external auth {
         require(initialOffer <= 1e18, "InitialOffer above 100%");
         require(proportion <= 1e18, "Proportion above 100%");
-        require(initialOffer == 0 || initialOffer >= 1e16, "InitialOffer below 1%");
-        require(proportion >= 1e16, "Proportion below 1%");
+        require(initialOffer == 0 || initialOffer >= 0.01e18, "InitialOffer below 1%");
+        require(proportion >= 0.01e18, "Proportion below 1%");
         lines[ilkId][baseId] = Line({
             duration: duration,
             proportion: proportion,
