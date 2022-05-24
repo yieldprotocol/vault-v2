4f4ea30a4e00162c6c8819d33bbe32410a6e86c8
Bruno Bonanno <bbonanno@gmail.com>
2022-05-24 11:20:34 +0200

    update solc version to 0.8.14

--- contracts/oracles/yieldspace/PoolOracle.sol
+++ contracts/oracles/yieldspace/PoolOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.6;
+pragma solidity 0.8.14;
 
 import {IPool} from "@yield-protocol/yieldspace-interfaces/IPool.sol";
 import {IPoolOracle} from "./IPoolOracle.sol";
--- contracts/oracles/yieldspace/YieldSpaceMultiOracle.sol
+++ contracts/oracles/yieldspace/YieldSpaceMultiOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.6;
+pragma solidity 0.8.14;
 
 import "./IPoolOracle.sol";
 import "@yield-protocol/yieldspace-interfaces/IPool.sol";
