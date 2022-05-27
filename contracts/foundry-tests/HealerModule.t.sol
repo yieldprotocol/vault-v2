0a816f7a1c86214f18dd47e7fed5c678db5fd084
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-05-27 01:03:40 -0500

    begin healer module test

--- contracts/foundry-tests/HealerModule.t.sol
+++ contracts/foundry-tests/HealerModule.t.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.14;
 
+import "@yield-protocol/vault-interfaces/src/ICauldron.sol";
+import "@yield-protocol/vault-interfaces/src/ILadle.sol";
+import "@yield-protocol/utils-v2/contracts/interfaces/IWETH9.sol";
+import "../mocks/WETH9Mock.sol";
+import "../other/backd/HealerModule.sol";
 import "./utils/Test.sol";
 import "./utils/Mocks.sol";
-import "@yield-protocol/vault-interfaces/src/ILadle.sol";
 
 interface ILadleCustom {
     function addModule(address module, bool set) external;
@@ -14,10 +18,17 @@ interface ILadleCustom {
 contract HealerModuleTest is Test {
     using Mocks for *;
 
+    ICauldron public cauldron;
     ILadle public ladle;
+    HealerModule public healer;
+    IWETH9 public weth;
 
     function setUp() public {
+        weth = IWETH9(Mocks.mock("WETH9"));
+        cauldron = ICauldron(Mocks.mock("Cauldron"));
         ladle = ILadle(Mocks.mock("Ladle"));
-        ILadleCustom(ladle).addModule(module, set);
+        healer = new HealerModule(cauldron, weth);
+
+        // ILadleCustom(ladle).addModule(address(healer), set);
     }
 }
