32a549419d1b9dd8370673d9ecec8b43ad26ff4d
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-05-27 23:25:15 -0500

    eod commit

--- contracts/foundry-tests/HealerModule.t.sol
+++ contracts/foundry-tests/HealerModule.t.sol
@@ -24,17 +24,25 @@ contract HealerModuleTest is Test {
     HealerModule public healer;
     IWETH9 public weth;
 
+    address public dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // DAI token address
+    bytes6 public ilkId = 0x303100000000; // DAI
+    bytes6 public seriesId = 0x303130370000; // ETH/DAI Sept 22 series
+    bytes12 public vaultId;
+
     function setUp() public {
         weth = IWETH9(Mocks.mock("WETH9"));
         cauldron = ICauldron(Mocks.mock("Cauldron"));
         ladle = ILadle(Mocks.mock("Ladle"));
         healer = new HealerModule(cauldron, weth);
-
         ILadleCustom(address(ladle)).addModule(address(healer), true);
+        (vaultId, ) = ladle.build(seriesId, ilkId, 0);
     }
 
     function testHeal() public {
         console.log("Should add collateral to vault");
-        ILadleCustom(address(ladle)).moduleCall(address(healer), abi.encode());
+        ILadleCustom(address(ladle)).moduleCall(
+            address(healer), 
+            abi.encodeWithSelector(bytes4(healer.heal.selector), vaultId, 1, 0)
+        );
     }
 }
