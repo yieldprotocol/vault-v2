de963f5e1c014a74aa7d661312c3d5623e857ec1
iamsahu <praffulsahu@gmail.com>
2022-05-31 21:46:37 +0530

    feat: comments & events

--- contracts/other/yieldspace-tv/BaseConverter.sol
+++ contracts/other/yieldspace-tv/BaseConverter.sol
@@ -4,17 +4,51 @@ pragma solidity 0.8.14;
 import "@yield-protocol/utils-v2/contracts/access/AccessControl.sol";
 
 abstract contract BaseConverter is AccessControl {
-    // View functions
+    /// @notice An even to capture wrapping of the asset
+    /// @param to Address to which wrapped asset is sent
+    /// @param amount Amount of asset which was wrapped
+    /// @param wrappedAmount Amount of wrapped asset received
+    event Wrapped(address indexed to, uint256 amount, uint256 wrappedAmount);
+
+    /// @notice An even to capture unwrapping of the asset
+    /// @param to Address to which unwrapped asset is sent
+    /// @param amount Amount of asset which was unwrapped
+    /// @param unwrappedAmount Amount of unwrapped asset received
+    event Unwrapped(address indexed to, uint256 amount, uint256 unwrappedAmount);
+
+    /* View Functions
+     ******************************************************************************************************************/
+    /// @notice A function to get exactly how many wrappedAsset would be obtained from wrapping assets.
+    /// @param assets Amount of assets to be wrapped
+    /// @return The amount of wrapped assets that would be received
     function wrappedFrom(uint256 assets) external view virtual returns (uint256);
 
+    /// @notice A function to get exactly how many asset would be obtained from unwrapping wrappedAssets
+    /// @param wrappedAssets Amount of wrapped asset
+    /// @return Amount of assets that would be received
     function assetFrom(uint256 wrappedAssets) external view virtual returns (uint256);
 
+    /// @notice A function to get exactly how many wrappedAssets would need to be unwrapped to obtain assets.
+    /// @param assets Amount of assets to be obtained
+    /// @return Amount of wrapped assets that would be required
     function wrappedFor(uint256 assets) external view virtual returns (uint256);
 
+    /// @notice A function to get exactly how many assets would need to be wrapped to obtain wrappedAssets.
+    /// @param wrappedAssets Amount of wrapped asset to be obtained
+    /// @return Amount of assets that would be required
     function assetFor(uint256 wrappedAssets) external view virtual returns (uint256);
 
-    // State modifier functions
-    function wrap(address to) external virtual {}
+    /* Converter functions
+     ******************************************************************************************************************/
+    /// @notice Wraps the asset present in the contract
+    /// @dev The asset to be wrapped must have been transferred to the converter before this is called
+    /// @param to Address to which the wrapped asset would be sent
+    /// @return wrappedAmount Amount of wrapped assets received
+    function wrap(address to) external virtual returns (uint256 wrappedAmount) {}
 
-    function unwrap(address to) external virtual {}
+    /// @notice Unwraps the asset present in the contract
+    /// @dev The asset to be unwrapped must have been transferred to the converter before this is called
+    /// @param to Address to which the unwrapped asset would be sent
+    /// @return unwrappedAmount Amount of unwrapped assets received
+    function unwrap(address to) external virtual returns (uint256 unwrappedAmount) {}
 }
--- contracts/other/yieldspace-tv/YVTokenConverter.sol
+++ contracts/other/yieldspace-tv/YVTokenConverter.sol
@@ -5,46 +5,72 @@ import "./BaseConverter.sol";
 import "../../oracles/yearn/IYvToken.sol";
 import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 
+/// @title A contract to convert between yvToken to underlying & vice versa
 contract YVTokenConverter is BaseConverter {
+    /// @notice The wrapped asset
     IYvToken wrappedAsset;
+
+    /// @notice The asset
     IERC20 asset;
 
     constructor(address asset_, address wrappedAsset_) {
         wrappedAsset = IYvToken(wrappedAsset_);
         asset = IERC20(asset_);
+        asset.approve(wrappedAsset_, type(uint256).max);
     }
 
-    // View functions
-
-    // Return exactly how many wrappedAsset would be obtained from wrapping assets.
-    function wrappedFrom(uint256 assets) external view override returns (uint256 shares) {
+    /* View Functions
+     ******************************************************************************************************************/
+    /// @notice A function to get exactly how many wrappedAsset would be obtained from wrapping assets.
+    /// @param assets Amount of assets to be wrapped
+    /// @return The amount of wrapped assets that would be received
+    function wrappedFrom(uint256 assets) external view override returns (uint256) {
         require(assets > 0);
         return (assets * wrappedAsset.totalSupply()) / wrappedAsset.totalAssets();
     }
 
-    // Return exactly how many asset would be obtained from unwrapping wrappedAssets
+    /// @notice A function to get exactly how many asset would be obtained from unwrapping wrappedAssets
+    /// @param wrappedAssets Amount of wrapped asset
+    /// @return Amount of assets that would be received
     function assetFrom(uint256 wrappedAssets) external view override returns (uint256) {
         return (wrappedAssets * wrappedAsset.totalAssets()) / wrappedAsset.totalSupply();
     }
 
-    // Return exactly how many wrappedAssets would need to be unwrapped to obtain assets.
+    /// @notice A function to get exactly how many wrappedAssets would need to be unwrapped to obtain assets.
+    /// @param assets Amount of assets to be obtained
+    /// @return Amount of wrapped assets that would be required
     function wrappedFor(uint256 assets) external view override returns (uint256) {
         return (assets * wrappedAsset.totalSupply()) / wrappedAsset.totalAssets();
     }
 
-    // Return exactly how many assets would need to be wrapped to obtain wrappedAssets.
+    /// @notice A function to get exactly how many assets would need to be wrapped to obtain wrappedAssets.
+    /// @param wrappedAssets Amount of wrapped asset to be obtained
+    /// @return Amount of assets that would be required
     function assetFor(uint256 wrappedAssets) external view override returns (uint256) {
         return (wrappedAssets * wrappedAsset.totalAssets()) / wrappedAsset.totalSupply();
     }
 
-    // State modifier functions
-    function wrap(address to) external override {
+    /* Converter functions
+     ******************************************************************************************************************/
+    /// @notice Wraps the asset present in the contract
+    /// @dev The asset to be wrapped must have been transferred to the converter before this is called
+    /// @param to Address to which the wrapped asset would be sent
+    /// @return wrappedAmount Amount of wrapped assets received
+    function wrap(address to) external override returns (uint256 wrappedAmount) {
         uint256 amount = asset.balanceOf(address(this));
-        wrappedAsset.deposit(amount, to); // Why are we depositing as a converter?
+        wrappedAmount = wrappedAsset.deposit(amount, to);
+
+        emit Wrapped(to, amount, wrappedAmount);
     }
 
-    function unwrap(address to) external override {
+    /// @notice Unwraps the asset present in the contract
+    /// @dev The asset to be unwrapped must have been transferred to the converter before this is called
+    /// @param to Address to which the unwrapped asset would be sent
+    /// @return unwrappedAmount Amount of unwrapped assets received
+    function unwrap(address to) external override returns (uint256 unwrappedAmount) {
         uint256 amount = IERC20(address(wrappedAsset)).balanceOf(address(this));
-        wrappedAsset.withdraw(amount, to); // Wouldn't the converter needs to be the depositer?
+        unwrappedAmount = wrappedAsset.withdraw(amount, to);
+
+        emit Unwrapped(to, amount, unwrappedAmount);
     }
 }
