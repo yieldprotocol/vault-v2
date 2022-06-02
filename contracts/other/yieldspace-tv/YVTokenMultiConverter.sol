709a559a5b382c29803a620437eacbcdaf79529b
iamsahu <praffulsahu@gmail.com>
2022-06-02 11:41:42 +0530

    fix: incorporated suggested changes

--- contracts/other/yieldspace-tv/YVTokenMultiConverter.sol
+++ contracts/other/yieldspace-tv/YVTokenMultiConverter.sol
@@ -9,43 +9,69 @@ import "@yield-protocol/utils-v2/contracts/access/AccessControl.sol";
 /// @title A contract to convert between yvToken to underlying & vice versa
 contract YVTokenMultiConverter is IConverter, AccessControl {
     /// @notice Mapping of yvToken to underlying asset
-    mapping(IYvToken => IERC20) yvTokenMapping;
+    mapping(IYvToken => IERC20) wrappers;
+
+    modifier supportedYV(IYvToken yvToken) {
+        require(wrappers[yvToken] != IERC20(address(0)), "yvToken not supported");
+        _;
+    }
 
     /* View Functions
      ******************************************************************************************************************/
     /// @notice A function to get exactly how many yvToken would be obtained from wrapping assets.
-    /// @param yvToken_ The yvToken for which calculation needs to be done
+    /// @param yvToken The yvToken for which calculation needs to be done
     /// @param assets Amount of assets to be wrapped
     /// @return The amount of wrapped assets that would be received
-    function wrappedFrom(address yvToken_, uint256 assets) external view override returns (uint256) {
-        IYvToken yvToken = IYvToken(yvToken_);
+    function wrappedFrom(IYvToken yvToken, uint256 assets)
+        external
+        view
+        override
+        supportedYV(yvToken)
+        returns (uint256)
+    {
         return (assets * yvToken.totalSupply()) / yvToken.totalAssets();
     }
 
     /// @notice A function to get exactly how many asset would be obtained from unwrapping wrappedAssets
-    /// @param yvToken_ The yvToken for which calculation needs to be done
+    /// @param yvToken The yvToken for which calculation needs to be done
     /// @param wrappedAssets Amount of wrapped asset
     /// @return Amount of assets that would be received
-    function assetFrom(address yvToken_, uint256 wrappedAssets) external view override returns (uint256) {
-        IYvToken yvToken = IYvToken(yvToken_);
+    function assetFrom(IYvToken yvToken, uint256 wrappedAssets)
+        external
+        view
+        override
+        supportedYV(yvToken)
+        returns (uint256)
+    {
+        require(wrappers[yvToken] != IERC20(address(0)), "yvToken not supported");
         return (wrappedAssets * yvToken.totalAssets()) / yvToken.totalSupply();
     }
 
     /// @notice A function to get exactly how many wrappedAssets would need to be unwrapped to obtain assets.]
-    /// @param yvToken_ The yvToken for which calculation needs to be done
+    /// @param yvToken The yvToken for which calculation needs to be done
     /// @param assets Amount of assets to be obtained
     /// @return Amount of wrapped assets that would be required
-    function wrappedFor(address yvToken_, uint256 assets) external view override returns (uint256) {
-        IYvToken yvToken = IYvToken(yvToken_);
+    function wrappedFor(IYvToken yvToken, uint256 assets)
+        external
+        view
+        override
+        supportedYV(yvToken)
+        returns (uint256)
+    {
         return (assets * yvToken.totalSupply()) / yvToken.totalAssets();
     }
 
     /// @notice A function to get exactly how many assets would need to be wrapped to obtain wrappedAssets.
-    /// @param yvToken_ The yvToken for which calculation needs to be done
+    /// @param yvToken The yvToken for which calculation needs to be done
     /// @param wrappedAssets Amount of wrapped asset to be obtained
     /// @return Amount of assets that would be required
-    function assetFor(address yvToken_, uint256 wrappedAssets) external view override returns (uint256) {
-        IYvToken yvToken = IYvToken(yvToken_);
+    function assetFor(IYvToken yvToken, uint256 wrappedAssets)
+        external
+        view
+        override
+        supportedYV(yvToken)
+        returns (uint256)
+    {
         return (wrappedAssets * yvToken.totalAssets()) / yvToken.totalSupply();
     }
 
@@ -53,39 +79,41 @@ contract YVTokenMultiConverter is IConverter, AccessControl {
      ******************************************************************************************************************/
     /// @notice Wraps the asset present in the contract
     /// @dev The asset to be wrapped must have been transferred to the converter before this is called
-    /// @param yvToken_ The yvToken to which asset would be wrapped into
+    /// @param yvToken The yvToken to which asset would be wrapped into
     /// @param to Address to which the wrapped asset would be sent
     /// @return wrappedAmount Amount of wrapped assets received
-    function wrap(address yvToken_, address to) external override returns (uint256 wrappedAmount) {
-        IYvToken yvToken = IYvToken(yvToken_);
-        IERC20 underlyingAsset = yvTokenMapping[yvToken];
+    function wrap(IYvToken yvToken, address to) external override supportedYV(yvToken) returns (uint256 wrappedAmount) {
+        IERC20 underlyingAsset = wrappers[yvToken];
         uint256 amount = underlyingAsset.balanceOf(address(this));
         wrappedAmount = yvToken.deposit(amount, to);
 
-        emit Wrapped(yvToken_, to, amount, wrappedAmount);
+        emit Wrapped(yvToken, to, amount, wrappedAmount);
     }
 
     /// @notice Unwraps the asset present in the contract
     /// @dev The asset to be unwrapped must have been transferred to the converter before this is called
-    /// @param yvToken_ The yvToken which would be unwrapped
+    /// @param yvToken The yvToken which would be unwrapped
     /// @param to Address to which the unwrapped asset would be sent
     /// @return unwrappedAmount Amount of unwrapped assets received
-    function unwrap(address yvToken_, address to) external override returns (uint256 unwrappedAmount) {
-        IYvToken yvToken = IYvToken(yvToken_);
+    function unwrap(IYvToken yvToken, address to)
+        external
+        override
+        supportedYV(yvToken)
+        returns (uint256 unwrappedAmount)
+    {
         uint256 amount = yvToken.balanceOf(address(this));
         unwrappedAmount = yvToken.withdraw(amount, to);
 
-        emit Unwrapped(yvToken_, to, amount, unwrappedAmount);
+        emit Unwrapped(yvToken, to, amount, unwrappedAmount);
     }
 
     /* Governance functions
      ******************************************************************************************************************/
-    /// @notice Explain to an end user what this does
-    /// @dev Explain to a developer any extra details
-    /// @param asset a parameter just like in doxygen (must be followed by parameter name)
-    /// @param yvToken a parameter just like in doxygen (must be followed by parameter name)
-    function addMapping(IERC20 asset, IYvToken yvToken) external auth {
-        yvTokenMapping[yvToken] = asset;
+    /// @notice Function to add wrapper token
+    /// @param yvToken The yvToken to be added
+    function addWrapper(IYvToken yvToken) external auth {
+        IERC20 asset = IERC20(yvToken.token());
+        wrappers[yvToken] = asset;
         asset.approve(address(yvToken), 0);
         asset.approve(address(yvToken), type(uint256).max);
     }
