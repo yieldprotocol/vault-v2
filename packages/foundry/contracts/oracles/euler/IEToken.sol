3aae7cef7d16c55bbffd51d327a3bc58d3a76978
David Brailovsky <davidbrai@gmail.com>
2022-06-10 10:47:15 +0200

    cleanup: remove unneeded import

--- packages/foundry/contracts/oracles/euler/IEToken.sol
+++ packages/foundry/contracts/oracles/euler/IEToken.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0;
-import "@yield-protocol/utils-v2/contracts/token/IERC20Metadata.sol";
 
 interface IEToken {
     function convertBalanceToUnderlying(uint256 balance) external view returns (uint256);
