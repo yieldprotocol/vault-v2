bcf27532e6830bfb66dea98200c1997dd79482d1
marcomariscal <marco.a.mariscal@gmail.com>
2022-06-28 13:41:29 -0700

    feat: flash join test from hardhat to foundry (preliminary implementation)

--- packages/foundry/contracts/test/join/JoinFlash.t.sol
+++ packages/foundry/contracts/test/join/JoinFlash.t.sol
@@ -2,15 +2,16 @@
 pragma solidity 0.8.14;
 
 import "../../mocks/DAIMock.sol";
-import "../../FlashJoin.sol";
+import {FlashJoin} from "../../FlashJoin.sol";
 import "../utils/Utilities.sol";
 import "../utils/Test.sol";
 import "../utils/TestConstants.sol";
 
-import "erc3156/contracts/interfaces/IERC3156FlashBorrower.sol";
-import "erc3156/contracts/FlashBorrower.sol";
+import {FlashBorrower} from "erc3156/contracts/FlashBorrower.sol";
 
 contract JoinFlashTest is Test, TestConstants {
+    event FlashFeeFactorSet(uint256 indexed fee);
+
     DAIMock public token;
     FlashJoin public join;
     Utilities internal utils;
@@ -26,22 +27,20 @@ contract JoinFlashTest is Test, TestConstants {
         string reenter;
     }
 
-    function setUp() public {
-        Actions actions = new Actions();
-        actions
-            .none = "0x0000000000000000000000000000000000000000000000000000000000000000";
-        actions
-            .transfer = "0x0000000000000000000000000000000000000000000000000000000000000001";
-        actions
-            .steal = "0x0000000000000000000000000000000000000000000000000000000000000002";
-        actions
-            .reenter = "0x0000000000000000000000000000000000000000000000000000000000000003";
+    Actions actions =
+        Actions(
+            "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000000000000000000000000000001",
+            "0x0000000000000000000000000000000000000000000000000000000000000002",
+            "0x0000000000000000000000000000000000000000000000000000000000000003"
+        );
 
+    function setUp() public {
         utils = new Utilities();
 
         token = new DAIMock();
         join = new FlashJoin(address(token));
-        borrower = new FlashBorrower();
+        borrower = new FlashBorrower(join);
 
         admin = address(0xa11ce);
         other = address(0xb0b);
@@ -53,33 +52,86 @@ contract JoinFlashTest is Test, TestConstants {
 
         token.mint(address(join), WAD * 100);
         vm.prank(admin);
-        join.join(admin, WAD * 100);
+        join.join(admin, uint128(WAD * 100));
     }
 
     function testFlashDisabledByDefault() public {
-        vm.expectRevert(
-            join.flashLoan(address(borrower), token, WAD, Actions.none)
-        );
+        vm.expectRevert(stdError.arithmeticError);
+        join.flashLoan(borrower, address(token), WAD, bytes(actions.none));
+    }
+
+    function testRevertsWithoutRepayApproval() public {
+        uint256 feeFactor = 0;
+        vm.prank(admin);
+        join.setFlashFeeFactor(feeFactor);
+
+        vm.prank(address(borrower));
+        vm.expectRevert();
+        join.flashLoan(borrower, address(token), WAD, bytes(actions.none));
     }
 
     // with zero fee
-    function testRevertsWithoutApproval() public {}
+    function testFlashNoFee() public {
+        uint256 feeFactor = 0;
+        vm.prank(admin);
+        join.setFlashFeeFactor(feeFactor);
+
+        borrower.flashBorrow(address(token), WAD);
+
+        assertEq(token.balanceOf(admin), 0);
+        assertEq(borrower.flashBalance(), WAD);
+        assertEq(borrower.flashToken(), address(token));
+        assertEq(borrower.flashAmount(), WAD);
+        assertEq(borrower.flashInitiator(), address(borrower));
+    }
 
     // with zero fee
-    function testFlashNoFee() public {}
+    function testRevertsWithInsuffienctBalance() public {
+        uint256 feeFactor = 0;
+        vm.prank(admin);
+        join.setFlashFeeFactor(feeFactor);
+
+        vm.expectRevert("ERC20: Insufficient approval");
+        borrower.flashBorrowAndSteal(address(token), WAD);
+    }
 
     // with zero fee
-    function testCanRepayByTransfer() public {}
+    function testTwoNestedFlash() public {
+        uint256 feeFactor = 0;
+        vm.prank(admin);
+        join.setFlashFeeFactor(feeFactor);
+
+        borrower.flashBorrowAndReenter(address(token), WAD); // It will borrow WAD, and then reenter and borrow WAD * 2
+        assertEq(borrower.flashBalance(), WAD * 3);
+    }
 
     // with zero fee
-    function testRevertsWithInsuffienctBalance() public {}
+    function testSetsFlashFeeFactor() public {
+        uint256 feeFactor = (WAD * 5) / 100; // 5%
+        vm.prank(admin);
 
-    // with zero fee
-    function testTwoNestedFlash() public {}
-
-    // with zero fee
-    function testSetsFlashFeeFactor() public {}
+        vm.expectEmit(true, false, false, false);
+        emit FlashFeeFactorSet(feeFactor);
+        join.setFlashFeeFactor(feeFactor);
+        assertEq(join.flashFeeFactor(), feeFactor);
+    }
 
     // with non-zero fee
-    function testFlashWithFee() public {}
+    function testFlashWithFee() public {
+        uint256 feeFactor = (WAD * 5) / 100; // 5%
+        vm.prank(admin);
+        join.setFlashFeeFactor(feeFactor);
+
+        uint256 principal = WAD;
+        uint256 fee = feeFactor;
+        token.mint(address(borrower), fee);
+
+        borrower.flashBorrow(address(token), principal);
+
+        assertEq(token.balanceOf(admin), 0);
+        assertEq(borrower.flashBalance(), principal + fee);
+        assertEq(borrower.flashToken(), address(token));
+        assertEq(borrower.flashAmount(), principal);
+        assertEq(borrower.flashInitiator(), address(borrower));
+    }
 }
