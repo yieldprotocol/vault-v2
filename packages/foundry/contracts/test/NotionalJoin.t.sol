1427d10ff045a3eb3f8779ac4382a15bf3fa13f4
calnix <calnix.289@gmail.com>
2022-07-06 10:24:45 +0800

    Changed NotionalJoin.t.sol to use deployed underlying join instead of Mock.sol

--- packages/foundry/contracts/test/NotionalJoin.t.sol
+++ packages/foundry/contracts/test/NotionalJoin.t.sol
@@ -8,7 +8,8 @@ import "../test/utils/TestConstants.sol";
 import "../test/utils/Mocks.sol";
 import "../mocks/ERC20Mock.sol";
 
-import {IJoin} from "@yield-protocol/vault-interfaces/src/IJoin.sol";
+//import {IJoin} from "@yield-protocol/vault-interfaces/src/IJoin.sol";
+import {Join} from "../Join.sol";
 import {NotionalJoin} from "../other/notional/NotionalJoin.sol";
 import {FCashMock} from "../other/notional/FCashMock.sol";
 import {DAIMock} from "../mocks/DAIMock.sol";
@@ -18,12 +19,11 @@ using stdStorage for StdStorage;
 abstract contract StateMatured is Test, TestConstants {
     using Mocks for *;
 
+    Join public underlyingJoin; 
     NotionalJoin public njoin; 
     FCashMock public fcash;
     DAIMock public dai;
-
-    IJoin internal underlyingJoin;
-    
+        
     address user; 
     address deployer;
 
@@ -35,29 +35,33 @@ abstract contract StateMatured is Test, TestConstants {
     event Redeemed(uint256 fCash, uint256 underlying, uint256 accrual);
 
     function setUp() public virtual {
-
-        dai = new DAIMock();
-        vm.label(address(dai), "dai contract");
         
-        // Create mock of underlying join
-        underlyingJoin = IJoin(Mocks.mock("Join"));
-
-        fcash = new FCashMock(ERC20Mock(address(dai)), fCashId);
-        vm.label(address(fcash), "fCashMock contract");
-
-        njoin = new NotionalJoin(address(fcash), address(dai), address(underlyingJoin), maturity, currencyId);
-        vm.label(address(njoin), "Notional Join");
-        
+        //... Users ...
         user = address(1);
         vm.label(user, "user");
-
+        
         deployer = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
         vm.label(deployer, "deployer");
 
-        //grant access permissions to deployer
+        //... Contracts ...
+        dai = new DAIMock();
+        vm.label(address(dai), "Dai token contract");
+        
+        fcash = new FCashMock(ERC20Mock(address(dai)), fCashId);
+        vm.label(address(fcash), "fCashMock contract");
+
+        //... Deploy Joins and grant access ...
+        underlyingJoin = new Join(address(dai));
+        vm.label(address(dai), "Dai Join");
+
+        njoin = new NotionalJoin(address(fcash), address(dai), address(underlyingJoin), maturity, currencyId);
+        vm.label(address(njoin), "Notional Join");
+
         njoin.grantRole(NotionalJoin.exit.selector, deployer);
-
-
+        njoin.grantRole(NotionalJoin.exit.selector, deployer);
+        underlyingJoin.grantRole(Join.join.selector, address(njoin));       
+        underlyingJoin.grantRole(Join.exit.selector, address(njoin));
+        
        // njoin has 10 fCash tokens 
        stdstore
        .target(address(fcash))
@@ -73,7 +77,6 @@ abstract contract StateMatured is Test, TestConstants {
        .checked_write(10e18);
 
        fcash.setAccrual(1e18);  // set fCash == underlying for simplicity
-
     }  
 }
 
@@ -99,64 +102,38 @@ contract StateMaturedTest is StateMatured {
         assertTrue(njoin.accrual() == 0); 
     }
     
-    // test will fail on .join() call to underlying join in redeem()
-    function testCannotExitUnderlyingBeforeReedem() public {
-        console2.log("First exit call should not bypass redeem()");
-        
-        // mock for _exitUnderlying()
-        underlyingJoin.exit.mock(user, 10e18, 10e18);
-
-        vm.expectRevert(bytes("Not mocked!"));
-        vm.prank(deployer);
-        njoin.exit(user, 10e18);
-
-    }
-
     function testRedeem() public {
         console2.log("First exit call should call redeem()");
-
-        // mock for redeem()
-        underlyingJoin.join.mock(address(njoin), 10e18, 10e18);
-        // mock for _exitUnderlying()
-        underlyingJoin.exit.mock(user, 10e18, 10e18);
-
-        underlyingJoin.join.verify(address(njoin), 10e18);
-        underlyingJoin.exit.verify(user, 10e18);
-        
+       
         vm.expectEmit(true, true, true, false);
         emit Redeemed(0, 10e18, 1e18);
 
         vm.prank(deployer);
-        njoin.exit(user, 10e18);
-
-        assertTrue(njoin.storedBalance() == 0); 
-        assertTrue(dai.balanceOf(address(njoin)) == 0); 
+        njoin.exit(user, 5e18);
         
         assertTrue(njoin.accrual() == 1e18);
-        assertTrue(dai.balanceOf(address(underlyingJoin)) == 10e18);
+        assertTrue(njoin.storedBalance() == 0); 
+        assertTrue(dai.balanceOf(address(njoin)) == 0); 
+                
+        assertTrue(dai.balanceOf(user) == 5e18);
+        assertTrue(dai.balanceOf(address(underlyingJoin)) == 5e18);
     }
 }
 
 abstract contract StateRedeemed is StateMatured {
-    using Mocks for *;
 
      function setUp() public override virtual {
         super.setUp();
 
-        // state transition: accrual > 0
-        underlyingJoin.join.mock(address(njoin), 10e18, 10e18);
-        underlyingJoin.exit.mock(user, 10e18, 10e18);
-          
+        // state transition: accrual > 0         
         vm.prank(deployer);
-        njoin.exit(user, 10e18);
-
+        njoin.exit(user, 5e18);
         assertTrue(njoin.accrual() == 1e18);
     }
 
 }
 
 contract StateRedeemedTest is StateRedeemed {
-    using Mocks for *;
 
     function testCannotRedeem() public {
         console2.log("Redeem will revert since accrual > 0");
@@ -167,17 +144,17 @@ contract StateRedeemedTest is StateRedeemed {
     }
 
     function testSubsequentExit() public {
-        console2.log("Redeem should be skipped, _exitUnderlying executed");
-
-        underlyingJoin.exit.mock(user, 10e18, 10e18);
-        underlyingJoin.exit.verify(user, 10e18);
+        console2.log("_exitUnderlying executed");
 
         vm.prank(deployer);
-        njoin.exit(user, 10e18);
+        njoin.exit(user, 5e18);
 
         assertTrue(njoin.storedBalance() == 0); 
         assertTrue(dai.balanceOf(address(njoin)) == 0); 
-        assertTrue(dai.balanceOf(address(underlyingJoin)) == 10e18);
+        assertTrue(dai.balanceOf(address(underlyingJoin)) == 0);
+
+        assertTrue(dai.balanceOf(address(user)) == 10e18);
+        
     }
 }
     
