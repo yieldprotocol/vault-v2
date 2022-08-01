e5a94e0e85a68c23e1503683d430a9649e725a46
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-08-01 13:51:31 -0500

    fix setup

--- packages/foundry/contracts/test/fyToken/FYToken.t.sol
+++ packages/foundry/contracts/test/fyToken/FYToken.t.sol
@@ -3,12 +3,16 @@ pragma solidity 0.8.15;
 
 import "forge-std/src/Test.sol";
 import "forge-std/src/console.sol";
+import "@yield-protocol/utils-v2/contracts/cast/CastU256I128.sol";
 import "../../Cauldron.sol";
 import "../../FYToken.sol";
 import "../../Join.sol";
 import "../../interfaces/ILadle.sol";
+import "../utils/TestConstants.sol";
 
-contract FYTokenTest is Test {
+contract FYTokenTest is Test, TestConstants {
+    using CastU256I128 for uint256;
+
     Cauldron public cauldron = Cauldron(0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867);
     ILadle public ladle = ILadle(0x6cB18fF2A33e981D1e38A663Ca056c0a5265066A);
     FYToken public fyDAI = FYToken(0xFCb9B8C5160Cf2999f9879D8230dCed469E72eeb);
@@ -23,8 +27,8 @@ contract FYTokenTest is Test {
 
     function setUp() public {
         vm.createSelectFork('mainnet');
-        vm.startPrank(timelock);
 
+        vm.startPrank(timelock);
         bytes4[] memory joinRoles = new bytes4[](2);
         joinRoles[0] = daiJoin.join.selector;
         joinRoles[1] = daiJoin.exit.selector;
@@ -36,12 +40,17 @@ contract FYTokenTest is Test {
         fyTokenRoles[1] = fyDAI.burn.selector;
         fyTokenRoles[2] = fyDAI.point.selector;
         fyDAI.grantRoles(fyTokenRoles, address(this));
-
-        bytes6[] memory ilkIds = new bytes6[](1);
-        ilkIds[0] = ilkId; 
-        (vaultId, ) = ladle.build(seriesId, ilkId, 0);
-
         vm.stopPrank();
+
+        (vaultId, ) = ladle.build(seriesId, ilkId, 0);                  //create vault
+        deal(dai, address(this), WAD * 1);                              //populate the test address/vault owner with 1 DAI
+        IERC20(dai).approve(address(daiJoin), WAD);         
+        ladle.pour(vaultId, address(this), WAD.i128(), WAD.i128());     //add ink and art to vault
+        
+        deal(dai, address(this), WAD * 2);                              //populate the test address/vault owner with 2 DAI
+        IERC20(dai).approve(address(daiJoin), WAD * 2);
+        vm.prank(address(ladle));
+        daiJoin.join(address(this), uint128(WAD * 2));                  //Join takes the 2 DAI
     }
 
     function testChangeChiOracle() public {
