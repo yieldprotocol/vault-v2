ee4482070ef3161a54a431e7368bcb513e0de687
iamsahu <praffulsahu@gmail.com>
2022-11-15 19:29:11 +0530

    chore: simplified tests further

--- packages/foundry/contracts/test/oracles/CrabOracleTest.t.sol
+++ packages/foundry/contracts/test/oracles/CrabOracleTest.t.sol
@@ -10,8 +10,6 @@ import "../utils/TestConstants.sol";
 
 contract CrabOracleTest is Test, TestConstants {
     CrabOracle public crabOracle;
-    bytes6 baseId = 0x323900000000;
-    bytes6 quoteId = ETH;
     UniswapV3Oracle uniswapV3Oracle =
         UniswapV3Oracle(0x358538ea4F52Ac15C551f88C701696f6d9b38F3C);
     address uniswapV3oSQTHPool = 0x82c427AdFDf2d245Ec51D8046b41c4ee87F0d29C;
@@ -21,17 +19,25 @@ contract CrabOracleTest is Test, TestConstants {
     function setUp() public {
         vm.createSelectFork("mainnet", 15974678);
         crabOracle = new CrabOracle();
-        crabOracle.grantRole(0x553aaac9, address(this));
+        crabOracle.grantRole(
+            bytes4(
+                keccak256("setSource(bytes6,bytes6,bytes6,address,address)")
+            ),
+            address(this)
+        );
         crabOracle.setSource(
-            baseId,
-            0x313900000000,
+            CRAB,
+            OSQTH,
             ETH,
             ICrabStrategy(crabStrategyV2),
             IOracle(address(uniswapV3Oracle))
         );
 
         vm.startPrank(timelock);
-        uniswapV3Oracle.grantRole(0xe4650418, timelock);
+        uniswapV3Oracle.grantRole(
+            bytes4(keccak256("setSource(bytes6,bytes6,address,uint32)")),
+            timelock
+        );
         uniswapV3Oracle.setSource(
             crabOracle.ethId(),
             crabOracle.oSQTHId(),
@@ -42,21 +48,13 @@ contract CrabOracleTest is Test, TestConstants {
     }
 
     function testPeek() public {
-        (uint256 amount, ) = crabOracle.peek(
-            bytes32(baseId),
-            bytes32(quoteId),
-            1e18
-        );
+        (uint256 amount, ) = crabOracle.peek(bytes32(CRAB), bytes32(ETH), 1e18);
         emit log_named_uint("Crab in ETH Value", amount);
         assertEq(amount, 962695640155633739);
     }
 
     function testPeekReversed() public {
-        (uint256 amount, ) = crabOracle.peek(
-            bytes32(quoteId),
-            bytes32(baseId),
-            1e18
-        );
+        (uint256 amount, ) = crabOracle.peek(bytes32(ETH), bytes32(CRAB), 1e18);
         emit log_named_uint("ETH in Crab Value", amount);
         assertEq(amount, 1038749900060143067);
     }
