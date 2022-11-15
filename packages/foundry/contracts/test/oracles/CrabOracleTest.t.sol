349b23b25f755642d45aa863055f30f0b7c408f4
iamsahu <praffulsahu@gmail.com>
2022-11-15 15:53:54 +0530

    fix: test fix

--- packages/foundry/contracts/test/oracles/CrabOracleTest.t.sol
+++ packages/foundry/contracts/test/oracles/CrabOracleTest.t.sol
@@ -6,33 +6,35 @@ import "@yield-protocol/utils-v2/contracts/access/AccessControl.sol";
 import "../../oracles/crab/CrabOracle.sol";
 import {ICrabStrategy} from "../../oracles/crab/CrabOracle.sol";
 import "../../oracles/uniswap/UniswapV3Oracle.sol";
+import "../utils/TestConstants.sol";
 
 contract CrabOracleTest is Test {
     CrabOracle public crabOracle;
-    bytes6 baseId = 0x323900000000;
+    bytes6 baseId = ETH;
     bytes6 quoteId = 0x303000000000;
     UniswapV3Oracle uniswapV3Oracle =
         UniswapV3Oracle(0x358538ea4F52Ac15C551f88C701696f6d9b38F3C);
+    address uniswapV3oSQTHPool = 0x82c427AdFDf2d245Ec51D8046b41c4ee87F0d29C;
+    address crabStrategyV2 = 0x3B960E47784150F5a63777201ee2B15253D713e8;
+    address timelock = 0x3b870db67a45611CF4723d44487EAF398fAc51E3;
 
     function setUp() public {
+        vm.createSelectFork("mainnet", 15974678);
         crabOracle = new CrabOracle();
         crabOracle.grantRole(0xac426579, address(this));
         crabOracle.setSource(
             baseId,
             0x313900000000,
-            ICrabStrategy(0x3B960E47784150F5a63777201ee2B15253D713e8),
+            ICrabStrategy(crabStrategyV2),
             IOracle(address(uniswapV3Oracle))
         );
 
-        vm.startPrank(0x3b870db67a45611CF4723d44487EAF398fAc51E3);
-        uniswapV3Oracle.grantRole(
-            0xe4650418,
-            0x3b870db67a45611CF4723d44487EAF398fAc51E3
-        );
+        vm.startPrank(timelock);
+        uniswapV3Oracle.grantRole(0xe4650418, timelock);
         uniswapV3Oracle.setSource(
             crabOracle.weth(),
             crabOracle.oSQTH(),
-            0x82c427AdFDf2d245Ec51D8046b41c4ee87F0d29C,
+            uniswapV3oSQTHPool,
             100
         );
         vm.stopPrank();
@@ -45,7 +47,7 @@ contract CrabOracleTest is Test {
             1e18
         );
         emit log_named_uint("Crab in ETH Value", amount);
-        // assertEq(amount, 1000000000000000000);
+        assertEq(amount, 962695640155633739);
     }
 
     function testPeekReversed() public {
@@ -55,6 +57,6 @@ contract CrabOracleTest is Test {
             1e18
         );
         emit log_named_uint("ETH in Crab Value", amount);
-        // assertEq(amount, 1000000000000000000);
+        assertEq(amount, 1038749900060143067);
     }
 }
