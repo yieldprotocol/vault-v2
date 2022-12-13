bec6f4396150f6f0bf09669c65e36ff03cd5b934
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-12-13 11:49:19 -0600

    tenderly compatibility

--- packages/foundry/contracts/test/fyToken/fytokenHarness.sh
+++ packages/foundry/contracts/test/fyToken/fytokenHarness.sh
@@ -6,7 +6,7 @@ MAINNET_FYTOKENS=(\
     "0x0FBd5ca8eE61ec921B3F61B707f1D7D64456d2d1"\
 )
 
-export NETWORK="MAINNET"
+export NETWORK="TENDERLY"
 export MOCK=false
 
 for fytoken in ${MAINNET_FYTOKENS[@]}; do
