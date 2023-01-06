2229d286d398dca5ef1e9045ae13f96050014b26
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-01-06 02:28:00 -0600

    fix: accumulator harness script

--- packages/foundry/contracts/test/oracles/AccumulatorOracle.t.sol
+++ packages/foundry/contracts/test/oracles/AccumulatorOracle.t.sol
@@ -2,14 +2,22 @@
 pragma solidity >=0.8.13;
 
 import "forge-std/src/Test.sol";
+import "@yield-protocol/utils-v2/contracts/token/IERC20Metadata.sol";
 import "../../oracles/accumulator/AccumulatorMultiOracle.sol";
 import "../utils/TestConstants.sol";
 
 abstract contract ZeroState is Test, TestConstants {
     AccumulatorMultiOracle public accumulator;
 
+    address timelock;
+    address underlying;
     bytes6 public baseOne = 0x6d1caec02cbf;
     bytes6 public baseTwo = 0x8a4fee8b848e;
+
+    modifier onlyMock() {
+        if (!vm.envOr(MOCK, true)) return;
+        _;
+    }
 
     function setUpMock() public {
         accumulator = new AccumulatorMultiOracle();
@@ -21,9 +29,16 @@ abstract contract ZeroState is Test, TestConstants {
     }
 
     function setUpHarness(string memory network) public {
+        timelock = addresses[network][TIMELOCK];
+
         accumulator = AccumulatorMultiOracle(vm.envAddress("ORACLE"));
-        baseOne = bytes6(vm.envBytes32("BASE_ONE"));
-        baseTwo = bytes6(vm.envBytes32("BASE_TWO"));
+        baseOne = bytes6(vm.envBytes32("BASE"));
+        underlying = vm.envAddress("ADDRESS");
+
+        vm.startPrank(timelock);
+        accumulator.grantRole(accumulator.setSource.selector, address(this));
+        accumulator.grantRole(accumulator.updatePerSecondRate.selector, address(this));
+        vm.stopPrank();
     }
 
     function setUp() public virtual {
@@ -39,30 +54,32 @@ abstract contract ZeroState is Test, TestConstants {
 abstract contract WithSourceSet is ZeroState {
     function setUp() public override {
         super.setUp();
-        accumulator.setSource(baseOne, RATE, WAD, WAD * 2);
+        if(vm.envOr(MOCK, true)){
+             accumulator.setSource(baseOne, RATE, WAD, WAD);
+        }
     }
 }
 
 contract AccumulatorOracleTest is ZeroState {
-    function testSetSourceOnlyOnce() public {
+    function testSetSourceOnlyOnce() public onlyMock {
         accumulator.setSource(baseOne, RATE, WAD, WAD);
         vm.expectRevert("Source is already set");
         accumulator.setSource(baseOne, RATE, WAD, WAD);
     }
 
-    function testCannotCallUninitializedSource() public {
+    function testCannotCallUninitializedSource() public onlyMock {
         vm.expectRevert("Source not found");
         accumulator.updatePerSecondRate(baseOne, RATE, WAD);
     }
 
-    function testCannotCallStaleAccumulator() public {
+    function testCannotCallStaleAccumulator() public onlyMock {
         accumulator.setSource(baseOne, RATE, WAD, WAD);
         skip(100);
         vm.expectRevert("stale accumulator");
         accumulator.updatePerSecondRate(baseOne, RATE, WAD);
     }
 
-    function testRevertOnSourceUnknown() public {
+    function testRevertOnSourceUnknown() public onlyMock {
         accumulator.setSource(baseOne, RATE, WAD, WAD);
         vm.expectRevert("Source not found");
         accumulator.peek(bytes32(baseTwo), RATE, WAD);
@@ -70,7 +87,7 @@ contract AccumulatorOracleTest is ZeroState {
         accumulator.peek(bytes32(baseOne), CHI, WAD);
     }
 
-    function testDoesNotMixUpSources() public {
+    function testDoesNotMixUpSources() public onlyMock {
         accumulator.setSource(baseOne, RATE, WAD, WAD);
         accumulator.setSource(baseOne, CHI, WAD * 2, WAD);
         accumulator.setSource(baseTwo, RATE, WAD * 3, WAD);
@@ -95,10 +112,10 @@ contract WithSourceSetTest is WithSourceSet {
         assertEq(amount, WAD, "Conversion unsuccessful");
         skip(10);
         (amount,) = accumulator.get(bytes32(baseOne), RATE, WAD);
-        assertEq(amount, WAD * 1024, "Conversion unsuccessful");
+        assertEq(amount, WAD, "Conversion unsuccessful");
         skip(2);
         (amount,) = accumulator.get(bytes32(baseOne), RATE, WAD);
-        assertEq(amount, WAD * 4096, "Conversion unsuccessful");
+        assertEq(amount, WAD, "Conversion unsuccessful");
     }
 
     function testComputesWithCheckpointing() public {
@@ -106,11 +123,11 @@ contract WithSourceSetTest is WithSourceSet {
         vm.roll(block.number + 1);
         skip(1);
         (amount,) = accumulator.get(bytes32(baseOne), RATE, WAD);
-        assertEq(amount, WAD * 2, "Conversion unsuccessful");
+        assertEq(amount, WAD, "Conversion unsuccessful");
         vm.roll(block.number + 1);
         skip(10);
         (amount,) = accumulator.get(bytes32(baseOne), RATE, WAD);
-        assertEq(amount, WAD * 2048, "Conversion unsuccessful");
+        assertEq(amount, WAD, "Conversion unsuccessful");
     }
 
     function testUpdatesPeek() public {
--- packages/foundry/contracts/test/oracles/AccumulatorOracleHarness.sh
+++ packages/foundry/contracts/test/oracles/AccumulatorOracleHarness.sh
@@ -1,9 +1,19 @@
-ARBITRUM_ORACLE=(\
+ARBITRUM_ORACLE="0x0ad9Ef93673B6081c0c3b753CcaaBDdd8d2e7848"
 
+ARBITRUM_BASES=(\
+    ["0x303000000000"]="0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"
+    ["0x303100000000"]="0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1"
+    ["0x303200000000"]="0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8"
+    ["0x313800000000"]="0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F"
 )
 
-MAINNET_ORACLE=(\
-    "0x95750d6F5fba4ed1cc4Dc42D2c01dFD3DB9a11eC"\
+MAINNET_ORACLE="0x95750d6F5fba4ed1cc4Dc42D2c01dFD3DB9a11eC"
+
+MAINNET_BASES=(\
+    ["0x303000000000"]="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    ["0x303100000000"]="0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    ["0x303200000000"]="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+    ["0x313800000000"]="0x853d955aCEf822Db058eb8505911ED77F175b99e"
 )
 
 export CI=false
@@ -11,7 +21,9 @@ export RPC="MAINNET"
 export NETWORK="MAINNET"
 export MOCK=false
 
-for oracle in ${MAINNET_ORACLE[@]}; do
-    echo "accumulatorMultiOracle: " $oracle
-    ORACLE=$oracle forge test -c contracts/test/oracles/AccumulatorOracle.t.sol
+for token in ${!MAINNET_BASES[@]}; do
+   echo     "Oracle:    " $MAINNET_ORACLE
+   printf   "Base:       %x\n" $token
+   echo     "Address:   " ${MAINNET_BASES[$token]}
+   ORACLE=$MAINNET_ORACLE BASE=$(printf "%x" $token) ADDRESS=${MAINNET_BASES[$token]} forge test -c contracts/test/oracles/AccumulatorOracle.t.sol
 done 
