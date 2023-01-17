888f59224bcad5b1b651685ee19230c6c127ffe2
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-01-17 05:54:08 -0600

    feat: uniswap harness

--- packages/foundry/contracts/test/oracles/UniswapOracle.t.sol
+++ packages/foundry/contracts/test/oracles/UniswapOracle.t.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.13;
 import "forge-std/src/Test.sol";
 import "../../oracles/uniswap/UniswapV3Oracle.sol";
 import "../utils/TestConstants.sol";
+import { ERC20Mock } from "../../mocks/ERC20Mock.sol";
 import { TestExtensions } from "../TestExtensions.sol";
 
 contract UniswapOracleTest is Test, TestConstants, TestExtensions {
@@ -20,8 +21,23 @@ contract UniswapOracleTest is Test, TestConstants, TestExtensions {
 	address uniEthPool = 0x1d42064Fc4Beb5F8aAF85F4617AE8b3b5B8Bd801;
 	address wbtcUsdcPool = 0x99ac8cA7087fA4A2A1FB6357269965A2014ABc35;
 
+    // Harness vars
+    bytes6 public base;
+    bytes6 public quote;
+    uint128 public unitForBase;
+    uint128 public unitForQuote;
+
+    modifier onlyMock() {
+        if (vm.envOr(MOCK, true))
+        _;
+    }
+
+    modifier onlyHarness() {
+        if (vm.envOr(MOCK, true)) return;
+        _;
+    }
     
-    function setUp() public {
+    function setUpMock() public {
         vm.createSelectFork(MAINNET, 15044600);
 
         uniswapV3Oracle = new UniswapV3Oracle();
@@ -33,31 +49,58 @@ contract UniswapOracleTest is Test, TestConstants, TestExtensions {
         uniswapV3Oracle.setSource(wbtcId, usdcId, wbtcUsdcPool, 100);
     }
 
-    function testFraxUsdcConversion() public {
+    function setUpHarness() public {
+        string memory rpc = vm.envOr(RPC, MAINNET);
+        vm.createSelectFork(rpc);
+
+        uniswapV3Oracle = UniswapV3Oracle(vm.envAddress("ORACLE"));
+
+        base = bytes6(vm.envBytes32("BASE"));
+        quote = bytes6(vm.envBytes32("QUOTE"));
+        unitForBase = uint128(10 ** ERC20Mock(address(vm.envAddress("BASE_ADDRESS"))).decimals());
+        unitForQuote = uint128(10 ** ERC20Mock(address(vm.envAddress("QUOTE_ADDRESS"))).decimals());
+    }
+
+    function setUp() public {
+        if (vm.envOr(MOCK, true)) setUpMock();
+        else setUpHarness();
+    }
+
+
+    function testFraxUsdcConversion() public onlyMock {
         (uint256 fraxUsdcAmount,) = uniswapV3Oracle.get(bytes32(fraxId), bytes32(usdcId), 1e18);
         assertEq(fraxUsdcAmount / 1e6, 0, "FRAX/USDC conversion unsuccessful");
         (uint256 usdcFraxAmount,) = uniswapV3Oracle.get(bytes32(usdcId), bytes32(fraxId), 1000000);
         assertEq(usdcFraxAmount / 1e18, 1, "USDC/FRAX conversion unsuccessful");
     }
 
-    function testUsdcEthConversion() public {
+    function testUsdcEthConversion() public onlyMock {
         (uint256 usdcWethAmount,) = uniswapV3Oracle.get(bytes32(usdcId), bytes32(wethId), 1000000);
         assertEq(usdcWethAmount / 1e9, 885240, "USDC/WETH conversion unsuccessful");    // will show USDC in terms of gwei
         (uint256 wethUsdcAmount,) = uniswapV3Oracle.get(bytes32(wethId), bytes32(usdcId), 1e18);
         assertEq(wethUsdcAmount / 1e6, 1129, "WETH/USDC conversion unsuccessful");
     }
 
-    function testUniEthConversion() public {
+    function testUniEthConversion() public onlyMock {
         (uint256 uniWethAmount,) = uniswapV3Oracle.get(bytes32(uniId), bytes32(wethId), 1e18);
         assertEq(uniWethAmount / 1e9, 4368531, "UNI/WETH conversion unsuccessful");     // will show UNI in terms of gwei
         (uint256 wethUniAmount,) = uniswapV3Oracle.get(bytes32(wethId), bytes32(uniId), 1e18);
         assertEq(wethUniAmount / 1e18, 228, "WETH/UNI conversion unsuccessful"); 
     }
 
-    function testWbtcUsdcConversion() public {
+    function testWbtcUsdcConversion() public onlyMock {
         (uint256 wbtcUsdcAmount,) = uniswapV3Oracle.get(bytes32(wbtcId), bytes32(usdcId), 100000000);
         assertEq(wbtcUsdcAmount / 1e6, 20078, "WBTC/USDC conversion unsuccessful");
         (uint256 usdcWbtcAmount,) = uniswapV3Oracle.get(bytes32(usdcId), bytes32(wbtcId), 1000000);
         assertEq(usdcWbtcAmount, 4980, "USDC/WBTC conversion unsuccessful");
+    }
+
+    function testConversionHarness() public onlyHarness {
+        uint256 amount;
+        uint256 updateTime;
+        (amount, updateTime) = uniswapV3Oracle.peek(base, quote, unitForBase);
+        assertGt(updateTime, 0, "Update time below lower bound");
+        assertLt(updateTime, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff, "Update time above upper bound");
+        assertApproxEqRel(amount, 1e18, 1000e18);
     }
 }
--- packages/foundry/contracts/test/oracles/UniswapOracleHarness.sh
+++ packages/foundry/contracts/test/oracles/UniswapOracleHarness.sh
@@ -1,17 +1,32 @@
-ARBITRUM_ORACLE=(\
+ARBITRUM_ORACLE=""
 
-)
+MAINNET_ORACLE="0x358538ea4F52Ac15C551f88C701696f6d9b38F3C"
 
-MAINNET_ORACLE=(\
-    "0x358538ea4F52Ac15C551f88C701696f6d9b38F3C"\
+MAINNET_BASES=(\
+    ["0x303000000000"]="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    ["0x303700000000"]="0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72"
+    ["0x333900000000"]="0x4d19F33948b99800B6113Ff3e83beC9b537C85d2"
 )
 
 export CI=false
-export RPC="HARNESS"
+export RPC="MAINNET"
 export NETWORK="MAINNET"
 export MOCK=false
 
-for oracle in ${MAINNET_ORACLE[@]}; do
-    echo "uniswapOracle: " $oracle
-    ORACLE=$oracle forge test -c contracts/test/oracles/UniswapOracle.t.sol
+for base in ${!MAINNET_BASES[@]}; do
+    for quote in ${!MAINNET_BASES[@]}; do 
+        if [ $base -ne $quote ]; then 
+            echo     "Uniswap Oracle:   " $MAINNET_ORACLE
+            printf   "Base:              %x\n" $base
+            printf   "Quote:             %x\n" $quote
+            echo     "Base Address:     " ${MAINNET_BASES[$base]}
+            echo     "Quote Address:    " ${MAINNET_BASES[$quote]}
+            ORACLE=$MAINNET_ORACLE \
+            BASE=$(printf "%x" $base) \
+            QUOTE=$(printf "%x" $quote) \
+            BASE_ADDRESS=${MAINNET_BASES[$base]} \
+            QUOTE_ADDRESS=${MAINNET_BASES[$quote]} \
+            forge test -c contracts/test/oracles/UniswapOracle.t.sol -m testConversionHarness
+        fi
+    done
 done 
