b4a205e392be12f8faef2e23bd8d63b6c57421d2
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-01-18 09:31:09 -0600

    feat: notional harness

--- packages/foundry/contracts/test/oracles/NotionalMultiOracle.t.sol
+++ packages/foundry/contracts/test/oracles/NotionalMultiOracle.t.sol
@@ -5,6 +5,7 @@ import "forge-std/src/Test.sol";
 import "../../other/notional/NotionalMultiOracle.sol";
 import "../../mocks/DAIMock.sol";
 import "../../mocks/USDCMock.sol";
+import { ERC20Mock } from "../../mocks/ERC20Mock.sol";
 import "../utils/TestConstants.sol";
 
 contract NotionalMultiOracleTest is Test, TestConstants {
@@ -18,7 +19,23 @@ contract NotionalMultiOracleTest is Test, TestConstants {
     uint256 oneUSDC = 1e6;
     uint256 oneFCASH = 1e8;
 
-    function setUp() public {
+    // Harness vars
+    bytes6 public base;
+    bytes6 public quote;
+    uint128 public unitForBase;
+
+    modifier onlyMock() {
+        if (vm.envOr(MOCK, true))
+        _;
+    }
+
+    modifier onlyHarness() {
+        if (vm.envOr(MOCK, true)) return;
+        _;
+    }
+
+
+    function setUpMock() public {
         dai = new DAIMock();
         usdc = new USDCMock();
         notionalMultiOracle = new NotionalMultiOracle();
@@ -27,17 +44,33 @@ contract NotionalMultiOracleTest is Test, TestConstants {
         notionalMultiOracle.setSource(FUSDC, USDC, usdc);
     }
 
-    function testRevertOnUnknownSource() public {
+    function setUpHarness() public {
+        string memory rpc = vm.envOr(RPC, MAINNET);
+        vm.createSelectFork(rpc);
+
+        notionalMultiOracle = NotionalMultiOracle(vm.envAddress("ORACLE"));
+
+        base = bytes6(vm.envBytes32("BASE"));
+        quote = bytes6(vm.envBytes32("QUOTE"));
+        unitForBase = uint128(10 ** ERC20Mock(address(vm.envAddress("BASE_ADDRESS"))).decimals());
+    }
+
+    function setUp() public {
+        if (vm.envOr(MOCK, true)) setUpMock();
+        else setUpHarness();
+    }
+
+    function testRevertOnUnknownSource() public onlyMock {
         vm.expectRevert("Source not found");
         notionalMultiOracle.get(bytes32(FDAI), bytes32(USDC), oneFCASH);
     }
 
-    function testReturnsCorrectAmountForSameBaseAndQuote() public {
+    function testReturnsCorrectAmountForSameBaseAndQuote() public onlyMock {
         (uint256 amount,) = notionalMultiOracle.get(FDAI, FDAI, WAD * 2500);
         assertEq(amount, WAD * 2500, "Conversion unsuccessful");
     }
 
-    function testRetrieveFaceValueFromOracle() public {
+    function testRetrieveFaceValueFromOracle() public onlyMock {
         uint256 amount;
         (amount,) = notionalMultiOracle.get(bytes32(FDAI), bytes32(DAI), oneFCASH * 2500);
         assertEq(amount, WAD * 2500, "Conversion unsuccessful");
@@ -56,5 +89,20 @@ contract NotionalMultiOracleTest is Test, TestConstants {
         assertEq(amount, oneFCASH * 2500, "Conversion unsuccessful");
         (amount,) = notionalMultiOracle.peek(bytes32(USDC), bytes32(FUSDC), oneUSDC * 2500);
         assertEq(amount, oneFCASH * 2500, "Conversion unsuccessful");
+    }
+
+    function testConversionHarness() public onlyHarness {
+        uint256 amount;
+        uint256 updateTime;
+        // all fcash have 8 decimals
+        (amount, updateTime) = notionalMultiOracle.peek(base, quote, unitForBase);
+        assertGt(updateTime, 0, "Update time below lower bound");
+        assertLt(updateTime, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff, "Update time above upper bound");
+        console.log(amount);
+        assertApproxEqRel(amount, 1e8, 1e6);
+        // and reverse
+        console.log(amount);
+        (amount, updateTime) = notionalMultiOracle.peek(quote, base, 1e8);
+        assertApproxEqRel(amount, unitForBase, unitForBase / 100);
     }
 }
