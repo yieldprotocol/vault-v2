106750b8ba8503399af91c216c936ff8ed524aab
alcueca <alberto@yield.is>
2023-02-23 16:12:00 +0000

    Just screw it already

--- packages/hardhat/src/helpers.ts
+++
@@ -1,7 +0,0 @@
-import { Cauldron } from '../typechain/Cauldron'
-
-export async function getLastVaultId(cauldron: Cauldron): Promise<string> {
-  const logs = await cauldron.queryFilter(cauldron.filters.VaultBuilt(null, null, null, null))
-  const event = logs[logs.length - 1]
-  return event.args.vaultId
-}
