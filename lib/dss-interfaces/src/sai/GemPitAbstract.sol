bfaffdeef67f545857e2d87147b1efbf5740f8d4
alcueca <alberto@yield.is>
2023-02-24 09:35:18 +0000

    Revert "remove hardhat"
    This reverts commit 856331a4a39169db2dadd759b2a359bf707a9ff4.

--- lib/dss-interfaces/src/sai/GemPitAbstract.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.5.12;
-
-// https://github.com/makerdao/sai/blob/master/src/pit.sol
-interface GemPitAbstract {
-    function burn(address) external;
-}
--- lib/dss-interfaces/src/sai/SaiTokenAbstract.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.5.12;
-
-import { DSTokenAbstract } from "../dapp/DSTokenAbstract.sol";
-
-// Sai Token adheres to the DS-Token contract interface
-//  Use DSTokenAbstract directly when abstracting Sai
