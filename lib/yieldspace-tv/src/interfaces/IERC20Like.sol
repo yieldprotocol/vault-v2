d0bb5f3331acdf449997ae457fa183a6dc4875cc
alcueca <alberto@yield.is>
2023-02-27 12:07:21 +0000

    Revert "removed yarn packages"
    This reverts commit 07ed1945140bd50580455c7a60f7b8e4dfb5f491.

--- lib/yieldspace-tv/src/interfaces/IERC20Like.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.15;
-import "@yield-protocol/utils-v2/src/token/IERC20Metadata.sol";
-import "@yield-protocol/utils-v2/src/token/IERC20.sol";
-
-interface IERC20Like is IERC20, IERC20Metadata {
-    function mint(address receiver, uint256 shares) external;
-}
