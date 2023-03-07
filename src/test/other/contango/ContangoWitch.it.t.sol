0ad544bb52d4b21d516a444e50f55f8178204ff7
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2023-03-07 11:20:49 -0300

    insurance auction test cases

--- src/test/other/contango/ContangoWitch.it.t.sol
+++ src/test/other/contango/ContangoWitch.it.t.sol
@@ -65,6 +65,7 @@ contract ContangoWitchIntegrationTest is Test, TestConstants {
         artSeries = cauldron.series(ART_SERIES_ID);
         inkSeries = cauldron.series(INK_SERIES_ID);
 
+        // TODO witch replacement and insurance fund mocking can be replaced after deployment
         // replace witch and configure insurance
         vm.etch(
             address(WITCH),
@@ -101,7 +102,7 @@ contract ContangoWitchIntegrationTest is Test, TestConstants {
 
         _startAuction();
 
-        _liquidate();
+        _liquidate({expectedTopUp: 0, payFunction: _payBase});
     }
 
     function testPayBaseAllWithInsuranceAtEndOfRegularAuction() public {
@@ -111,7 +112,181 @@ contract ContangoWitchIntegrationTest is Test, TestConstants {
 
         vm.warp(auction.start + line.duration);
 
-        _liquidate();
+        _liquidate({expectedTopUp: 0, payFunction: _payBase});
+    }
+
+    function testPayBaseAllAndTakesAllWithInsurance() public {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + 1);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 1e18,
+            insureWithBasePercentage: 1e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payBase});
+    }
+
+    function testPayBaseAllAndTakesAllWithInsuranceAtEndOfInsuranceAuction()
+        public
+    {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + INSURANCE_AUCTION_DURATION);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 1e18,
+            insureWithBasePercentage: 0.5e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payBase});
+    }
+
+    function testPayBaseAllAndTakesAllWithInsurancePastEndOfInsuranceAuction()
+        public
+    {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(
+            auction.start + line.duration + INSURANCE_AUCTION_DURATION + 1 hours
+        );
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 1e18,
+            insureWithBasePercentage: 0
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payBase});
+    }
+
+    function testPayBaseAllAndTakesAllWithInsuranceUpToAvailablePercentage()
+        public
+    {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + 1);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 0.5e18,
+            insureWithBasePercentage: 1e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payBase});
+    }
+
+    function testPayBaseAllAndTakesAllWithInsuranceNotAvailable() public {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + 1);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 0,
+            insureWithBasePercentage: 1e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payBase});
+    }
+
+    function testPayFYTokenAllWithInsurance() public {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        _liquidate({expectedTopUp: 0, payFunction: _payFYToken});
+    }
+
+    function testPayFYTokenAllWithInsuranceAtEndOfRegularAuction() public {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration);
+
+        _liquidate({expectedTopUp: 0, payFunction: _payFYToken});
+    }
+
+    function testPayFYTokenAllAndTakesAllWithInsurance() public {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + 1);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 1e18,
+            insureWithBasePercentage: 1e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payFYToken});
+    }
+
+    function testPayFYTokenAllAndTakesAllWithInsuranceAtEndOfInsuranceAuction()
+        public
+    {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + INSURANCE_AUCTION_DURATION);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 1e18,
+            insureWithBasePercentage: 0.5e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payFYToken});
+    }
+
+    function testPayFYTokenAllAndTakesAllWithInsurancePastEndOfInsuranceAuction()
+        public
+    {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(
+            auction.start + line.duration + INSURANCE_AUCTION_DURATION + 1 hours
+        );
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 1e18,
+            insureWithBasePercentage: 0
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payFYToken});
+    }
+
+    function testPayFYTokenAllAndTakesAllWithInsuranceUpToAvailablePercentage()
+        public
+    {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + 1);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 0.5e18,
+            insureWithBasePercentage: 1e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payFYToken});
+    }
+
+    function testPayFYTokenAllAndTakesAllWithInsuranceNotAvailable() public {
+        _enterVaultAtCRLimit(100e6);
+
+        _startAuction();
+
+        vm.warp(auction.start + line.duration + 1);
+        uint128 expectedTopUp = _mockInsuranceFund({
+            availablePercentage: 0,
+            insureWithBasePercentage: 1e18
+        });
+
+        _liquidate({expectedTopUp: expectedTopUp, payFunction: _payFYToken});
     }
 
     // ================================ helpers ================================
@@ -163,39 +338,32 @@ contract ContangoWitchIntegrationTest is Test, TestConstants {
             .lines(auction.ilkId, auction.baseId);
     }
 
-    function _liquidate() private {
-        address artUnderlying = artSeries.fyToken.underlying();
+    function _liquidate(
+        uint128 expectedTopUp,
+        function(uint256, uint256) payFunction
+    ) private {
         address inkUnderlying = inkSeries.fyToken.underlying();
         IFYToken inkFYToken = inkSeries.fyToken;
 
         // quote
         (uint256 liquidatorCut, uint256 auctioneerCut, uint256 artIn) = witch
             .calcPayout(VAULT_ID, bot, auction.art);
-        uint256 premium = _premium(liquidatorCut);
+        uint256 expectedPremium = _premium(liquidatorCut);
 
         IContangoWitchListener(CONTANGO).collateralBought.mockAndVerify(
             VAULT_ID,
             bot,
-            liquidatorCut + auctioneerCut + premium,
-            artIn
+            liquidatorCut + auctioneerCut + expectedPremium,
+            artIn + expectedTopUp
         );
-
-        uint128 baseIn = cauldron.debtToBase(ART_SERIES_ID, artIn.u128());
 
         // liquidate
         IContangoWitchListener(CONTANGO).auctionEnded.mockAndVerify(
             VAULT_ID,
             CONTANGO
         );
-        deal(artUnderlying, bot, baseIn);
-        vm.startPrank(bot);
-        IERC20(artUnderlying).transfer(
-            address(ladle.joins(artSeries.baseId)),
-            baseIn
-        );
 
-        witch.payBase(VAULT_ID, bot, liquidatorCut.u128(), baseIn);
-        vm.stopPrank();
+        payFunction(liquidatorCut, artIn);
 
         // assert
         uint256 decimals = IERC20Metadata(inkUnderlying).decimals();
@@ -213,10 +381,36 @@ contract ContangoWitchIntegrationTest is Test, TestConstants {
         );
         assertEqDecimal(
             inkFYToken.balanceOf(insurancePremiumReceiver),
-            premium,
+            expectedPremium,
             decimals,
             "premium"
         );
+    }
+
+    function _payBase(uint256 liquidatorCut, uint256 artIn) private {
+        uint128 baseIn = cauldron.debtToBase(ART_SERIES_ID, artIn.u128());
+        address artUnderlying = artSeries.fyToken.underlying();
+
+        deal(artUnderlying, bot, baseIn);
+
+        vm.startPrank(bot);
+        IERC20(artUnderlying).transfer(
+            address(ladle.joins(artSeries.baseId)),
+            baseIn
+        );
+
+        witch.payBase(VAULT_ID, bot, liquidatorCut.u128(), baseIn);
+        vm.stopPrank();
+    }
+
+    function _payFYToken(uint256 liquidatorCut, uint256 artIn) private {
+        deal(address(artSeries.fyToken), bot, artIn);
+
+        vm.startPrank(bot);
+        artSeries.fyToken.transfer(address(artSeries.fyToken), artIn);
+
+        witch.payFYToken(VAULT_ID, bot, liquidatorCut.u128(), artIn.u128());
+        vm.stopPrank();
     }
 
     function _premium(
@@ -226,6 +420,72 @@ contract ContangoWitchIntegrationTest is Test, TestConstants {
             premium =
                 liquidatorCut.wdiv(1e18 - insurancePremium) -
                 liquidatorCut;
+        }
+    }
+
+    function _mockInsuranceFund(
+        uint256 availablePercentage,
+        uint256 insureWithBasePercentage
+    ) private returns (uint128 expectedTopUp) {
+        // quote
+        uint256 elapsed = (block.timestamp - (auction.start + line.duration));
+        uint256 cappedElapsed = elapsed < INSURANCE_AUCTION_DURATION
+            ? elapsed
+            : INSURANCE_AUCTION_DURATION;
+        expectedTopUp = cappedElapsed
+            .wdiv(INSURANCE_AUCTION_DURATION)
+            .wmul(maxInsuredProportion)
+            .wmul(auction.art)
+            .u128();
+        expectedTopUp = expectedTopUp.wmul(availablePercentage).u128();
+
+        // mock
+        insuranceFund.insuranceAvailable.mockAndVerify(
+            ART_SERIES_ID,
+            expectedTopUp
+        );
+
+        uint256 expectedFYTokenTopUp = expectedTopUp.wmul(
+            1e18 - insureWithBasePercentage
+        );
+        uint256 expectedBaseTopUp = cauldron.debtToBase(
+            ART_SERIES_ID,
+            expectedTopUp.wmulup(insureWithBasePercentage).u128()
+        );
+
+        if (expectedTopUp > 0) {
+            insuranceFund.insure.mockAndVerify(
+                ART_SERIES_ID,
+                cauldron.debtToBase(ART_SERIES_ID, expectedTopUp),
+                expectedFYTokenTopUp.u128(),
+                expectedBaseTopUp
+            );
+        }
+
+        // insure
+        // with fyToken
+        if (expectedFYTokenTopUp > 0) {
+            deal(
+                address(artSeries.fyToken),
+                address(insuranceFund),
+                expectedFYTokenTopUp
+            );
+
+            vm.prank(address(insuranceFund));
+            artSeries.fyToken.transfer(
+                address(artSeries.fyToken),
+                expectedFYTokenTopUp
+            );
+        }
+
+        // with base
+        if (expectedBaseTopUp > 0) {
+            address underlying = artSeries.fyToken.underlying();
+            deal(underlying, address(insuranceFund), expectedBaseTopUp);
+
+            address join = address(ladle.joins(artSeries.baseId));
+            vm.prank(address(insuranceFund));
+            IERC20(underlying).transfer(join, expectedBaseTopUp);
         }
     }
 }
